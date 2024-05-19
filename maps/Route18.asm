9fd5b6faf6edcaf2cb6bf067dd795b05587796be
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2024-05-19 13:14:57 -0500

    Route 18 trainers

--- maps/Route18.asm
+++ maps/Route18.asm
@@ -1,6 +1,4 @@
 	object_const_def
-;	const ROUTE_18_YOUNGSTER1
-;	const ROUTE_18_YOUNGSTER2
 
 Route18_MapScripts:
 	def_scene_scripts
@@ -20,67 +18,81 @@ Route18AlwaysOnBikeCallback:
 	clearflag ENGINE_ALWAYS_ON_BIKE
 	endcallback
 
-;TrainerBirdKeeperBoris:
-;	trainer BIRD_KEEPER, BORIS, EVENT_BEAT_BIRD_KEEPER_BORIS, BirdKeeperBorisSeenText, BirdKeeperBorisBeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext BirdKeeperBorisAfterBattleText
-;	waitbutton
-;	closetext
-;	end
+TrainerBirdKeeperJose:
+	trainer BIRD_KEEPER, JOSE1, EVENT_BEAT_BIRD_KEEPER_JOSE, BirdKeeperJoseSeenText, BirdKeeperJoseBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	jumptextfaceplayer BirdKeeperJoseAfterBattleText
 
-;TrainerBirdKeeperBob:
-;	trainer BIRD_KEEPER, BOB, EVENT_BEAT_BIRD_KEEPER_BOB, BirdKeeperBobSeenText, BirdKeeperBobBeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext BirdKeeperBobAfterBattleText
-;	waitbutton
-;	closetext
-;	end
+BirdKeeperJoseSeenText:
+	text "I always check"
+	line "every grassy area"
+	cont "for new #MON."
+	done
+
+BirdKeeperJoseBeatenText:
+	text "Tch!"
+	done
+
+BirdKeeperJoseAfterBattleText:
+	text "I wish I had a"
+	line "BIKE!"
+	done
+
+TrainerBirdKeeperBryan:
+	trainer BIRD_KEEPER, BRYAN, EVENT_BEAT_BIRD_KEEPER_BRYAN, BirdKeeperBryanSeenText, BirdKeeperBryanBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	jumptextfaceplayer BirdKeeperBryanAfterBattleText
+
+BirdKeeperBryanSeenText:
+	text "This is my turf!"
+	line "Get out of here!"
+	done
+
+BirdKeeperBryanBeatenText:
+	text "Darn!"
+	done
+
+BirdKeeperBryanAfterBattleText:
+	text "This is my fave"
+	line "#MON hunting"
+	cont "area!"
+	done
+
+TrainerBirdKeeperPeter:
+	trainer BIRD_KEEPER, PETER, EVENT_BEAT_BIRD_KEEPER_PETER, BirdKeeperPeterSeenText, BirdKeeperPeterBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	jumptextfaceplayer BirdKeeperPeterAfterBattleText
+
+BirdKeeperPeterSeenText:
+	text "Kurukkoo!"
+	line "How do you like"
+	cont "my bird call?"
+	done
+
+BirdKeeperPeterBeatenText:
+	text "I had to"
+	line "bug you!"
+	done
+
+BirdKeeperPeterAfterBattleText:
+	text "I also collect"
+	line "sea #MON on"
+	cont "weekends!"
+	done
+
+Route18CyclingRoadSign:
+	jumptext Route18CyclingRoadSignText
+Route18CyclingRoadSignText:
+	text "CYCLING ROAD"
+	line "No pedestrians"
+	cont "permitted!"
+	done
 
 Route18Sign:
 	jumptext Route18SignText
-
-BirdKeeperBorisSeenText:
-	text "If you're looking"
-	line "for #MON, you"
-
-	para "have to look in"
-	line "the tall grass."
-	done
-
-BirdKeeperBorisBeatenText:
-	text "Ayieee!"
-	done
-
-BirdKeeperBorisAfterBattleText:
-	text "Since you're so"
-	line "strong, it must be"
-	cont "fun to battle."
-	done
-
-BirdKeeperBobSeenText:
-	text "CYCLING ROAD is a"
-	line "quick shortcut to"
-	cont "CELADON."
-	done
-
-BirdKeeperBobBeatenText:
-	text "…Whew!"
-	done
-
-BirdKeeperBobAfterBattleText:
-	text "If you don't have"
-	line "a BICYCLE, you're"
-
-	para "not allowed to use"
-	line "the shortcut."
-	done
-
 Route18SignText:
 	text "ROUTE 18"
 
@@ -100,8 +112,10 @@ Route18_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event 35,  5, BGEVENT_READ, Route18CyclingRoadSign
 	bg_event 43,  7, BGEVENT_READ, Route18Sign
 
 	def_object_events
-;	object_event 37, 14, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperBoris, -1
-;	object_event 40, 11, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperBob, -1
+	object_event 36, 11, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperJose, -1
+	object_event 42, 13, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperBryan, -1
+	object_event 40, 15, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 4, TrainerBirdKeeperPeter, -1
