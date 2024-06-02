ce5174ae0c93d01df414d13aaf78b8676b5b54b3
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2024-06-02 14:27:49 -0500

    ice path tileset reorganization

--- maps/SeafoamIslands.asm
+++ maps/SeafoamIslands.asm
@@ -1,24 +1,66 @@
 	object_const_def
+	const SEAFOAM1F_BOULDER1
+	const SEAFOAM1F_BOULDER2
 
 SeafoamIslands_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_STONETABLE, SeafoamIslands1FSetUpStoneTableCallback
+
+SeafoamIslands1FSetUpStoneTableCallback:
+	usestonetable .StoneTable
+	endcallback
+
+.StoneTable:
+	stonetable 6, SEAFOAM1F_BOULDER1, .Boulder1
+	stonetable 7, SEAFOAM1F_BOULDER2, .Boulder2
+	db -1 ; end
+
+.Boulder1:
+	disappear SEAFOAM1F_BOULDER1
+	clearevent EVENT_SEAFOAM_B1F_BOULDER_1
+	sjump .Fall
+
+.Boulder2:
+	disappear SEAFOAM1F_BOULDER2
+	clearevent EVENT_SEAFOAM_B1F_BOULDER_2
+	sjump .Fall
+
+.Fall:
+	pause 30
+	playsound SFX_STRENGTH
+	earthquake 60
+	opentext
+	writetext SeafoamIslands1FBoulderFellText
+	waitbutton
+	closetext
+	end
+
+SeafoamIslands1FBoulderFellText:
+	text "The boulder fell"
+	line "through!"
+	done
+
+SeafoamIslands1FBoulder:
+	jumpstd StrengthBoulderScript
 
 SeafoamIslands_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 25,  3, SEAFOAM_ISLANDS_B1F, 1 ; 1
-	warp_event  7,  5, SEAFOAM_ISLANDS_B1F, 3 ; 2
-	warp_event 16,  7, SEAFOAM_ISLANDS_B1F, 1 ; 3 pit
-	warp_event 24,  7, SEAFOAM_ISLANDS_B1F, 1 ; 4 pit
-	warp_event  5, 15, ROUTE_20, 1            ; 5
-	warp_event 23, 17, SEAFOAM_ISLANDS_B1F, 9 ; 6
-	warp_event 25, 17, ROUTE_20, 2            ; 7
+	warp_event  5, 15, ROUTE_20, 1 ; 1 out
+	warp_event 25, 17, ROUTE_20, 2 ; 2 out
+	warp_event  7,  5, SEAFOAM_ISLANDS_B1F, 1 ; 3 down
+	warp_event 25,  3, SEAFOAM_ISLANDS_B1F, 2 ; 4 down
+	warp_event 23, 17, SEAFOAM_ISLANDS_B1F, 3 ; 5 down
+	warp_event 16,  7, SEAFOAM_ISLANDS_B1F, 10 ; 6 pit
+	warp_event 24,  7, SEAFOAM_ISLANDS_B1F, 11 ; 7 pit
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
+	object_event 14, 10, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslands1FBoulder, EVENT_SEAFOAM_1F_BOULDER_1
+	object_event 26,  6, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslands1FBoulder, EVENT_SEAFOAM_1F_BOULDER_2
--- maps/SeafoamIslandsB1F.asm
+++ maps/SeafoamIslandsB1F.asm
@@ -1,16 +1,49 @@
 	object_const_def
-;	const SEAFOAM_ISLANDS_B1F_BOULDER1
-;	const SEAFOAM_ISLANDS_B1F_BOULDER2
-;	const SEAFOAM_ISLANDS_B1F_BOULDER3
-;	const SEAFOAM_ISLANDS_B1F_BOULDER4
-;	const SEAFOAM_ISLANDS_B1F_BOULDER5
-;	const SEAFOAM_ISLANDS_B1F_POKE_BALL1
-;	const SEAFOAM_ISLANDS_B1F_POKE_BALL2
+	const SEAFOAMB1F_BOULDER1
+	const SEAFOAMB1F_BOULDER2
 
 SeafoamIslandsB1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_STONETABLE, SeafoamIslandsB1FSetUpStoneTableCallback
+
+SeafoamIslandsB1FSetUpStoneTableCallback:
+	usestonetable .StoneTable
+	endcallback
+
+.StoneTable:
+	stonetable 8, SEAFOAMB1F_BOULDER1, .Boulder1
+	stonetable 9, SEAFOAMB1F_BOULDER2, .Boulder2
+	db -1 ; end
+
+.Boulder1:
+	disappear SEAFOAMB1F_BOULDER1
+	clearevent EVENT_SEAFOAM_B2F_BOULDER_1
+	sjump .Fall
+
+.Boulder2:
+	disappear SEAFOAMB1F_BOULDER2
+	clearevent EVENT_SEAFOAM_B2F_BOULDER_2
+	sjump .Fall
+
+.Fall:
+	pause 30
+	playsound SFX_STRENGTH
+	earthquake 60
+	opentext
+	writetext SeafoamIslandsB1FBoulderFellText
+	waitbutton
+	closetext
+	end
+
+SeafoamIslandsB1FBoulderFellText:
+	text "The boulder fell"
+	line "through!"
+	done
+
+SeafoamIslandsB1FBoulder:
+	jumpstd StrengthBoulderScript
 
 ;SeafoamIslandsB1FHiddenIceHeal:
 ;	hiddenitem ICE_HEAL, EVENT_SEAFOAM_ISLANDS_B1F_HIDDEN_ICE_HEAL
@@ -28,15 +61,17 @@ SeafoamIslandsB1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 25,  3, SEAFOAM_ISLANDS, 1     ; 1
-	warp_event  5,  5, SEAFOAM_ISLANDS_B2F, 1 ; 2
-	warp_event  7,  7, SEAFOAM_ISLANDS, 2     ; 3
-	warp_event 28,  8, SEAFOAM_ISLANDS_B2F, 0 ; 4 pit
-	warp_event 13,  9, SEAFOAM_ISLANDS_B2F, 4 ; 5
-	warp_event 18,  9, SEAFOAM_ISLANDS_B2F, 0 ; 6 pit
-	warp_event 25, 13, SEAFOAM_ISLANDS_B2F, 6 ; 7
-	warp_event 19, 17, SEAFOAM_ISLANDS_B2F, 9 ; 8
-	warp_event 23, 17, SEAFOAM_ISLANDS, 6     ; 9
+	warp_event  7,  7, SEAFOAM_ISLANDS, 3 ; 1 up
+	warp_event 25,  3, SEAFOAM_ISLANDS, 4 ; 2 up
+	warp_event 23, 17, SEAFOAM_ISLANDS, 5 ; 3 up
+	warp_event  6,  5, SEAFOAM_ISLANDS_B2F, 1 ; 4 down
+	warp_event 13,  9, SEAFOAM_ISLANDS_B2F, 2 ; 5 down
+	warp_event 18, 17, SEAFOAM_ISLANDS_B2F, 3 ; 6 down
+	warp_event 25, 13, SEAFOAM_ISLANDS_B2F, 4 ; 7 down
+	warp_event 18,  9, SEAFOAM_ISLANDS_B2F, 10 ; 8 pit
+	warp_event 23,  8, SEAFOAM_ISLANDS_B2F, 11 ; 9 pit
+	warp_event 17,  8, SEAFOAM_ISLANDS, 6 ; 10 fall
+	warp_event 24,  7, SEAFOAM_ISLANDS, 7 ; 11 fall
 
 	def_coord_events
 
@@ -44,11 +79,7 @@ SeafoamIslandsB1F_MapEvents:
 ;	bg_event 14, 10, BGEVENT_ITEM, SeafoamIslandsB1FHiddenIceHeal
 
 	def_object_events
-;	object_event  1, 12, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
-;	object_event  6, 16, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
-;	object_event 32, 10, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
-;	object_event 31,  9, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
-;	object_event  4,  8, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
-;	object_event 30,  9, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, -1
+	object_event 17,  9, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, EVENT_SEAFOAM_B1F_BOULDER_1
+	object_event 24,  8, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SeafoamIslandsB1FBoulder, EVENT_SEAFOAM_B1F_BOULDER_2
 ;	object_event 21,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SeafoamIslandsB1FMaxElixer, EVENT_SEAFOAM_ISLANDS_B1F_MAX_ELIXER
 ;	object_event 11,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SeafoamIslandsB1FIceHeal, EVENT_SEAFOAM_ISLANDS_B1F_ICE_HEAL
