9dce6f43f7aa8c8caa7b1f17ed747c5718379032
Th3Element05 <sean.t.crawford@outlook.com>
2024-06-10 09:47:13 -0500

    Scripted PokemonMansion

--- maps/PokemonMansion1F.asm
+++ maps/PokemonMansion1F.asm
@@ -4,12 +4,100 @@ PokemonMansion1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-	
-;PokemonMansion1FUltraBall:
-;	itemball ULTRA_BALL
+	callback MAPCALLBACK_TILES, PokemonMansion1FDoorsCallback
 
-;PokemonMansion1FHiddenMaxElixer:
-;	hiddenitem MAX_ELIXER, EVENT_POKEMON_MANSION_1F_HIDDEN_MAX_ELIXER
+PokemonMansion1FDoorsCallback:
+	checkevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	iffalse .SwitchOff
+	changeblock 16, 6, $65 ; open door
+	changeblock 20, 16, $65 ; open door
+	changeblock 26, 26, $02 ; warp carpet
+	changeblock 24, 12, $39 ; close door
+.SwitchOff
+	endcallback
+
+PokemonMansion1FSecretSwitch:
+	opentext
+	writetext PokemonMansion1FSecretSwitchText
+	yesorno
+	iffalse .NoSwitch
+	writetext PokemonMansion1FPressSwitchText
+	waitbutton
+	closetext
+	playsound SFX_ENTER_DOOR
+	checkevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	iftrue .TurnOff
+	setevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	changeblock 16, 6, $65 ; open door
+	changeblock 20, 16, $65 ; open door
+	changeblock 26, 26, $02 ; warp carpet
+	changeblock 24, 12, $39 ; close door
+	end
+
+.TurnOff
+	clearevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	changeblock 16, 6, $39 ; close door
+	changeblock 20, 16, $39 ; close door
+	changeblock 26, 26, $39 ; close door
+	changeblock 24, 12, $65 ; open door
+	end
+
+.NoSwitch
+	writetext PokemonMansion1FNoSwitchText
+	waitbutton
+	closetext
+	end
+
+PokemonMansion1FSecretSwitchText:
+	text "A secret switch!"
+
+	para "Press it?"
+	done
+
+PokemonMansion1FPressSwitchText:
+	text "Who wouldn't?"
+	done
+
+PokemonMansion1FNoSwitchText:
+	text "Not quite yet!"
+	done
+
+TrainerScientistSilas:
+	trainer SCIENTIST, SILAS, EVENT_BEAT_SCIENTIST_SILAS, ScientistSilasSeenText, ScientistSilasBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext ScientistSilasAfterBattleText
+	waitbutton
+	closetext
+	end
+
+ScientistSilasSeenText:
+	text "Who are you? There"
+	line "shouldn't be"
+	cont "anyone here."
+	done
+
+ScientistSilasBeatenText:
+	text "Ouch!"
+	done
+
+ScientistSilasAfterBattleText:
+	text "A key? I don't"
+	line "know what you're"
+	cont "talking about."
+	done
+
+; itemballs
+PokemonMansion1FEscapeRope:
+	itemball ESCAPE_ROPE
+
+PokemonMansion1FCarbos:
+	itemball CARBOS
+
+; hidden items
+PokemonMansion1FHiddenMoonStone:
+	hiddenitem MOON_STONE, EVENT_POKEMON_MANSION_1F_HIDDEN_MOON_STONE
 
 PokemonMansion1F_MapEvents:
 	db 0, 0 ; filler
@@ -22,13 +110,14 @@ PokemonMansion1F_MapEvents:
 	warp_event  5, 10, POKEMON_MANSION_2F, 2  ; 5
 	warp_event 21, 22, POKEMON_MANSION_B1F, 1 ; 6
 	warp_event 16, 14, POKEMON_MANSION_1F, 7  ; 7
-	warp_event 19, 14, POKEMON_MANSION_1F, 8  ; 8
 
 	def_coord_events
 
 	def_bg_events
-;	bg_event 19, 18, BGEVENT_ITEM, PokemonMansion1FHiddenMaxElixer
+	bg_event  2,  5, BGEVENT_READ, PokemonMansion1FSecretSwitch
+	bg_event  8, 16, BGEVENT_ITEM, PokemonMansion1FHiddenMoonStone
 
 	def_object_events
-;	object_event 18,  5, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansion1FCalcium, EVENT_POKEMON_MANSION_1F_CALCIUM
-;	object_event  6,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansion1FUltraBall, EVENT_POKEMON_MANSION_1F_ULTRA_BALL
+	object_event 16, 17, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 3, TrainerScientistSilas, -1
+	object_event 14,  3, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansion1FEscapeRope, EVENT_POKEMON_MANSION_1F_ESCAPE_ROPE
+	object_event 18, 21, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansion1FCarbos, EVENT_POKEMON_MANSION_1F_CARBOS
--- maps/PokemonMansionB1F.asm
+++ maps/PokemonMansionB1F.asm
@@ -1,103 +1,130 @@
 	object_const_def
-;	const POKEMON_MANSION_B1F_BOULDER1
-;	const POKEMON_MANSION_B1F_BOULDER2
-;	const POKEMON_MANSION_B1F_BOULDER3
-;	const POKEMON_MANSION_B1F_MEW
-;	const POKEMON_MANSION_B1F_DOCUMENT
-;	const POKEMON_MANSION_B1F_POKE_BALL1
-;	const POKEMON_MANSION_B1F_POKE_BALL2
+	const POKEMONMANSIONB1F_BLAINE
+;	const POKEMONMANSIONB1F_MEW
 
 PokemonMansionB1F_MapScripts:
 	def_scene_scripts
-;	scene_script .DummyScene
+;	scene_script PokemonMansionB1FDisappearMew
 
 	def_callbacks
-;	callback MAPCALLBACK_TILES, .BouldersPokemonMansionB1F
-;	callback MAPCALLBACK_STONETABLE, .SetUpStoneTable
+	callback MAPCALLBACK_TILES, PokemonMansionB1FDoorsCallback
 
-;.DummyScene:
+;PokemonMansionB1FDisappearMew:
 ;	checkevent EVENT_FOUGHT_MEW
 ;	iftrue .DoNothing
-;	disappear POKEMON_MANSION_B1F_MEW
+;	disappear POKEMONMANSIONB1F_MEW
 ;.DoNothing
 ;	end
 
-;.BouldersPokemonMansionB1F:
-;	checkevent EVENT_POKEMON_MANSION_B1F_BOULDER_1
-;	iffalse .skip_boulder_1
-;	changeblock 26, 22, $85
-;	changeblock 24, 22, $6e
-;.skip_boulder_1
-;	checkevent EVENT_POKEMON_MANSION_B1F_BOULDER_2
-;	iffalse .skip_boulder_2
-;	changeblock 22, 12, $8f
-;.skip_boulder_2
-;	checkevent EVENT_POKEMON_MANSION_B1F_BOULDER_3
-;	iffalse .skip_boulder_3
-;	changeblock 4, 24, $90
-;	changeblock 6, 24, $67
-;.skip_boulder_3
-;	endcallback
-;
-;.SetUpStoneTable:
-;	usestonetable .StoneTable
-;	endcallback
-;
-;.StoneTable:
-;	stonetable 2, POKEMON_MANSION_B1F_BOULDER1, .Boulder1
-;	stonetable 3, POKEMON_MANSION_B1F_BOULDER2, .Boulder2
-;	stonetable 4, POKEMON_MANSION_B1F_BOULDER3, .Boulder3
-;	db -1 ; end
-;
-;.Boulder1:
-;	disappear POKEMON_MANSION_B1F_BOULDER1
-;	pause 30
-;	playsound SFX_STRENGTH
-;	earthquake 80
-;	changeblock 26, 22, $85
-;	changeblock 24, 22, $6e
-;	opentext
-;	writetext PokemonMansionB1FBoulderFilled
-;	waitbutton
-;	closetext
-;	setevent EVENT_POKEMON_MANSION_B1F_BOULDER_1
-;	end
-;
-;.Boulder2:
-;	disappear POKEMON_MANSION_B1F_BOULDER2
-;	pause 30
-;	playsound SFX_STRENGTH
-;	earthquake 80
-;	changeblock 22, 12, $8f
-;	opentext
-;	writetext PokemonMansionB1FBoulderFilled
-;	waitbutton
-;	closetext
-;	setevent EVENT_POKEMON_MANSION_B1F_BOULDER_2
-;	end
-;
-;.Boulder3:
-;	disappear POKEMON_MANSION_B1F_BOULDER3
-;	pause 30
-;	playsound SFX_STRENGTH
-;	earthquake 80
-;	changeblock 4, 24, $90
-;	changeblock 6, 24, $67
-;	opentext
-;	writetext PokemonMansionB1FBoulderFilled
-;	waitbutton
-;	closetext
-;	setevent EVENT_POKEMON_MANSION_B1F_BOULDER_3
-;	end
-;	
-;PokemonMansionB1FBoulder:
-;	jumpstd StrengthBoulderScript
-;
-;PokemonMansionB1FBoulderFilled:
-;	text "The boulder filled"
-;	line "the gap!"
-;	done
-;
+PokemonMansionB1FDoorsCallback:
+	checkevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	iffalse .SwitchOff
+	changeblock  8,  6, $66 ; open
+	changeblock 16, 16, $64 ; open
+	changeblock 26, 16, $39 ; closed
+	changeblock 12, 22, $3a ; closed
+.SwitchOff
+	endcallback
+
+PokemonMansionB1FSecretSwitch:
+	opentext
+	writetext PokemonMansionB1FSecretSwitchText
+	yesorno
+	iffalse .NoSwitch
+	writetext PokemonMansionB1FPressSwitchText
+	waitbutton
+	closetext
+	playsound SFX_ENTER_DOOR
+	checkevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	iftrue .TurnOff
+	setevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	changeblock  8,  6, $66 ; open
+	changeblock 16, 16, $64 ; open
+	changeblock 26, 16, $39 ; closed
+	changeblock 12, 22, $3a ; closed
+	reloadmappart
+	end
+
+.TurnOff
+	clearevent EVENT_POKEMON_MANSION_SECRET_SWITCH
+	changeblock  8,  6, $3a ; closed
+	changeblock 16, 16, $38 ; closed
+	changeblock 26, 16, $65 ; open
+	changeblock 12, 22, $66 ; open
+	reloadmappart
+	end
+
+.NoSwitch
+	writetext PokemonMansionB1FNoSwitchText
+	waitbutton
+	closetext
+	end
+
+PokemonMansionB1FSecretSwitchText:
+	text "A secret switch!"
+
+	para "Press it?"
+	done
+
+PokemonMansionB1FPressSwitchText:
+	text "Who wouldn't?"
+	done
+
+PokemonMansionB1FNoSwitchText:
+	text "Not quite yet!"
+	done
+
+PokemonMansionB1FBlaineScript:
+	faceplayer
+	opentext
+	writetext PokemonMansionB1FBlaineText
+	waitbutton
+	readvar VAR_FACING
+	ifnotequal DOWN, .Skip
+	applymovement POKEMONMANSIONB1F_BLAINE, PokemonMansion3FBlaineGoesAroundMovement
+.Skip
+	applymovement POKEMONMANSIONB1F_BLAINE, PokemonMansion3FBlaineLeavesMovement
+	disappear POKEMONMANSIONB1F_BLAINE
+	setevent EVENT_CINNABAR_GYM_LEADER_RETURNED
+	end
+
+PokemonMansionB1FBlaineText:
+	text "What is a child"
+	line "doing in a place"
+	cont "like this?"
+
+	para "..."
+
+	para "I see, you are a"
+	line "#MON trianer!"
+
+	para "I am BLAINE! I"
+	line "am the LEADER of"
+	cont "CINNABAR GYM!"
+
+	para "I came here to see"
+	line "what I could find"
+	cont "out about the dis-"
+	cont "aster that caused"
+	cont "the scientists to"
+	cont "abandon the lab."
+
+	para "But so much has"
+	line "been looted, there"
+	cont "isn't much left to"
+	cont "find..."
+
+	para "I imagine you want"
+	line "to challenge the"
+	cont "CINNABAR GYM?"
+
+	para "Very well, I will"
+	line "return to my GYM!"
+
+	para "You can challenge"
+	line "me there!"
+	done
+
 ;LabDocument:
 ;	opentext
 ;	writetext DocumentText
@@ -120,7 +147,7 @@ PokemonMansionB1F_MapScripts:
 ;	closetext
 ;	playsound SFX_FLASH
 ;	pause 10
-;	appear POKEMON_MANSION_B1F_MEW
+;	appear POKEMONMANSIONB1F_MEW
 ;	pause 10
 ;	opentext
 ;	writetext MewText
@@ -130,7 +157,7 @@ PokemonMansionB1F_MapScripts:
 ;	loadvar VAR_BATTLETYPE, BATTLETYPE_KANTO_LEGEND
 ;	loadwildmon MEW, 60
 ;	startbattle
-;	disappear POKEMON_MANSION_B1F_MEW
+;	disappear POKEMONMANSIONB1F_MEW
 ;	setevent EVENT_FOUGHT_MEW
 ;	reloadmapafterbattle
 ;	special CheckBattleCaughtResult
@@ -171,34 +198,121 @@ PokemonMansionB1F_MapScripts:
 ;	text "MEW!"
 ;	done
 ;
-;PokemonMansionB1FFullRestore:
-;	itemball FULL_RESTORE
-;
-;PokemonMansionB1FRareCandy:
-;	itemball RARE_CANDY
-;
-;PokemonMansion1FHiddenStarPiece:
-;	hiddenitem STAR_PIECE, EVENT_POKEMON_MANSION_B1F_HIDDEN_STAR_PIECE
-;
+TrainerScientistEzekiel:
+	trainer SCIENTIST, EZEKIEL, EVENT_BEAT_SCIENTIST_EZEKIEL, ScientistEzekielSeenText, ScientistEzekielBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext ScientistEzekielAfterBattleText
+	waitbutton
+	closetext
+	end
+
+ScientistEzekielSeenText:
+	text "This place is"
+	line "ideal for a lab."
+	done
+
+ScientistEzekielBeatenText:
+	text "What was"
+	line "that for?"
+	done
+
+ScientistEzekielAfterBattleText:
+	text "I like it here!"
+	line "It's conducive to"
+	cont "my studies!"
+	done
+
+TrainerBurglarCorey:
+	trainer BURGLAR, COREY, EVENT_BEAT_BURGLAR_COREY, BurglarCoreySeenText, BurglarCoreyBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext BurglarCoreyAfterBattleText
+	waitbutton
+	closetext
+	end
+
+BurglarCoreySeenText:
+	text "Uh-oh. Where am"
+	line "I now?"
+	done
+
+BurglarCoreyBeatenText:
+	text "Awooh!"
+	done
+
+BurglarCoreyAfterBattleText:
+	text "You can find stuff"
+	line "lying around."
+	done
+
+PokemonMansionB1FDiary:
+	jumptext PokemonMansionB1FDiaryText
+PokemonMansionB1FDiaryText:
+	text "Lab Notes:"
+	line "Sept. 1"
+	
+	para "MEWTWO is far too"
+	line "powerful."
+
+	para "We have failed to"
+	line "curb its vicious"
+	cont "tendencies..."
+	done
+
+PokemonMansion3FBlaineGoesAroundMovement:
+	step LEFT
+	step UP
+	step_end
+
+PokemonMansion3FBlaineLeavesMovement:
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step_end
+
+; itemballs
+PokemonMansionB1FRareCandy:
+	itemball RARE_CANDY
+
+PokemonMansionB1FTMSolarbeam:
+	itemball TM_SOLARBEAM
+
+PokemonMansionB1FFullRestore:
+	itemball FULL_RESTORE
+
+PokemonMansionB1FTMBlizzard:
+	itemball TM_BLIZZARD
+
+; hidden items
+PokemonMansionB1FHiddenRareCandy:
+	hiddenitem RARE_CANDY, EVENT_POKEMON_MANSION_B1F_HIDDEN_RARE_CANDY
+
 PokemonMansionB1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 23, 22, POKEMON_MANSION_1F, 5 ; 1
-;	warp_event 26, 23, POKEMON_MANSION_B1F, 1; boulder 3
-;	warp_event 22, 12, POKEMON_MANSION_B1F, 1; boulder 2
-;	warp_event  5, 24, POKEMON_MANSION_B1F, 1; boulder 1
+	warp_event 23, 22, POKEMON_MANSION_1F, 6 ; 1
 
 	def_coord_events
 
 	def_bg_events
-;	bg_event 10, 24, BGEVENT_ITEM, PokemonMansion1FHiddenStarPiece
+	bg_event 18, 25, BGEVENT_READ, PokemonMansionB1FSecretSwitch
+	bg_event 20,  3, BGEVENT_READ, PokemonMansionB1FSecretSwitch
+	bg_event  1,  9, BGEVENT_ITEM, PokemonMansionB1FHiddenRareCandy
 
 	def_object_events
-;	object_event 22, 23, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PokemonMansionB1FBoulder, EVENT_POKEMON_MANSION_B1F_BOULDER_1
-;	object_event 25,  5, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PokemonMansionB1FBoulder, EVENT_POKEMON_MANSION_B1F_BOULDER_2
-;	object_event 18, 25, SPRITE_BOULDER, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PokemonMansionB1FBoulder, EVENT_POKEMON_MANSION_B1F_BOULDER_3
-;	object_event  4,  4, SPRITE_MEW, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-;	object_event  4,  5, SPRITE_PAPER, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_SCRIPT, 0, LabDocument, -1
-;	object_event 14, 11, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FFullRestore, EVENT_POKEMON_MANSION_B1F_FULL_RESTORE
-;	object_event  4, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FRareCandy, EVENT_POKEMON_MANSION_B1F_RARE_CANDY
+	object_event  5, 13, SPRITE_BLAINE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PokemonMansionB1FBlaineScript, EVENT_CINNABAR_GYM_LEADER_RETURNED
+;	object_event  3,  4, SPRITE_MEW, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+;	object_event  4,  5, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_ROCK, OBJECTTYPE_SCRIPT, 0, LabDocument, -1
+	object_event 27, 11, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 3, TrainerScientistEzekiel, -1
+	object_event 16, 23, SPRITE_PHARMACIST, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 0, TrainerBurglarCorey, -1
+	object_event 10,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FRareCandy, EVENT_POKEMON_MANSION_B1F_RARE_CANDY
+	object_event  5,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FTMSolarbeam, EVENT_POKEMON_MANSION_B1F_TM_SOLARBEAM
+	object_event  1, 22, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FFullRestore, EVENT_POKEMON_MANSION_B1F_FULL_RESTORE
+	object_event 19, 25, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, PokemonMansionB1FTMBlizzard, EVENT_POKEMON_MANSION_B1F_TM_BLIZZARD
+	object_event 16, 20, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PokemonMansionB1FDiary, -1
