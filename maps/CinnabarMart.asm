bc1748f99398fea43de46b9a401c86320b50f6c2
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2024-06-10 09:48:49 -0500

    fixed Cinnabar warps

--- maps/CinnabarMart.asm
+++ maps/CinnabarMart.asm
@@ -49,8 +49,8 @@ CinnabarMart_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  3,  7, CINNABAR_ISLAND, 5
-	warp_event  4,  7, CINNABAR_ISLAND, 5
+	warp_event  3,  7, CINNABAR_ISLAND, 4
+	warp_event  4,  7, CINNABAR_ISLAND, 4
 
 	def_coord_events
 
