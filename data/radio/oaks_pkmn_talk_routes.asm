5ecd8c28e7a9e3b6196241e4601ce99089061c4b
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2024-06-25 09:19:19 -0500

    pkmn talk about kanto not johto

--- data/radio/oaks_pkmn_talk_routes.asm
+++ data/radio/oaks_pkmn_talk_routes.asm
@@ -1,19 +1,35 @@
 ; Oak's Pokémon Talk will list wild Pokémon on these maps.
 
 OaksPKMNTalkRoutes:
-	map_id ROUTE_29
-	map_id ROUTE_46
-	map_id ROUTE_30
-	map_id ROUTE_32
-	map_id ROUTE_34
-	map_id ROUTE_35
-	map_id ROUTE_37
-	map_id ROUTE_38
-	map_id ROUTE_39
-	map_id ROUTE_42
-	map_id ROUTE_43
-	map_id ROUTE_44
-	map_id ROUTE_45
-	map_id ROUTE_36
-	map_id ROUTE_31
+	map_id ROUTE_1
+	map_id ROUTE_22
+	map_id ROUTE_2_SOUTH
+	map_id ROUTE_3
+	map_id ROUTE_4
+	map_id ROUTE_24
+	map_id ROUTE_5
+	map_id ROUTE_6
+	map_id ROUTE_8
+	map_id ROUTE_10_NORTH
+	map_id ROUTE_11
+	map_id ROUTE_13
+	map_id ROUTE_14
+	map_id ROUTE_16
+	map_id ROUTE_17
 .End
+
+;	map_id ROUTE_29
+;	map_id ROUTE_46
+;	map_id ROUTE_30
+;	map_id ROUTE_32
+;	map_id ROUTE_34
+;	map_id ROUTE_35
+;	map_id ROUTE_37
+;	map_id ROUTE_38
+;	map_id ROUTE_39
+;	map_id ROUTE_42
+;	map_id ROUTE_43
+;	map_id ROUTE_44
+;	map_id ROUTE_45
+;	map_id ROUTE_36
+;	map_id ROUTE_31
