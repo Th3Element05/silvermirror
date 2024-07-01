f1f8b753a1906668f76904e807bb419c4882c545
Th3Element05 <sean.t.crawford@outlook.com>
2024-06-30 21:28:42 -0500

    scripted

--- maps/CopycatsHouse1F.asm
+++ maps/CopycatsHouse1F.asm
@@ -1,74 +1,46 @@
 	object_const_def
-;	const COPYCATSHOUSE1F_POKEFAN_M
-;	const COPYCATSHOUSE1F_POKEFAN_F
-;	const COPYCATSHOUSE1F_CLEFAIRY
 
 CopycatsHouse1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-;CopycatsHouse1FPokefanMScript:
-;	jumptextfaceplayer CopycatsHouse1FPokefanMText
+CopycatsHouse1FPokefanMScript:
+	jumptextfaceplayer CopycatsHouse1FPokefanMText
 
-;CopycatsHouse1FPokefanFScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_RETURNED_MACHINE_PART
-;	iftrue .ReturnedMachinePart
-;	writetext CopycatsHouse1FPokefanFText
-;	waitbutton
-;	closetext
-;	end
-;
-;.ReturnedMachinePart:
-;	writetext CopycatsHouse1FPokefanFText_ReturnedMachinePart
-;	waitbutton
-;	closetext
-;	end
+CopycatsHouse1FPokefanFScript:
+	jumptextfaceplayer CopycatsHouse1FPokefanFText
 
-;CopycatsHouse1FBlisseyScript:
-;	opentext
-;	writetext CopycatsHouse1FBlisseyText
-;	cry BLISSEY
-;	waitbutton
-;	closetext
-;	end
+CopycatsHouse1FChanseyScript:
+	faceplayer
+	opentext
+	cry CHANSEY
+	writetext CopycatsHouse1FChanseyText
+	waitbutton
+	closetext
+	end
 
-;CopycatsHouse1FPokefanMText:
-;	text "My daughter likes"
-;	line "to mimic people."
-;
-;	para "Her mimicry has"
-;	line "earned her the"
-;
-;	para "nickname COPYCAT"
-;	line "around here."
-;	done
+CopycatsHouse1FPokefanMText:
+	text "My daughter likes"
+	line "to mimic people."
 
-;CopycatsHouse1FPokefanFText:
-;	text "My daughter is so"
-;	line "self-centered…"
-;
-;	para "She only has a few"
-;	line "friends."
-;	done
+	para "Her mimicry has"
+	line "earned her the"
+	cont "nickname COPYCAT"
+	cont "around here."
+	done
 
-;CopycatsHouse1FPokefanFText_ReturnedMachinePart:
-;	text "She recently lost"
-;	line "the # DOLL that"
-;
-;	para "a boy gave her"
-;	line "three years ago."
-;
-;	para "Ever since then,"
-;	line "she's gotten even"
-;	cont "better at mimicry…"
-;	done
+CopycatsHouse1FPokefanFText:
+	text "My daughter is so"
+	line "self-centered…"
 
-;CopycatsHouse1FBlisseyText:
-;	text "BLISSEY: Bliisii!"
-;	done
+	para "She only has a few"
+	line "friends."
+	done
+
+CopycatsHouse1FChanseyText:
+	text "CHANSEY: Chan-si!"
+	done
 
 CopycatsHouse1F_MapEvents:
 	db 0, 0 ; filler
@@ -76,13 +48,13 @@ CopycatsHouse1F_MapEvents:
 	def_warp_events
 	warp_event  2,  7, SAFFRON_CITY, 7
 	warp_event  3,  7, SAFFRON_CITY, 7
-	warp_event  2,  0, COPYCATS_HOUSE_2F, 1
+	warp_event  7,  0, COPYCATS_HOUSE_2F, 1
 
 	def_coord_events
 
 	def_bg_events
 
 	def_object_events
-;	object_event  2,  3, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FPokefanMScript, -1
-;	object_event  5,  4, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FPokefanFScript, -1
-;	object_event  6,  6, SPRITE_BLISSEY, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FBlisseyScript, -1
+	object_event  5,  4, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FPokefanMScript, -1
+	object_event  2,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FPokefanFScript, -1
+	object_event  1,  5, SPRITE_CHANSEY_MOVE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_SCRIPT, 0, CopycatsHouse1FChanseyScript, -1
