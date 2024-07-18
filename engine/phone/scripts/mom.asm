8e745bffbf6cde7d82bee2b397c1481a9aab2d86
Th3Element05 <sean.t.crawford@outlook.com>
2024-07-18 08:21:35 -0500

    Mostly configured special calls

--- engine/phone/scripts/mom.asm
+++ engine/phone/scripts/mom.asm
@@ -27,33 +27,39 @@ MomPhoneLandmark:
 
 MomPhoneInTown:
 	readvar VAR_MAPGROUP
-	ifequal GROUP_NEW_BARK_TOWN, .newbark
-	ifequal GROUP_CHERRYGROVE_CITY, .cherrygrove
-	ifequal GROUP_VIOLET_CITY, .violet
-	ifequal GROUP_AZALEA_TOWN, .azalea
-	ifequal GROUP_GOLDENROD_CITY, .goldenrod
+;	ifequal GROUP_NEW_BARK_TOWN, .newbark
+;	ifequal GROUP_CHERRYGROVE_CITY, .cherrygrove
+;	ifequal GROUP_VIOLET_CITY, .violet
+;	ifequal GROUP_AZALEA_TOWN, .azalea
+;	ifequal GROUP_GOLDENROD_CITY, .goldenrod
+;	farwritetext MomPhoneGenericAreaText
+	ifequal GROUP_PALLET_TOWN, .pallet
+	ifequal GROUP_VIRIDIAN_CITY, .viridian
+	ifequal GROUP_LAVENDER_TOWN, .lavender
+	ifequal GROUP_FUCHSIA_CITY, .fuchsia
+	ifequal GROUP_SAFFRON_CITY, .saffron
 	farwritetext MomPhoneGenericAreaText
 	promptbutton
 	sjump MomSavingMoney
 
-.newbark
-	farwritetext MomPhoneNewBarkText
+.pallet ;.newbark
+	farwritetext MomPhonePalletText
 	promptbutton
 	sjump MomSavingMoney
 
-.cherrygrove
-	farwritetext MomPhoneCherrygroveText
+.viridian ;.cherrygrove
+	farwritetext MomPhoneViridianText
 	promptbutton
 	sjump MomSavingMoney
 
-.violet
-	getlandmarkname STRING_BUFFER_4, LANDMARK_SPROUT_TOWER
+.lavender ;.violet
+	getlandmarkname STRING_BUFFER_4, LANDMARK_POKEMON_TOWER ;LANDMARK_SPROUT_TOWER
 	sjump MomPhoneLandmark
-.azalea
-	getlandmarkname STRING_BUFFER_4, LANDMARK_SLOWPOKE_WELL
+.fuchsia ;.azalea
+	getlandmarkname STRING_BUFFER_4, LANDMARK_SAFARI_ZONE ;LANDMARK_SLOWPOKE_WELL
 	sjump MomPhoneLandmark
-.goldenrod
-	getlandmarkname STRING_BUFFER_4, LANDMARK_RADIO_TOWER
+.saffron ;.goldenrod
+	getlandmarkname STRING_BUFFER_4, LANDMARK_SILPH_CO ;LANDMARK_RADIO_TOWER
 	sjump MomPhoneLandmark
 
 MomPhoneOnRoute:
