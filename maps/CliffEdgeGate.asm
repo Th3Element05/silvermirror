4d80660d5b88795dcb18cc5fb2ca81d5d0fb29c7
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-10-19 23:50:12 -0500

    commented out everything, should make sure these maps are removed

--- maps/CliffEdgeGate.asm
+++ maps/CliffEdgeGate.asm
@@ -1,26 +1,26 @@
 	object_const_def
-	const CLIFFEDGEGATE_RECEPTIONIST
+;	const CLIFFEDGEGATE_RECEPTIONIST
 
 CliffEdgeGate_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-CliffEdgeGateReceptionistScript:
-	jumptextfaceplayer CliffEdgeGateReceptionistText
-
-CliffEdgeGateHiddenBigPearl:
-	hiddenitem BIG_PEARL, EVENT_CLIFF_EDGE_GATE_HIDDEN_BIG_PEARL
-
-CliffEdgeGateReceptionistText:
-	text "The SAFARI ZONE"
-	line "is this way!"
-
-	para "It's a popular"
-	line "attraction for"
-	cont "tourists and"
-	cont "trainers alike!"
-	done
+;CliffEdgeGateReceptionistScript:
+;	jumptextfaceplayer CliffEdgeGateReceptionistText
+;
+;CliffEdgeGateHiddenBigPearl:
+;	hiddenitem BIG_PEARL, EVENT_CLIFF_EDGE_GATE_HIDDEN_BIG_PEARL
+;
+;CliffEdgeGateReceptionistText:
+;	text "The SAFARI ZONE"
+;	line "is this way!"
+;
+;	para "It's a popular"
+;	line "attraction for"
+;	cont "tourists and"
+;	cont "trainers alike!"
+;	done
 
 CliffEdgeGate_MapEvents:
 	db 0, 0 ; filler
@@ -32,7 +32,7 @@ CliffEdgeGate_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 17,  6, BGEVENT_ITEM, CliffEdgeGateHiddenBigPearl
+;	bg_event 17,  6, BGEVENT_ITEM, CliffEdgeGateHiddenBigPearl
 
 	def_object_events
-	object_event 11, 16, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, PERSONTYPE_SCRIPT, 0, CliffEdgeGateReceptionistScript, -1
+;	object_event 11, 16, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, PERSONTYPE_SCRIPT, 0, CliffEdgeGateReceptionistScript, -1
