ea3f1af39514d5e8e5625a7168e3732da2a505a5
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-10-31 14:19:46 -0500

    tin tower entrance scripting

--- maps/WiseTriosRoom.asm
+++ maps/WiseTriosRoom.asm
@@ -1,184 +1,88 @@
 	object_const_def
-;	const WISETRIOSROOM_SAGE1
-;	const WISETRIOSROOM_SAGE2
-;	const WISETRIOSROOM_SAGE3
-;	const WISETRIOSROOM_SAGE4
-;	const WISETRIOSROOM_SAGE5
-;	const WISETRIOSROOM_SAGE6
+	const WISETRIOSROOM_SAGE1
+	const WISETRIOSROOM_SAGE2
+	const WISETRIOSROOM_SAGEKOJI
 
 WiseTriosRoom_MapScripts:
 	def_scene_scripts
-;	scene_script .DummyScene0 ; SCENE_DEFAULT
-;	scene_script .DummyScene1 ; SCENE_FINISHED
 
 	def_callbacks
-;	callback MAPCALLBACK_OBJECTS, .WiseTrioCallback
 
-;.DummyScene0:
-;	end
-;
-;.DummyScene1:
-;	end
+WiseTriosRoomSage1Script:
+	jumptextfaceplayer WiseTriosRoomSagesSpeech2Text
+WiseTriosRoomSagesSpeech2Text:
+	text "You possess the"
+	line "FOG BADGE."
 
-;.WiseTrioCallback:
-;	checkevent EVENT_FOUGHT_SUICUNE
-;	iftrue .NoWiseTrio
-;	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
-;	iftrue .WiseTrio2
-;	checkitem CLEAR_BELL
-;	iftrue .WiseTrio2
-;	clearevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	setevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
-;	endcallback
-;
-;.WiseTrio2:
-;	setevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	clearevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
-;	endcallback
-;
-;.NoWiseTrio:
-;	setevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	setevent EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
-;	endcallback
-;
-;WiseTriosRoomSage1Script:
-;	jumptextfaceplayer WiseTriosRoomSage1Text
-;
+	para "You may enter the"
+	line "TIN TOWER."
+
+	para "But, please be"
+	line "respectful."
+	done
+
 ;WiseTriosRoomSage2Script:
-;	jumptextfaceplayer WiseTriosRoomSage2Text
+;	jumptextfaceplayer WiseTriosRoomSagesSpeech1Text
+;WiseTriosRoomSagesSpeech1Text:
+;	text "The BURNED TOWER…"
 ;
+;	para "The #MON that"
+;	line "died in that fire"
+;	cont "were resurrected."
+;
+;	para "They became"
+;	line "SUICUNE, ENTEI,"
+;	cont "and RAIKOU."
+;	done
+
 ;WiseTriosRoomSage3Script:
-;	jumptextfaceplayer WiseTriosRoomSage3Text
+;	jumptextfaceplayer WiseTriosRoomSagesSpeech3Text
+;WiseTriosRoomSagesSpeech3Text:
+;	text "A rainbow-winged"
+;	line "#MON used to"
+;	cont "roost at the peak"
+;	cont "of TIN TOWER."
 ;
-;WiseTriosRoom_CannotEnterTinTowerScript:
-;	turnobject WISETRIOSROOM_SAGE3, UP
-;	turnobject PLAYER, DOWN
-;	showemote EMOTE_SHOCK, WISETRIOSROOM_SAGE3, 20
-;	follow PLAYER, WISETRIOSROOM_SAGE3
-;	applymovement PLAYER, MovementData_0x98622
-;	stopfollow
-;	turnobject PLAYER, RIGHT
-;	opentext
-;	writetext UnknownText_0x98712
-;	waitbutton
-;	closetext
-;	applymovement WISETRIOSROOM_SAGE3, MovementData_0x98625
-;	turnobject WISETRIOSROOM_SAGE3, LEFT
-;	end
-;
-;TrainerSageGaku:
-;	trainer SAGE, GAKU, EVENT_BEAT_SAGE_GAKU, SageGakuSeenText, SageGakuBeatenText, 0, .Script
-;
-;.Script:
-;	opentext
-;	writetext UnknownText_0x98938
-;	waitbutton
-;	closetext
-;	end
-;
-;TrainerSageMasa:
-;	trainer SAGE, MASA, EVENT_BEAT_SAGE_MASA, SageMasaSeenText, SageMasaBeatenText, 0, .Script
-;
-;.Script:
-;	opentext
-;	writetext UnknownText_0x98a35
-;	waitbutton
-;	closetext
-;	end
-;
-;TrainerSageKoji:
-;	trainer SAGE, KOJI, EVENT_BEAT_SAGE_KOJI, SageKojiSeenText, SageKojiBeatenText, 0, .Script
-;
-;.Script:
-;	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
-;	iftrue .KojiAllowsPassage
-;	pause 10
-;	showemote EMOTE_SHOCK, WISETRIOSROOM_SAGE6, 20
-;	opentext
-;	writetext UnknownText_0x98c6c
-;	promptbutton
-;	writetext UnknownText_0x98cac
-;	waitbutton
-;	closetext
-;	applymovement WISETRIOSROOM_SAGE6, MovementData_0x98628
-;	turnobject WISETRIOSROOM_SAGE6, UP
-;	setevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
-;	setscene SCENE_FINISHED
-;	end
-;
-;.KojiAllowsPassage:
-;	opentext
-;	writetext UnknownText_0x98db5
-;	waitbutton
-;	closetext
-;	end
-;
-;MovementData_0x98622:
-;	step LEFT
-;	step LEFT
-;	step_end
-;
-;MovementData_0x98625:
-;	step RIGHT
-;	step DOWN
-;	step_end
-;
-;MovementData_0x98628:
-;	step RIGHT
-;	step DOWN
-;	step_end
-;
-;WiseTriosRoomSage1Text:
-;	text "Astounding…"
-;
-;	para "SUICUNE, ENTEI and"
-;	line "RAIKOU have arisen"
-;	cont "from their sleep…"
-;
-;	para "Is the legend"
-;	line "coming true?"
+;	para "It flew off after"
+;	line "the BRASS TOWER"
+;	cont "burned down."
 ;	done
-;
-;WiseTriosRoomSage2Text:
-;	text "We train at the"
-;	line "BURNED TOWER, but"
-;
-;	para "we've never heard"
-;	line "of a hole opening"
-;
-;	para "up there before."
-;	line "It must have been"
-;
-;	para "deliberately made"
-;	line "by someone."
-;	done
-;
-;UnknownText_0x98712:
-;	text "TIN TOWER may be"
-;	line "entered by those"
-;
-;	para "bearing ECRUTEAK's"
-;	line "GYM BADGE."
-;
-;	para "However, now that"
-;	line "SUICUNE, RAIKOU"
-;
-;	para "and ENTEI have"
-;	line "arisen, I ask you"
-;
-;	para "to refrain from"
-;	line "entering!"
-;	done
-;
-;WiseTriosRoomSage3Text:
-;	text "We, the WISE TRIO,"
-;	line "are the protectors"
-;
-;	para "of the legendary"
-;	line "#MON."
-;	done
-;
-;SageGakuSeenText:
+
+TrainerSageGaku:
+	trainer SAGE, GAKU, EVENT_BEAT_SAGE_GAKU, SageGakuSeenText, SageGakuBeatenText, 0, .Script
+.Script:
+	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
+	iftrue .GakuAllowsPassage
+	opentext
+	writetext SageGakuAfterBattleText
+	waitbutton
+	closetext
+	end
+
+.GakuAllowsPassage:
+	jumptextfaceplayer SageKojiAfterBattleFinalText
+
+SageGakuSeenText:
+	text "A long time ago"
+	line "a rainbow-winged"
+	cont "#MON made its"
+	cont "roost at the top"
+	cont "of TIN TOWER."
+
+	para "That #MON has"
+	line "returned!"
+
+	para "We, the WISE TRIO,"
+	line "are the protectors"
+	cont "of this legendary"
+	cont "#MON."
+
+	para "We shall test your"
+	line "your worthiness to"
+	cont "climb TIN TOWER"
+	cont "to meet it!"
+	done
+
 ;	text "Legend has it that"
 ;	line "upon the emergence"
 ;
@@ -207,18 +111,26 @@ WiseTriosRoom_MapScripts:
 ;	para "worthiness to go"
 ;	line "inside!"
 ;	done
-;
-;SageGakuBeatenText:
-;	text "Stronger than we"
-;	line "thought? Perhaps…"
-;	done
-;
-;UnknownText_0x98938:
-;	text "Ah, so it is you"
-;	line "who claim to have"
-;
-;	para "seen SUICUNE,"
-;	line "ENTEI and RAIKOU"
+
+SageGakuBeatenText:
+	text "Stronger than we"
+	line "thought? Perhaps…"
+	done
+
+SageGakuAfterBattleText:
+	text "The rainbow-winged"
+	line "#MON has not"
+	cont "been seen for many"
+	cont "years."
+
+	para "I never thought"
+	line "it would return"
+	cont "in my lifetime."
+	done
+
+;	text "You claim to have"
+;	cont "seen SUICUNE,"
+;	cont "ENTEI and RAIKOU"
 ;	cont "while they slept?"
 ;
 ;	para "Unbelievable!"
@@ -227,120 +139,161 @@ WiseTriosRoom_MapScripts:
 ;	line "they can't be seen"
 ;	cont "while they sleep…"
 ;	done
-;
-;SageMasaSeenText:
-;	text "Can you be trusted"
-;	line "with the truth?"
-;
-;	para "I must ascertain"
-;	line "your worthiness."
-;	done
-;
-;SageMasaBeatenText:
-;	text "…I will tell you"
-;	line "the truth…"
-;	done
-;
-;UnknownText_0x98a35:
-;	text "In the past, there"
-;	line "were two nine-tier"
-;	cont "towers here."
-;
-;	para "The BRASS TOWER,"
-;	line "which was said to"
-;
-;	para "awaken #MON,"
-;	line "and the TIN TOWER,"
-;
-;	para "where #MON were"
-;	line "said to rest."
-;
-;	para "The view from the"
-;	line "tops of the TOWERS"
-;
-;	para "must have been"
-;	line "magnificent."
-;
-;	para "At the time, an"
-;	line "immense, silver-"
-;
-;	para "colored #MON"
-;	line "was said to make"
-;
-;	para "its roost atop the"
-;	line "BRASS TOWER."
-;
-;	para "However…"
-;
-;	para "About 150 years"
-;	line "ago, a lightning"
-;
-;	para "bolt struck one of"
-;	line "the TOWERS."
-;
-;	para "It was engulfed in"
-;	line "flames that raged"
-;	cont "for three days."
-;
-;	para "A sudden downpour"
-;	line "finally put out"
-;	cont "the blaze."
-;
-;	para "And that is how"
-;	line "the BURNED TOWER"
-;	cont "came to be."
-;	done
-;
-;SageKojiSeenText:
-;	text "Let me see your"
-;	line "power!"
-;	done
-;
-;SageKojiBeatenText:
-;	text "Too strong!"
-;	line "Why?"
-;	done
-;
-;UnknownText_0x98c6c:
-;	text "You… Are you the"
-;	line "trainer who is"
-;
-;	para "awaited by the"
-;	line "legendary #MON?"
-;	done
-;
-;UnknownText_0x98cac:
-;	text "I see…"
-;
-;	para "We, the WISE TRIO,"
-;	line "have been given "
-;
-;	para "the responsibility"
-;	line "of protecting the"
-;
-;	para "legendary #MON."
-;	line "We are to allow"
-;
-;	para "passage only to"
-;	line "those people who"
-;
-;	para "possess the power"
-;	line "and soul of truth."
-;
-;	para "Please, do go on"
-;	line "and enter the TIN"
-;	cont "TOWER ahead."
-;
-;	para "SUICUNE will put"
-;	line "you to the test."
-;	done
-;
-;UnknownText_0x98db5:
-;	text "Please, do go on."
-;
-;	para "SUICUNE will put"
-;	line "you to the test."
-;	done
+
+TrainerSageMasa:
+	trainer SAGE, MASA, EVENT_BEAT_SAGE_MASA, SageMasaSeenText, SageMasaBeatenText, 0, .Script
+.Script:
+	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
+	iftrue .MasaAllowsPassage
+	opentext
+	writetext SageMasaAfterBattleText
+	waitbutton
+	closetext
+	end
+
+.MasaAllowsPassage:
+	jumptextfaceplayer SageKojiAfterBattleFinalText
+
+SageMasaSeenText:
+	text "I must ascertain"
+	line "your worthiness."
+	done
+
+SageMasaBeatenText:
+	text "…I will tell you"
+	line "the history of"
+	cont "the BURNED TOWER…"
+	done
+
+SageMasaAfterBattleText:
+	text "In the past, there"
+	line "were two nine-tier"
+	cont "towers here."
+
+	para "The BRASS TOWER,"
+	line "which was said to"
+	cont "awaken #MON,"
+	cont "and the TIN TOWER,"
+	cont "where #MON were"
+	cont "said to rest."
+
+	para "The view from the"
+	line "tops of the TOWERS"
+	cont "must have been"
+	cont "magnificent."
+
+	para "However…"
+
+	para "About 150 years"
+	line "ago, a lightning"
+	cont "bolt struck the"
+	cont "BRASS TOWER."
+
+	para "It was engulfed in"
+	line "flames that raged"
+	cont "for days, and"
+	cont "three #MON died"
+	cont "in the blaze."
+
+	para "A sudden downpour"
+	line "finally put out"
+	cont "the flames."
+
+	para "And that is how"
+	line "the BURNED TOWER"
+	cont "came to be."
+	done
+
+TrainerSageKoji:
+	trainer SAGE, KOJI, EVENT_BEAT_SAGE_KOJI, SageKojiSeenText, SageKojiBeatenText, 0, .Script
+.Script:
+	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
+	iftrue .KojiAllowsPassage
+	pause 10
+	showemote EMOTE_SHOCK, WISETRIOSROOM_SAGEKOJI, 20
+	opentext
+	writetext SageKojiAfterBattleSpeechText
+	promptbutton
+	writetext SageKojiAfterBattleFinalText
+	waitbutton
+	closetext
+	applymovement WISETRIOSROOM_SAGEKOJI, WiseTriosRoomSageAllowsPassageMovement
+	turnobject WISETRIOSROOM_SAGEKOJI, UP
+	setevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
+	end
+
+.KojiAllowsPassage:
+	jumptextfaceplayer SageKojiAfterBattleFinalText
+
+SageKojiSeenText:
+	text "Let me see your"
+	line "power!"
+	done
+
+SageKojiBeatenText:
+	text "…I will tell you"
+	line "the history of"
+	cont "these TOWERS…"
+	done
+
+SageKojiAfterBattleSpeechText:
+	para "A long time ago,"
+	line "a rainbow-winged"
+	cont "#MON was said"
+	line "to make its roost"
+	cont "at the top of the"
+	line "TIN TOWER."
+
+	para "At the same time,"
+	line "a huge silver-"
+	cont "colored #MON"
+	line "was said to roost"
+	cont "at the top of the"
+	line "BRASS TOWER."
+
+	para "However…"
+
+	para "A terrible fire"
+	line "burned the BRASS"
+	cont "TOWER about 150"
+	cont "years ago."
+
+	para "Without a roost,"
+	line "the silver-colored"
+	cont "#MON left."
+
+	para "Soon after, the"
+	line "rainbow-winged"
+	cont "#MON of the TIN"
+	cont "TOWER flew away."
+
+	para "We, the WISE TRIO,"
+	line "have been given "
+	cont "the responsibility"
+	cont "of protecting the"
+	cont "legendary #MON."
+
+	para "We are to allow"
+	cont "passage only to"
+	cont "those people who"
+	cont "possess the power"
+	cont "and soul of truth."
+	done
+
+SageKojiAfterBattleFinalText:
+	text "Please, go on,"
+	line "and enter the TIN"
+	cont "TOWER ahead."
+	done
+
+WiseTriosRoomSageAllowsPassageMovement:
+	step DOWN
+	turn_head UP
+	step_end
+;WiseTriosRoomSageAllowsPassageMovement:
+;	step RIGHT
+;	step DOWN
+;	step_end
 
 WiseTriosRoom_MapEvents:
 	db 0, 0 ; filler
@@ -351,14 +304,13 @@ WiseTriosRoom_MapEvents:
 	warp_event  1,  4, ECRUTEAK_TIN_TOWER_ENTRANCE, 5
 
 	def_coord_events
-;	coord_event  7,  4, SCENE_DEFAULT, WiseTriosRoom_CannotEnterTinTowerScript
 
 	def_bg_events
 
 	def_object_events
-;	object_event  6,  2, SPRITE_SAGE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage1Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	object_event  6,  7, SPRITE_SAGE, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage2Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	object_event  7,  5, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage3Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
-;	object_event  4,  2, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 2, TrainerSageGaku, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
-;	object_event  4,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 2, TrainerSageMasa, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
-;	object_event  6,  4, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 2, TrainerSageKoji, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
+	object_event  4,  2, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 2, TrainerSageGaku, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
+	object_event  4,  6, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 2, TrainerSageMasa, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
+	object_event  6,  4, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_PURPLE, OBJECTTYPE_TRAINER, 2, TrainerSageKoji, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_2
+	object_event  6,  2, SPRITE_SAGE, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage1Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
+	object_event  6,  7, SPRITE_SAGE, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage1Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
+	object_event  5,  5, SPRITE_SAGE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_PURPLE, OBJECTTYPE_SCRIPT, 0, WiseTriosRoomSage1Script, EVENT_WISE_TRIOS_ROOM_WISE_TRIO_1
