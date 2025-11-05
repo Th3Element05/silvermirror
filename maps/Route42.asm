462e607e1940d1821ff72b644483c09bb16552c4
Th3Element05 <sean.t.crawford@outlook.com>
2025-11-04 21:36:26 -0600

    minor map tweaks

--- maps/Route42.asm
+++ maps/Route42.asm
@@ -38,7 +38,7 @@ Route42BouldersCallback:
 	checkevent EVENT_ROUTE_42_BOULDERS_CLEARED
 	iftrue .End
 	changeblock 50, 6, $0a ; boulder
-;	changeblock 52, 4, $0a ; boulder
+	changeblock 52, 4, $0a ; boulder
 ;	changeblock 52, 6, $0a ; boulder
 	changeblock 52, 8, $0a ; boulder
 .End
@@ -174,213 +174,98 @@ Route42PrimeapeText:
 	line "Prruh! Priii!"
 	done
 
-;TrainerFisherTully:
-;	trainer FISHER, TULLY1, EVENT_BEAT_FISHER_TULLY, FisherTullySeenText, FisherTullyBeatenText, 0, .Script
-;.Script:
-;	loadvar VAR_CALLERID, PHONE_FISHER_TULLY
-;	opentext
-;	checkflag ENGINE_TULLY_READY_FOR_REMATCH
-;	iftrue .WantsBattle
-;	checkflag ENGINE_TULLY_HAS_WATER_STONE
-;	iftrue .HasWaterStone
-;	checkcellnum PHONE_FISHER_TULLY
-;	iftrue .TullyDefeated
-;	checkevent EVENT_TULLY_ASKED_FOR_PHONE_NUMBER
-;	iftrue .AskedAlready
-;	writetext FisherTullyAfterBattleText
-;	promptbutton
-;	setevent EVENT_TULLY_ASKED_FOR_PHONE_NUMBER
-;	scall .AskNumber1
-;	sjump .AskForNumber
-;
-;.AskedAlready:
-;	scall .AskNumber2
-;.AskForNumber:
-;	askforphonenumber PHONE_FISHER_TULLY
-;	ifequal PHONE_CONTACTS_FULL, .PhoneFull
-;	ifequal PHONE_CONTACT_REFUSED, .NumberDeclined
-;	gettrainername STRING_BUFFER_3, FISHER, TULLY1
-;	scall .RegisteredNumber
-;	sjump .NumberAccepted
-;
-;.WantsBattle:
-;	scall .Rematch
-;	winlosstext FisherTullyBeatenText, 0
-;	checkevent EVENT_RESTORED_POWER_TO_KANTO
-;	iftrue .LoadFight3
-;	checkevent EVENT_BEAT_ELITE_FOUR
-;	iftrue .LoadFight2
-;	checkevent EVENT_CLEARED_ROCKET_HIDEOUT
-;	iftrue .LoadFight1
-;	loadtrainer FISHER, TULLY1
-;	startbattle
-;	reloadmapafterbattle
-;	clearflag ENGINE_TULLY_READY_FOR_REMATCH
-;	end
-;
-;.LoadFight1:
-;	loadtrainer FISHER, TULLY2
-;	startbattle
-;	reloadmapafterbattle
-;	clearflag ENGINE_TULLY_READY_FOR_REMATCH
-;	end
-;
-;.LoadFight2:
-;	loadtrainer FISHER, TULLY3
-;	startbattle
-;	reloadmapafterbattle
-;	clearflag ENGINE_TULLY_READY_FOR_REMATCH
-;	end
-;
-;.LoadFight3:
-;	loadtrainer FISHER, TULLY4
-;	startbattle
-;	reloadmapafterbattle
-;	clearflag ENGINE_TULLY_READY_FOR_REMATCH
-;	end
-;
-;.HasWaterStone:
-;	scall .Gift
-;	verbosegiveitem WATER_STONE
-;	iffalse .NoRoom
-;	clearflag ENGINE_TULLY_HAS_WATER_STONE
-;	setevent ENGINE_TULLY_GAVE_WATER_STONE
-;	sjump .NumberAccepted
-;
-;.NoRoom:
-;	sjump .PackFull
-;
-;.AskNumber1:
-;	jumpstd AskNumber1MScript
-;	end
-;
-;.AskNumber2:
-;	jumpstd AskNumber2MScript
-;	end
-;
-;.RegisteredNumber:
-;	jumpstd RegisteredNumberMScript
-;	end
-;
-;.NumberAccepted:
-;	jumpstd NumberAcceptedMScript
-;	end
-;
-;.NumberDeclined:
-;	jumpstd NumberDeclinedMScript
-;	end
-;
-;.PhoneFull:
-;	jumpstd PhoneFullMScript
-;	end
-;
-;.Rematch:
-;	jumpstd RematchMScript
-;	end
-;
-;.Gift:
-;	jumpstd GiftMScript
-;	end
-;
-;.PackFull:
-;	jumpstd PackFullMScript
-;	end
-;
-;.TullyDefeated:
-;	writetext FisherTullyAfterBattleText
-;	promptbutton
-;	closetext
-;	end
-;
-;FisherTullySeenText:
-;	text "Let me demonstrate"
-;	line "the power of the"
-;	cont "#MON I caught!"
-;	done
-;
-;FisherTullyBeatenText:
-;	text "What? That's not"
-;	line "right."
-;	done
-;
-;FisherTullyAfterBattleText:
-;	text "I want to become"
-;	line "the trainer CHAMP"
-;
-;	para "using the #MON"
-;	line "I caught."
-;
-;	para "That's the best"
-;	line "part of fishing!"
-;	done
+TrainerHikerAnthony:
+	trainer HIKER, ANTHONY1, EVENT_BEAT_HIKER_ANTHONY, HikerAnthonySeenText, HikerAnthonyBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext HikerAnthonyAfterBattleText
+	waitbutton
+	closetext
+	end
 
+HikerAnthonySeenText:
+	text "Ah, it's good to"
+	line "be outside!"
+	cont "I feel so free!"
+	done
 
+HikerAnthonyBeatenText:
+	text "Gahahah!"
+	done
 
-;TrainerPokemaniacShane:
-;	trainer POKEMANIAC, SHANE, EVENT_BEAT_POKEMANIAC_SHANE, PokemaniacShaneSeenText, PokemaniacShaneBeatenText, 0, .Script
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext PokemaniacShaneAfterBattleText
-;	waitbutton
-;	closetext
-;	end
-;
-;PokemaniacShaneSeenText:
-;	text "HEY!"
-;
-;	para "This is my secret"
-;	line "place! Get lost,"
-;	cont "you outsider!"
-;	done
-;
-;PokemaniacShaneBeatenText:
-;	text "I should have used"
-;	line "my MOON STONE…"
-;	done
-;
-;PokemaniacShaneAfterBattleText:
-;	text "You're working on"
-;	line "a #DEX?"
-;
-;	para "Wow, you must know"
-;	line "some pretty rare"
-;	cont "#MON!"
-;
-;	para "May I please see"
-;	line "it. Please?"
-;	done
+HikerAnthonyAfterBattleText:
+	text "Losing feels in-"
+	line "significant if you"
+	cont "look up at the big"
+	cont "sky!"
+	done
 
+TrainerPokemaniacZach:
+	trainer POKEMANIAC, ZACH, EVENT_BEAT_POKEMANIAC_ZACH, PokemaniacZachSeenText, PokemaniacZachBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext PokemaniacZachAfterBattleText
+	waitbutton
+	closetext
+	end
 
+PokemaniacZachSeenText:
+	text "HEY!"
 
-;TrainerHikerBenjamin:
-;	trainer HIKER, BENJAMIN, EVENT_BEAT_HIKER_BENJAMIN, HikerBenjaminSeenText, HikerBenjaminBeatenText, 0, .Script
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext HikerBenjaminAfterBattleText
-;	waitbutton
-;	closetext
-;	end
-;
-;HikerBenjaminSeenText:
-;	text "Ah, it's good to"
-;	line "be outside!"
-;	cont "I feel so free!"
-;	done
-;
-;HikerBenjaminBeatenText:
-;	text "Gahahah!"
-;	done
-;
-;HikerBenjaminAfterBattleText:
-;	text "Losing feels in-"
-;	line "significant if you"
-;
-;	para "look up at the big"
-;	line "sky!"
-;	done
+	para "This is my secret"
+	line "place! Get lost,"
+	cont "you outsider!"
+	done
 
+PokemaniacZachBeatenText:
+	text "This is still"
+	line "my spot…"
+	done
+
+PokemaniacZachAfterBattleText:
+	text "You're working on"
+	line "a #DEX?"
+
+	para "Wow, you must know"
+	line "some pretty rare"
+	cont "#MON!"
+
+	para "May I please see"
+	line "it. Please?"
+	done
+
+TrainerFisherBarney:
+	trainer FISHER, BARNEY, EVENT_BEAT_FISHER_BARNEY, FisherBarneySeenText, FisherBarneyBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext FisherBarneyAfterBattleText
+	waitbutton
+	closetext
+	end
+
+FisherBarneySeenText:
+	text "Let me demonstrate"
+	line "the power of the"
+	cont "#MON I caught!"
+	done
+
+FisherBarneyBeatenText:
+	text "What? That's not"
+	line "right."
+	done
+
+FisherBarneyAfterBattleText:
+	text "I want to become"
+	line "the trainer CHAMP"
+	cont "using the #MON"
+	cont "I caught."
+
+	para "That's the best"
+	line "part of fishing!"
+	done
 
 Route42Sign1:
 Route42Sign2:
@@ -416,8 +301,8 @@ Route42HiddenMaxPotion:
 ;Route42UltraBall:
 ;	itemball ULTRA_BALL
 
-;Route42SuperPotion:
-;	itemball SUPER_POTION
+Route42MaxPotion:
+	itemball MAX_POTION
 
 Route42ApricornTree:
 	opentext
@@ -527,13 +412,11 @@ Route42_MapEvents:
 	object_event 52,  6, SPRITE_CHUCK, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42ChuckScript, EVENT_ROUTE_42_BOULDERS_CLEARED
 	object_event 52,  7, SPRITE_PRIMEAPE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42PrimeapeScript, EVENT_ROUTE_42_BOULDERS_CLEARED
 	object_event 50, 10, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42HikerScript, -1
-;
-;	object_event 40, 10, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerFisherTully, -1
-;	object_event 51,  9, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerHikerBenjamin, -1
-;	object_event 47,  8, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerPokemaniacShane, -1
-;
+	object_event 12,  6, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerHikerAnthony, -1 ;benjamin
+	object_event 26, 11, SPRITE_SUPER_NERD, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 2, TrainerPokemaniacZach, -1 ;shane
+	object_event 40, 10, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerFisherBarney, -1 ;tully
 ;	object_event  6,  4, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route42UltraBall, EVENT_ROUTE_42_ULTRA_BALL
-;	object_event 33,  8, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route42SuperPotion, EVENT_ROUTE_42_SUPER_POTION
+	object_event 33,  8, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route42MaxPotion, EVENT_ROUTE_42_MAX_POTION ;EVENT_ROUTE_42_SUPER_POTION
 ;	object_event 53,  4, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42Rock, EVENT_CIANWOOD_GYM_TRAINERS
 	object_event 54,  4, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42Rock, -1 ;EVENT_ROUTE_42_BOULDERS_CLEARED
 	object_event 50,  6, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route42Rock, EVENT_CIANWOOD_GYM_TRAINERS
