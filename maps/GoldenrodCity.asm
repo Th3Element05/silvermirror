6a4c546cab11f487dafbf6d1dca1f205879976cf
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-11-04 21:34:36 -0600

    cleared floria from callback

--- maps/GoldenrodCity.asm
+++ maps/GoldenrodCity.asm
@@ -19,16 +19,12 @@ GoldenrodCity_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-	callback MAPCALLBACK_NEWMAP, GoldenrodCityFlypointAndFloriaCallback
+	callback MAPCALLBACK_NEWMAP, GoldenrodCityFlypointCallback
 ;	callback MAPCALLBACK_OBJECTS, GoldenrodCityMoveTutorCallback
 
-GoldenrodCityFlypointAndFloriaCallback:
+GoldenrodCityFlypointCallback:
 	setflag ENGINE_FLYPOINT_GOLDENROD
 	setflag ENGINE_REACHED_GOLDENROD
-;	checkevent EVENT_MET_FLORIA
-;	iftrue .FloriaDone
-;	clearevent EVENT_FLORIA_AT_SUDOWOODO
-;.FloriaDone:
 	endcallback
 
 ;GoldenrodCityMoveTutorCallback:
