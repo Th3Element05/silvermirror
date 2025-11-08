0bfb343150492f0f616681b7c5e1e2ecdaf18449
Th3Element05 <sean.t.crawford@outlook.com>
2025-11-08 17:36:03 -0600

    Silver Cave Pokecenter is Kanto

--- maps/SilverCavePokecenter1F.asm
+++ maps/SilverCavePokecenter1F.asm
@@ -1,6 +1,4 @@
 	object_const_def
-	const SILVERCAVEPOKECENTER1F_NURSE
-	const SILVERCAVEPOKECENTER1F_GRANNY
 
 SilverCavePokecenter1F_MapScripts:
 	def_scene_scripts
@@ -10,22 +8,36 @@ SilverCavePokecenter1F_MapScripts:
 SilverCavePokecenter1FNurseScript:
 	jumpstd PokecenterNurseScript
 
-SilverCavePokecenter1FGrannyScript:
-	jumptextfaceplayer SilverCavePokecenter1FGrannyText
+SilverCavePokecenter1FReceptionistScript:
+	jumpstd PokecenterReceptionistScript
 
-SilverCavePokecenter1FGrannyText:
-	text "Trainers who seek"
-	line "power climb MT."
-
-	para "SILVER despite its"
-	line "many dangers…"
+SilverCavePokecenter1FBenchGuyScript:
+	jumptext SilverCavePokecenter1FBenchGuyText
+SilverCavePokecenter1FBenchGuyText:
+	text "Powerful trainers"
+	line "climb MT.SILVER"
+	cont "despite its many"
+	cont "dangers…"
 
 	para "With their trusted"
 	line "#MON, they must"
-
-	para "feel they can go"
-	line "anywhere…"
+	cont "feel they can go"
+	cont "anywhere…"
 	done
+
+;SilverCavePokecenter1FGrannyScript:
+;	jumptextfaceplayer SilverCavePokecenter1FGrannyText
+;SilverCavePokecenter1FGrannyText:
+;	text "Powerful trainers"
+;	line "climb MT.SILVER"
+;	cont "despite its many"
+;	cont "dangers…"
+;
+;	para "With their trusted"
+;	line "#MON, they must"
+;	cont "feel they can go"
+;	cont "anywhere…"
+;	done
 
 SilverCavePokecenter1F_MapEvents:
 	db 0, 0 ; filler
@@ -39,5 +51,7 @@ SilverCavePokecenter1F_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FNurseScript, -1
-	object_event  1,  5, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_LEFT, 2, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FGrannyScript, -1
+	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FNurseScript, -1
+	object_event 11,  2, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FReceptionistScript, -1
+	object_event  0,  4, SPRITE_BENCH_GUY, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FBenchGuyScript, -1
+;	object_event  7,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCavePokecenter1FGrannyScript, -1
