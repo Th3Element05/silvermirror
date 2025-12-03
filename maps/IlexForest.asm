4ba871b21575f25a082641438c8e57b9a169020e
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-12-03 00:36:56 -0600

    redid forests and cleaned up unused blocks

--- maps/IlexForest.asm
+++ maps/IlexForest.asm
@@ -2,20 +2,309 @@
 ;	const ILEXFOREST_FARFETCHD
 ;	const ILEXFOREST_YOUNGSTER1
 ;	const ILEXFOREST_BLACK_BELT
-;	const ILEXFOREST_ROCKER
-;	const ILEXFOREST_POKE_BALL1
 	const ILEXFOREST_KURT
 	const ILEXFOREST_LASS
-;	const ILEXFOREST_YOUNGSTER2
-;	const ILEXFOREST_POKE_BALL2
-;	const ILEXFOREST_POKE_BALL3
-;	const ILEXFOREST_POKE_BALL4
 
 IlexForest_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 ;	callback MAPCALLBACK_OBJECTS, IlexForestFarfetchdCallback
+
+IlexForestShrineScript:
+	checkevent EVENT_PUT_GS_BALL_IN_SHRINE
+	iftrue .AskCelebiEventTakeTwo
+	checkevent EVENT_FOREST_IS_RESTLESS
+	iftrue .ForestIsRestless
+	sjump .DontDoCelebiEvent
+
+.ForestIsRestless:
+	checkitem GS_BALL
+	iftrue .AskCelebiEvent
+.DontDoCelebiEvent:
+	jumptext Text_IlexForestShrine
+
+.AskCelebiEvent:
+	opentext
+	writetext Text_ShrineCelebiEvent
+	yesorno
+	iftrue .CelebiEvent
+	closetext
+	end
+
+.AskCelebiEventTakeTwo:
+	opentext
+	writetext Text_ShrineCelebiEventTakeTwo
+	yesorno
+	iftrue .CelebiEventTakeTwo
+	closetext
+	end
+
+.CelebiEvent:
+	setevent EVENT_PUT_GS_BALL_IN_SHRINE
+	takeitem GS_BALL
+	clearevent EVENT_FOREST_IS_RESTLESS
+	setevent EVENT_AZALEA_TOWN_KURT
+	disappear ILEXFOREST_LASS
+	clearevent EVENT_ROUTE_34_ILEX_FOREST_GATE_LASS
+	writetext Text_InsertGSBall
+	waitbutton
+.CelebiEventTakeTwo:
+	closetext
+	pause 20
+	showemote EMOTE_SHOCK, PLAYER, 20
+	special FadeOutMusic
+	applymovement PLAYER, IlexForestPlayerStepsDownMovement
+	pause 30
+	turnobject PLAYER, DOWN
+	pause 20
+	clearflag ENGINE_FOREST_IS_RESTLESS
+	special CelebiShrineEvent
+	loadwildmon CELEBI, 30
+	startbattle
+	reloadmapafterbattle
+	pause 20
+	special CheckBattleCaughtResult
+	iffalse .DidntCatchCelebi
+	setflag ENGINE_PLAYER_CAUGHT_CELEBI
+	clearevent EVENT_PUT_GS_BALL_IN_SHRINE
+	appear ILEXFOREST_KURT
+	applymovement ILEXFOREST_KURT, IlexForestKurtStepsUpMovement
+	opentext
+	writetext Text_KurtCaughtCelebi
+	waitbutton
+	closetext
+	applymovement ILEXFOREST_KURT, IlexForestKurtStepsDownMovement
+	disappear ILEXFOREST_KURT
+.DidntCatchCelebi:
+	end
+
+Text_IlexForestShrine:
+	text "ILEX FOREST"
+	line "SHRINE…"
+
+	para "It's in honor of"
+	line "the forest's"
+	cont "protector…"
+	done
+
+Text_ShrineCelebiEvent:
+	text "ILEX FOREST"
+	line "SHRINE…"
+
+	para "It's in honor of"
+	line "the forest's"
+	cont "protector…"
+
+	para "Oh? What is this?"
+
+	para "It's a hole."
+
+	para "It looks like the"
+	line "GS BALL would fit"
+	cont "inside it."
+
+	para "Do you want to put"
+	line "the GS BALL here?"
+	done
+
+Text_InsertGSBall:
+	text "<PLAYER> put in"
+	line "the GS BALL."
+	done
+
+Text_ShrineCelebiEventTakeTwo:
+	text "The GS BALL is"
+	line "still there in"
+	cont "the shrine."
+
+	para "Do you want to"
+	line "touch it?"
+	done
+
+Text_KurtCaughtCelebi:
+	text "Whew, wasn't that"
+	line "something!"
+
+	para "<PLAYER>, that was"
+	line "fantastic. Thanks!"
+
+	para "The legends about"
+	line "that SHRINE were"
+	cont "real after all."
+
+	para "I feel inspired by"
+	line "what I just saw."
+
+	para "It motivates me to"
+	line "make better BALLS!"
+
+	para "I'm going!"
+	done
+
+IlexForestPlayerStepsDownMovement:
+	fix_facing
+	slow_step DOWN
+	remove_fixed_facing
+	step_end
+
+IlexForestKurtStepsUpMovement:
+	step UP
+	step UP
+	step UP
+	step UP
+	step_end
+
+IlexForestKurtStepsDownMovement:
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step_end
+
+IlexForestLassScript:
+	jumptextfaceplayer Text_IlexForestLass
+Text_IlexForestLass:
+	text "Did something"
+	line "happen to the"
+	cont "forest's guardian?"
+	done
+
+IlexForestHeadbuttGuyScript:
+	faceplayer
+	opentext
+	checkevent EVENT_GOT_TM55_HEADBUTT
+	iftrue .AlreadyGotHeadbutt
+	writetext Text_HeadbuttIntro
+	promptbutton
+	verbosegiveitem TM_HEADBUTT
+	iffalse .BagFull
+	setevent EVENT_GOT_TM55_HEADBUTT
+.AlreadyGotHeadbutt:
+	writetext Text_HeadbuttOutro
+	waitbutton
+.BagFull:
+	closetext
+	end
+
+Text_HeadbuttIntro:
+	text "What am I doing?"
+
+	para "I'm shaking trees"
+	line "using HEADBUTT."
+
+	para "It's fun. Here,"
+	line "you try it too!"
+	done
+
+Text_HeadbuttOutro:
+	text "Rattle trees with"
+	line "HEADBUTT. Some-"
+	cont "times, sleeping"
+	cont "#MON fall out."
+	done
+
+TrainerBugCatcherArnie:
+	trainer BUG_CATCHER, ARNIE1, EVENT_BEAT_BUG_CATCHER_ARNIE, BugCatcherArnieSeenText, BugCatcherArnieBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext BugCatcherArnieAfterBattleText
+	waitbutton
+	closetext
+	end
+
+BugCatcherArnieSeenText:
+	text "Don't sneak up on"
+	line "me like that!"
+
+	para "You frightened a"
+	line "#MON away!"
+	done
+
+BugCatcherArnieBeatenText:
+	text "I hadn't seen that"
+	line "#MON before…"
+	done
+
+BugCatcherArnieAfterBattleText:
+	text "A #MON I've"
+	line "never seen before"
+	cont "fell out of the"
+	cont "tree when I used"
+	cont "HEADBUTT."
+
+	para "I ought to use"
+	line "HEADBUTT in other"
+	cont "places too."
+	done
+
+IlexForestSignpost:
+	jumptext IlexForestSignpostText
+IlexForestSignpostText:
+	text "ILEX FOREST is"
+	line "so overgrown with"
+	cont "trees that you"
+	cont "can't see the sky."
+
+	para "Please watch out"
+	line "for items that may"
+	cont "have been dropped."
+	done
+
+IlexForestRevive:
+	itemball REVIVE
+
+IlexForestXAttack:
+	itemball X_ATTACK
+
+IlexForestAntidote:
+	itemball ANTIDOTE
+
+IlexForestEther:
+	itemball ETHER
+
+IlexForestHiddenEther:
+	hiddenitem ETHER, EVENT_ILEX_FOREST_HIDDEN_ETHER
+
+IlexForestHiddenHyperPotion:
+	hiddenitem HYPER_POTION, EVENT_ILEX_FOREST_HIDDEN_HYPER_POTION
+
+IlexForestHiddenFullHeal:
+	hiddenitem FULL_HEAL, EVENT_ILEX_FOREST_HIDDEN_FULL_HEAL
+
+;IlexForestBoulder: ; unreferenced
+;	jumpstd StrengthBoulderScript
+
+IlexForest_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event  1,  5, ROUTE_34_ILEX_FOREST_GATE, 3
+	warp_event  3, 42, ILEX_FOREST_AZALEA_GATE, 1
+	warp_event  3, 43, ILEX_FOREST_AZALEA_GATE, 2
+
+	def_coord_events
+
+	def_bg_events
+	bg_event  3, 17, BGEVENT_READ, IlexForestSignpost
+	bg_event 11,  7, BGEVENT_ITEM, IlexForestHiddenEther
+	bg_event 22, 14, BGEVENT_ITEM, IlexForestHiddenHyperPotion
+	bg_event  1, 17, BGEVENT_ITEM, IlexForestHiddenFullHeal
+	bg_event  8, 22, BGEVENT_UP, IlexForestShrineScript
+
+	def_object_events
+;	object_event 14, 31, SPRITE_FARFETCH_D_MOVE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, IlexForestFarfetchdScript, EVENT_ILEX_FOREST_FARFETCHD
+;	object_event  7, 28, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, IlexForestCharcoalApprenticeScript, EVENT_ILEX_FOREST_APPRENTICE
+;	object_event  5, 28, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IlexForestCharcoalMasterScript, EVENT_ILEX_FOREST_CHARCOAL_MASTER
+	object_event  8, 29, SPRITE_KURT, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_ILEX_FOREST_KURT
+	object_event  3, 24, SPRITE_LASS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, IlexForestLassScript, EVENT_ILEX_FOREST_LASS
+	object_event 15, 14, SPRITE_ROCKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IlexForestHeadbuttGuyScript, -1
+	object_event 12,  1, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 0, TrainerBugCatcherArnie, -1 ;wayne
+	object_event 20, 32, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestRevive, EVENT_ILEX_FOREST_REVIVE
+	object_event  9, 17, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestXAttack, EVENT_ILEX_FOREST_X_ATTACK
+	object_event 17,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestAntidote, EVENT_ILEX_FOREST_ANTIDOTE
+	object_event 27,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestEther, EVENT_ILEX_FOREST_ETHER
 
 ;IlexForestFarfetchdCallback:
 ;	checkevent EVENT_GOT_HM01_CUT
@@ -99,7 +388,36 @@ IlexForest_MapScripts:
 ;	waitbutton
 ;	closetext
 ;	end
-
+;
+;IlexForestApprenticeIntroText:
+;	text "Oh, man… My boss"
+;	line "is going to be"
+;	cont "steaming…"
+;
+;	para "The FARFETCH'D"
+;	line "that CUTS trees"
+;
+;	para "for charcoal took"
+;	line "off on me."
+;
+;	para "I can't go looking"
+;	line "for it here in the"
+;	cont "ILEX FOREST."
+;
+;	para "It's too big, dark"
+;	line "and scary for me…"
+;	done
+;
+;IlexForestApprenticeAfterText:
+;	text "Wow! Thanks a"
+;	line "whole bunch!"
+;
+;	para "My boss's #MON"
+;	line "won't obey me be-"
+;	cont "cause I don't have"
+;	cont "a BADGE."
+;	done
+;
 ;IlexForestFarfetchdScript:
 ;	readmem wFarfetchdPosition
 ;	ifequal  1, .Position1
@@ -341,7 +659,16 @@ IlexForest_MapScripts:
 ;	closetext
 ;	readvar VAR_FACING
 ;	end
-
+;
+;Text_ItsTheMissingPokemon:
+;	text "It's the missing"
+;	line "#MON!"
+;	done
+;
+;Text_Kwaaaa:
+;	text "FARFETCH'D: Kwaa!"
+;	done
+;
 ;IlexForestCharcoalMasterScript:
 ;	faceplayer
 ;	opentext
@@ -386,120 +713,66 @@ IlexForest_MapScripts:
 ;	text "SCYTHER was" ;silvermirror -"SCYTHER CHOP was"
 ;	line "added to the PPS!"
 ;	done
-
-;IlexForestHeadbuttGuyScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_GOT_TM02_HEADBUTT
-;	iftrue .AlreadyGotHeadbutt
-;	writetext Text_HeadbuttIntro
-;	promptbutton
-;	verbosegiveitem TM_HEADBUTT
-;	iffalse .BagFull
-;	setevent EVENT_GOT_TM02_HEADBUTT
-;.AlreadyGotHeadbutt:
-;	writetext Text_HeadbuttOutro
-;	waitbutton
-;.BagFull:
-;	closetext
-;	end
-
-;TrainerBugCatcherWayne:
-;	trainer BUG_CATCHER, WAYNE, EVENT_BEAT_BUG_CATCHER_WAYNE, BugCatcherWayneSeenText, BugCatcherWayneBeatenText, 0, .Script
 ;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext BugCatcherWayneAfterBattleText
-;	waitbutton
-;	closetext
-;	end
-
-IlexForestLassScript:
-	jumptextfaceplayer Text_IlexForestLass
-
-;IlexForestRevive:
-;	itemball REVIVE
+;Text_CharcoalMasterIntro:
+;	text "Ah! My FARFETCH'D!"
 ;
-;IlexForestXAttack:
-;	itemball X_ATTACK
+;	para "You found it for"
+;	line "us, kid?"
 ;
-;IlexForestAntidote:
-;	itemball ANTIDOTE
+;	para "Without it, we"
+;	line "wouldn't be able"
 ;
-;IlexForestEther:
-;	itemball ETHER
+;	para "to CUT trees for"
+;	line "charcoal."
 ;
-;IlexForestHiddenEther:
-;	hiddenitem ETHER, EVENT_ILEX_FOREST_HIDDEN_ETHER
+;	para "Thanks, kid!"
 ;
-;IlexForestHiddenSuperPotion:
-;	hiddenitem SUPER_POTION, EVENT_ILEX_FOREST_HIDDEN_SUPER_POTION
+;	para "Now, how can I"
+;	line "thank you…"
 ;
-;IlexForestHiddenFullHeal:
-;	hiddenitem FULL_HEAL, EVENT_ILEX_FOREST_HIDDEN_FULL_HEAL
+;	para "I know! Here, take"
+;	line "this."
+;	done
 ;
-;IlexForestBoulder: ; unreferenced
-;	jumpstd StrengthBoulderScript
+;Text_CharcoalMasterMid:
+;	text "That's the"
+;	line "CUT PAGER."
 ;
-IlexForestSignpost:
-	jumptext IlexForestSignpostText
-
-IlexForestShrineScript:
-	checkevent EVENT_FOREST_IS_RESTLESS
-	iftrue .ForestIsRestless
-	sjump .DontDoCelebiEvent
-
-.ForestIsRestless:
-	checkitem GS_BALL
-	iftrue .AskCelebiEvent
-.DontDoCelebiEvent:
-	jumptext Text_IlexForestShrine
-
-.AskCelebiEvent:
-	opentext
-	writetext Text_ShrineCelebiEvent
-	yesorno
-	iftrue .CelebiEvent
-	closetext
-	end
-
-.CelebiEvent:
-	takeitem GS_BALL
-	clearevent EVENT_FOREST_IS_RESTLESS
-	setevent EVENT_AZALEA_TOWN_KURT
-	disappear ILEXFOREST_LASS
-	clearevent EVENT_ROUTE_34_ILEX_FOREST_GATE_LASS
-	writetext Text_InsertGSBall
-	waitbutton
-	closetext
-	pause 20
-	showemote EMOTE_SHOCK, PLAYER, 20
-	special FadeOutMusic
-	applymovement PLAYER, IlexForestPlayerStepsDownMovement
-	pause 30
-	turnobject PLAYER, DOWN
-	pause 20
-	clearflag ENGINE_FOREST_IS_RESTLESS
-	special CelebiShrineEvent
-	loadwildmon CELEBI, 30
-	startbattle
-	reloadmapafterbattle
-	pause 20
-	special CheckBattleCaughtResult
-	iffalse .DidntCatchCelebi
-	setflag ENGINE_PLAYER_CAUGHT_CELEBI
-	appear ILEXFOREST_KURT
-	applymovement ILEXFOREST_KURT, IlexForestKurtStepsUpMovement
-	opentext
-	writetext Text_KurtCaughtCelebi
-	waitbutton
-	closetext
-	applymovement ILEXFOREST_KURT, IlexForestKurtStepsDownMovement
-	disappear ILEXFOREST_KURT
-.DidntCatchCelebi:
-	end
-
+;	para "It lets you"
+;	line "summon a #MON"
+;	cont "to cut small "
+;	cont "trees."
+;
+;	para "Of course, you"
+;	line "have to have the"
+;
+;	para "GYM BADGE from"
+;	line "AZALEA to use it."
+;
+;	para "Oh here take this"
+;	line "too!"
+;	done
+;
+;Text_CharcoalMasterOutro:
+;	text "That's the CUT HM."
+;	line "Teach that to a"
+;
+;	para "#MON to use"
+;	line "CUT in battle."
+;	done
+;
+;Text_CharcoalMasterTalkAfter:
+;	text "Do you want to"
+;	line "apprentice as a"
+;
+;	para "charcoal maker"
+;	line "with me?"
+;
+;	para "You'll be first-"
+;	line "rate in ten years!"
+;	done
+;
 ;MovementData_Farfetchd_Pos1_Pos2:
 ;	big_step UP
 ;	big_step UP
@@ -734,265 +1007,4 @@ IlexForestShrineScript:
 ;	big_step UP
 ;	big_step UP
 ;	step_end
-
-IlexForestKurtStepsUpMovement:
-	step UP
-	step UP
-	step UP
-	step UP
-	step_end
-
-IlexForestKurtStepsDownMovement:
-	step DOWN
-	step DOWN
-	step DOWN
-	step DOWN
-	step_end
-
-IlexForestPlayerStepsDownMovement:
-	fix_facing
-	slow_step DOWN
-	remove_fixed_facing
-	step_end
-
-IlexForestApprenticeIntroText:
-	text "Oh, man… My boss"
-	line "is going to be"
-	cont "steaming…"
-
-	para "The FARFETCH'D"
-	line "that CUTS trees"
-
-	para "for charcoal took"
-	line "off on me."
-
-	para "I can't go looking"
-	line "for it here in the"
-	cont "ILEX FOREST."
-
-	para "It's too big, dark"
-	line "and scary for me…"
-	done
-
-IlexForestApprenticeAfterText:
-	text "Wow! Thanks a"
-	line "whole bunch!"
-
-	para "My boss's #MON"
-	line "won't obey me be-"
-	cont "cause I don't have"
-	cont "a BADGE."
-	done
-
-Text_ItsTheMissingPokemon:
-	text "It's the missing"
-	line "#MON!"
-	done
-
-Text_Kwaaaa:
-	text "FARFETCH'D: Kwaa!"
-	done
-
-Text_CharcoalMasterIntro:
-	text "Ah! My FARFETCH'D!"
-
-	para "You found it for"
-	line "us, kid?"
-
-	para "Without it, we"
-	line "wouldn't be able"
-
-	para "to CUT trees for"
-	line "charcoal."
-
-	para "Thanks, kid!"
-
-	para "Now, how can I"
-	line "thank you…"
-
-	para "I know! Here, take"
-	line "this."
-	done
-
-Text_CharcoalMasterMid:
-	text "That's the"
-	line "CUT PAGER."
-
-	para "It lets you"
-	line "summon a #MON"
-	cont "to cut small "
-	cont "trees."
-
-	para "Of course, you"
-	line "have to have the"
-
-	para "GYM BADGE from"
-	line "AZALEA to use it."
-
-	para "Oh here take this"
-	line "too!"
-	done
-
-Text_CharcoalMasterOutro:
-	text "That's the CUT HM."
-	line "Teach that to a"
-
-	para "#MON to use"
-	line "CUT in battle."
-	done
-
-Text_CharcoalMasterTalkAfter:
-	text "Do you want to"
-	line "apprentice as a"
-
-	para "charcoal maker"
-	line "with me?"
-
-	para "You'll be first-"
-	line "rate in ten years!"
-	done
-
-Text_HeadbuttIntro:
-	text "What am I doing?"
-
-	para "I'm shaking trees"
-	line "using HEADBUTT."
-
-	para "It's fun. Here,"
-	line "you try it too!"
-	done
-
-Text_HeadbuttOutro:
-	text "Rattle trees with"
-	line "HEADBUTT. Some-"
-	cont "times, sleeping"
-	cont "#MON fall out."
-	done
-
-Text_IlexForestLass:
-	text "Did something"
-	line "happen to the"
-	cont "forest's guardian?"
-	done
-
-IlexForestSignpostText:
-	text "ILEX FOREST is"
-	line "so overgrown with"
-
-	para "trees that you"
-	line "can't see the sky."
-
-	para "Please watch out"
-	line "for items that may"
-	cont "have been dropped."
-	done
-
-Text_IlexForestShrine:
-	text "ILEX FOREST"
-	line "SHRINE…"
-
-	para "It's in honor of"
-	line "the forest's"
-	cont "protector…"
-	done
-
-Text_ShrineCelebiEvent:
-	text "ILEX FOREST"
-	line "SHRINE…"
-
-	para "It's in honor of"
-	line "the forest's"
-	cont "protector…"
-
-	para "Oh? What is this?"
-
-	para "It's a hole."
-	line "It looks like the"
-
-	para "GS BALL would fit"
-	line "inside it."
-
-	para "Want to put the GS"
-	line "BALL here?"
-	done
-
-Text_InsertGSBall:
-	text "<PLAYER> put in the"
-	line "GS BALL."
-	done
-
-Text_KurtCaughtCelebi:
-	text "Whew, wasn't that"
-	line "something!"
-
-	para "<PLAYER>, that was"
-	line "fantastic. Thanks!"
-
-	para "The legends about"
-	line "that SHRINE were"
-	cont "real after all."
-
-	para "I feel inspired by"
-	line "what I just saw."
-
-	para "It motivates me to"
-	line "make better BALLS!"
-
-	para "I'm going!"
-	done
-
-;BugCatcherWayneSeenText:
-;	text "Don't sneak up on"
-;	line "me like that!"
 ;
-;	para "You frightened a"
-;	line "#MON away!"
-;	done
-;
-;BugCatcherWayneBeatenText:
-;	text "I hadn't seen that"
-;	line "#MON before…"
-;	done
-;
-;BugCatcherWayneAfterBattleText:
-;	text "A #MON I've"
-;	line "never seen before"
-;
-;	para "fell out of the"
-;	line "tree when I used"
-;	cont "HEADBUTT."
-;
-;	para "I ought to use"
-;	line "HEADBUTT in other"
-;	cont "places too."
-;	done
-
-IlexForest_MapEvents:
-	db 0, 0 ; filler
-
-	def_warp_events
-	warp_event  1,  5, ROUTE_34_ILEX_FOREST_GATE, 3
-	warp_event  3, 42, ILEX_FOREST_AZALEA_GATE, 1
-	warp_event  3, 43, ILEX_FOREST_AZALEA_GATE, 2
-
-	def_coord_events
-
-	def_bg_events
-	bg_event  3, 17, BGEVENT_READ, IlexForestSignpost
-;	bg_event 11,  7, BGEVENT_ITEM, IlexForestHiddenEther
-;	bg_event 22, 14, BGEVENT_ITEM, IlexForestHiddenSuperPotion
-;	bg_event  1, 17, BGEVENT_ITEM, IlexForestHiddenFullHeal
-	bg_event  8, 22, BGEVENT_UP, IlexForestShrineScript
-
-	def_object_events
-;	object_event 14, 31, SPRITE_FARFETCH_D_MOVE, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, IlexForestFarfetchdScript, EVENT_ILEX_FOREST_FARFETCHD
-;	object_event  7, 28, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, IlexForestCharcoalApprenticeScript, EVENT_ILEX_FOREST_APPRENTICE
-;	object_event  5, 28, SPRITE_BLACK_BELT, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IlexForestCharcoalMasterScript, EVENT_ILEX_FOREST_CHARCOAL_MASTER
-;	object_event 15, 14, SPRITE_ROCKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, IlexForestHeadbuttGuyScript, -1
-;	object_event 20, 32, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestRevive, EVENT_ILEX_FOREST_REVIVE
-	object_event  8, 29, SPRITE_KURT, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_ILEX_FOREST_KURT
-	object_event  3, 24, SPRITE_LASS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, IlexForestLassScript, EVENT_ILEX_FOREST_LASS
-;	object_event 12,  1, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 0, TrainerBugCatcherWayne, -1
-;	object_event  9, 17, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestXAttack, EVENT_ILEX_FOREST_X_ATTACK
-;	object_event 17,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestAntidote, EVENT_ILEX_FOREST_ANTIDOTE
-;	object_event 27,  1, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IlexForestEther, EVENT_ILEX_FOREST_ETHER
