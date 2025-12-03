d58571536526d1d006273a4f995101c8055765b3
Th3Element05 <sean.t.crawford@outlook.com>
2025-12-03 00:39:18 -0600

    scripted GS_BALL event stuff

--- maps/BillsFamilysHouse.asm
+++ maps/BillsFamilysHouse.asm
@@ -1,5 +1,5 @@
 	object_const_def
-;	const BILLSFAMILYSHOUSE_BILL
+	const BILLSFAMILYSHOUSE_BILL
 ;	const BILLSFAMILYSHOUSE_POKEFAN_F
 ;	const BILLSFAMILYSHOUSE_TWIN
 
@@ -7,29 +7,35 @@ BillsFamilysHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_TILES, BillsFamilysHouseTilesCallback
 
-;BillScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_GOT_EEVEE
-;	iftrue .GotEevee
-;	writetext BillTakeThisEeveeText
+BillsFamilysHouseTilesCallback:
+	changeblock 2, 0, $1c ; pc
+	endcallback
+
+BillScript:
+	faceplayer
+	opentext
+;	checkevent EVENT_GOT_GS_BALL_FROM_BILL
+;	iftrue .GotGSBall
+	writetext BillsFamilysHouseBill_GiveGSBallText
+	waitbutton
 ;	yesorno
 ;	iffalse .Refused
-;	writetext BillImCountingOnYouText
-;	promptbutton
-;	waitsfx
-;	readvar VAR_PARTYCOUNT
-;	ifequal PARTY_LENGTH, .NoRoom
-;	writetext ReceivedEeveeText
-;	playsound SFX_CAUGHT_MON
-;	waitsfx
-;	givepoke EEVEE, 20
-;	setevent EVENT_GOT_EEVEE
-;	writetext BillEeveeMayEvolveText
-;	waitbutton
-;	closetext
-;	end
+	verbosegiveitem GS_BALL
+	setevent EVENT_GOT_GS_BALL_FROM_BILL
+	setevent EVENT_CAN_GIVE_GS_BALL_TO_KURT
+	writetext BillsFamilysHouseBill_ExplainGSBallText
+	waitbutton
+	closetext
+	readvar VAR_FACING
+	ifequal LEFT, .PlayerDoesntNeedToMove
+	applymovement PLAYER, BillsFamilysHouse_PlayerStepsBackMovement
+.PlayerDoesntNeedToMove
+	applymovement BILLSFAMILYSHOUSE_BILL, BillsFamilysHouse_BillLeavesMovement
+	disappear BILLSFAMILYSHOUSE_BILL
+	playsound SFX_EXIT_BUILDING
+	end
 ;
 ;.NoRoom:
 ;	writetext BillPartyFullText
@@ -42,203 +48,195 @@ BillsFamilysHouse_MapScripts:
 ;	waitbutton
 ;	closetext
 ;	end
-;
-;.GotEevee:
-;	writetext BillPopWontWorkText
-;	waitbutton
-;	closetext
-;	end
 
-;BillsMomScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_MET_BILL
-;	iffalse .HaventMetBill
-;	writetext BillsMomText_BeforeEcruteak
-;	waitbutton
-;	closetext
-;	end
-;
-;.HaventMetBill:
-;	writetext BillsMomText_AfterEcruteak
-;	waitbutton
-;	closetext
-;	end
+BillsFamilysHouseBill_GiveGSBallText:
+	text "BILL: <PLAYER>!"
+	line "What are you doing"
+	cont "in GOLDENROD?"
 
-;BillsSisterScript:
-;	faceplayer
-;	opentext
-;	checkcellnum PHONE_BILL
-;	iftrue .GotBillsNumber
-;	writetext BillsSisterUsefulNumberText
-;	askforphonenumber PHONE_BILL
-;	ifequal PHONE_CONTACTS_FULL, .NoRoom
-;	ifequal PHONE_CONTACT_REFUSED, .Refused
-;	waitsfx
-;	addcellnum PHONE_BILL
-;	writetext RecordedBillsNumberText
-;	playsound SFX_REGISTER_PHONE_NUMBER
-;	waitsfx
-;	promptbutton
-;.GotBillsNumber:
-;	writetext BillsSisterStorageSystemText
-;	waitbutton
-;	closetext
-;	end
-;
-;.Refused:
-;	writetext BillsSisterRefusedNumberText
-;	waitbutton
-;	closetext
-;	end
-;
-;.NoRoom:
-;	writetext BillsSisterPhoneFullText
-;	promptbutton
-;	sjump .Refused
+	para "I heard you were"
+	line "the new #MON"
+	cont "LEAGUE CHAMPION!"
 
-;BillsHouseBookshelf1:
-;	jumpstd PictureBookshelfScript
+	para "Congratulations!"
 
-;BillsHouseBookshelf2:
-;	jumpstd MagazineBookshelfScript
+	para "You couldn't have"
+	line "picked a better"
+	cont "time to visit!"
 
-;BillsHouseRadio:
-;	jumpstd Radio2Script
+	para "I'm here working"
+	line "on an experimental"
+	cont "#MON trading"
+	cont "program."
 
-;BillTakeThisEeveeText:
-;	text "BILL: Hi, <PLAYER>!"
-;	line "Do us a favor and"
-;	cont "take this EEVEE."
+	para "I'm trying to see"
+	line "if #MON can be"
+	cont "traded between the"
+	cont "past and future."
+
+	para "I'm calling it the"
+	line "TIME CAPSULE!"
+
+	para "Its not ready yet,"
+	line "but this object"
+	cont "came through when"
+	cont "I was running one"
+	cont "of my tests."
+
+	para "I think you should"
+	line "take it."
+	done
+
+BillsFamilysHouseBill_ExplainGSBallText:
+	text "BILL: It resembles"
+	line "a # BALL, but"
+	cont "it isn't one."
+
+	para "I'm not an expert"
+	line "on # BALLS."
+
+	para "But maybe you"
+	line "will meet someone"
+	cont "who can tell you"
+	cont "more about it."
+
+	para "Anyway, I need to"
+	line "get back to my"
+	cont "house in KANTO."
+
+	para "You should come"
+	line "visit sometime!"
+
+	para "See you later!"
+	done
+
+BillsFamilysHouse_BillLeavesMovement:
+	step LEFT
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step DOWN
+	step_end
+
+BillsFamilysHouse_PlayerStepsBackMovement:
+	fix_facing
+	step LEFT
+	remove_fixed_facing
+	step_end
+
+BillsMomScript:
+	faceplayer
+	opentext
+	checkevent EVENT_GOT_GS_BALL_FROM_BILL
+	iftrue .TalkedToBill
+	writetext BillsMomText_BeforeGSBall
+	waitbutton
+	closetext
+	end
+
+.TalkedToBill:
+	writetext BillsMomText_AfterGSBall
+	waitbutton
+	closetext
+	end
+
+BillsMomText_BeforeGSBall:
+	text "Oh, you collect"
+	line "#MON? My son"
+	cont "BILL is an expert."
+	done
+
+BillsMomText_AfterGSBall:
+;	text "Any friend of my"
+;	line "son is welcome!"
 ;
-;	para "It came over when"
-;	line "I was adjusting"
-;	cont "the TIME CAPSULE."
-;
-;	para "Someone has to"
-;	line "take care of it,"
-;
-;	para "but I don't like"
-;	line "being outside."
-;
-;	para "Can I count on you"
-;	line "to play with it,"
-;	cont "<PLAYER>?"
+;	para "Come for a visit"
+;	line "any time!"
 ;	done
-
-;BillImCountingOnYouText:
-;	text "BILL: I knew you'd"
-;	line "come through!"
-;
-;	para "Way to go! You're"
-;	line "the real deal!"
-;
-;	para "OK, I'm counting"
-;	line "on you."
-;
-;	para "Take good care of"
-;	line "it!"
-;	done
-
-;ReceivedEeveeText:
-;	text "<PLAYER> received"
-;	line "EEVEE!"
-;	done
-
-;BillEeveeMayEvolveText:
-;	text "BILL: PROF.ELM"
-;	line "claims EEVEE may"
-;
-;	para "evolve in new and"
-;	line "unknown ways."
-;	done
-
-;BillPartyFullText:
-;	text "Whoa, wait. You"
-;	line "can't carry any"
-;	cont "more #MON."
-;	done
-
-;BillNoEeveeText:
-;	text "Oh… Now what to"
-;	line "do?"
-;	done
-
-;BillPopWontWorkText:
-;	text "BILL: My pop, he"
-;	line "won't work. All he"
-;
-;	para "does is goof off"
-;	line "all day long."
-;
-;	para "He's getting to be"
-;	line "a real headache…"
-;	done
-
-;BillsMomText_BeforeEcruteak:
-;	text "Oh, you collect"
-;	line "#MON? My son"
-;	cont "BILL is an expert."
-;
-;	para "He just got called"
-;	line "to the #MON"
-;
-;	para "CENTER in ECRUTEAK"
-;	line "CITY."
-;
-;	para "My husband went"
-;	line "off to the GAME"
-;
-;	para "CORNER without"
-;	line "being called…"
-;	done
-
 ;BillsMomText_AfterEcruteak:
-;	text "My husband was"
-;	line "once known as a"
-;
-;	para "#MANIAC."
-;	line "BILL must have"
-;
-;	para "taken after his"
-;	line "father."
-;	done
+	text "My husband was"
+	line "once known as a"
+	cont "#MANIAC."
 
-;BillsSisterUsefulNumberText:
-;	text "Are you a trainer?"
-;
-;	para "I've got a useful"
-;	line "phone number for"
-;	cont "you."
-;	done
+	para "BILL must have"
+	line "taken after his"
+	cont "father."
+	done
 
-;RecordedBillsNumberText:
-;	text "<PLAYER> recorded"
-;	line "BILL's number."
-;	done
+BillsSisterScript:
+	faceplayer
+	opentext
+	checkcellnum PHONE_BILL
+	iftrue .GotBillsNumber
+	writetext BillsSisterUsefulNumberText
+	askforphonenumber PHONE_BILL
+	ifequal PHONE_CONTACTS_FULL, .NoRoom
+	ifequal PHONE_CONTACT_REFUSED, .Refused
+	waitsfx
+	addcellnum PHONE_BILL
+	writetext SisterGaveBillsNumberText
+	playsound SFX_REGISTER_PHONE_NUMBER
+	waitsfx
+	promptbutton
+.GotBillsNumber:
+	writetext BillsSisterStorageSystemText
+	waitbutton
+	closetext
+	end
 
-;BillsSisterRefusedNumberText:
-;	text "My brother made"
-;	line "the PC #MON"
-;	cont "storage system."
-;
-;	para "I was going to"
-;	line "give you BILL's"
-;	cont "number…"
-;	done
+.NoRoom:
+	writetext BillsSisterPhoneFullText
+	promptbutton
+.Refused:
+	writetext BillsSisterRefusedNumberText
+	waitbutton
+	closetext
+	end
 
-;BillsSisterPhoneFullText:
-;	text "You can't record"
-;	line "any more numbers."
-;	done
+BillsSisterUsefulNumberText:
+	text "Are you a trainer?"
 
-;BillsSisterStorageSystemText:
-;	text "My big brother"
-;	line "BILL made the PC"
-;
-;	para "#MON storage"
-;	line "system."
-;	done
+	para "I've got a useful"
+	line "phone number for"
+	cont "you."
+	done
+
+SisterGaveBillsNumberText:
+	text "<PLAYER> recorded"
+	line "BILL's number."
+	done
+
+BillsSisterRefusedNumberText:
+	text "My brother made"
+	line "the PC #MON"
+	cont "storage system."
+
+	para "I was going to"
+	line "give you BILL's"
+	cont "number…"
+	done
+
+BillsSisterPhoneFullText:
+	text "You can't record"
+	line "any more numbers."
+	done
+
+BillsSisterStorageSystemText:
+	text "My big brother"
+	line "BILL made the PC"
+	cont "#MON storage"
+	cont "system."
+	done
+
+BillsHouseBookshelf1:
+	jumpstd PictureBookshelfScript
+
+BillsHouseBookshelf2:
+	jumpstd MagazineBookshelfScript
+
+BillsHouseRadio:
+	jumpstd Radio2Script
 
 BillsFamilysHouse_MapEvents:
 	db 0, 0 ; filler
@@ -250,11 +248,11 @@ BillsFamilysHouse_MapEvents:
 	def_coord_events
 
 	def_bg_events
-;	bg_event  0,  1, BGEVENT_READ, BillsHouseBookshelf2
-;	bg_event  1,  1, BGEVENT_READ, BillsHouseBookshelf1
-;	bg_event  7,  1, BGEVENT_READ, BillsHouseRadio
+	bg_event  0,  1, BGEVENT_READ, BillsHouseBookshelf2
+	bg_event  1,  1, BGEVENT_READ, BillsHouseBookshelf1
+	bg_event  7,  1, BGEVENT_READ, BillsHouseRadio
 
 	def_object_events
-;	object_event  2,  3, SPRITE_BILL, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BillScript, EVENT_MET_BILL
-;	object_event  5,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, BillsMomScript, -1
-;	object_event  5,  4, SPRITE_TWIN, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BillsSisterScript, -1
+	object_event  3,  2, SPRITE_BILL, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BillScript, EVENT_GOT_GS_BALL_FROM_BILL
+	object_event  5,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, BillsMomScript, -1
+	object_event  5,  4, SPRITE_TWIN, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BillsSisterScript, -1
