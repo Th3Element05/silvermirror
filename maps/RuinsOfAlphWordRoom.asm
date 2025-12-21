75f801e08352d729d38d1fc6038f8657ea8d108b
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-12-21 15:09:53 -0600

    Added generic Alph Word Room

---
+++ maps/RuinsOfAlphWordRoom.asm
@@ -0,0 +1,18 @@
+RuinsOfAlphWordRoom_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+RuinsOfAlphWordRoom_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event  7,  5, RUINS_OF_ALPH_AERODACTYL_ITEM_ROOM, 3
+	warp_event  8,  5, RUINS_OF_ALPH_AERODACTYL_ITEM_ROOM, 4
+	warp_event 11, 10, RUINS_OF_ALPH_INNER_CHAMBER, 8
+
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
