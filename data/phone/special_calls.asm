b9b59ae5a8b513e490bc57a2899e49db8a45e276
Sean Crawford <scrawford@sumnerone.com>
2025-12-31 21:37:54 -0600

    special call to start radio tower event

--- data/phone/special_calls.asm
+++ data/phone/special_calls.asm
@@ -8,13 +8,13 @@ ENDM
 SpecialPhoneCallList:
 ; entries correspond to SPECIALCALL_* constants
 	table_width SPECIALCALL_SIZE, SpecialPhoneCallList
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;POKERUS
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;SS ANNE
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;FLASH
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;SILPHCO
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;VIRIDIANGYM
-	specialcall SpecialCallWhereverYouAre,  PHONECONTACT_OAK,      OakPhoneCallerScript ;elm ss ticket
-;	specialcall SpecialCallWhereverYouAre,  PHONECONTACT_BIKESHOP, BikeShopPhoneCallerScript ;bikeshop
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_MOM,      MomPhoneLectureScript ;MOM WORRIED
-	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK,      OakPhoneCallerScript ;elm masterball
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;POKERUS
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;SS ANNE
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;FLASH
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;SILPHCO
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;VIRIDIANGYM
+	specialcall SpecialCallWhereverYouAre,  PHONECONTACT_OAK, OakPhoneCallerScript ;SS TICKET
+	specialcall SpecialCallWhereverYouAre,  PHONECONTACT_OAK, OakPhoneCallerScript ;RADIOTOWER
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_MOM, MomPhoneLectureScript ;MOM WORRIED
+	specialcall SpecialCallOnlyWhenOutside, PHONECONTACT_OAK, OakPhoneCallerScript ;elm masterball
 	assert_table_length NUM_SPECIALCALLS
