ff4830654104d25c4aba0a6b3d61fa0447ae2e4b
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-12-31 22:05:07 -0600

    scripted oak encounter in mr.pokemons house

--- maps/MrPokemonsHouse.asm
+++ maps/MrPokemonsHouse.asm
@@ -1,11 +1,11 @@
 	object_const_def
-;	const MRPOKEMONSHOUSE_GENTLEMAN
-;	const MRPOKEMONSHOUSE_OAK
+	const MRPOKEMONSHOUSE_OAK
+	const MRPOKEMONSHOUSE_GENTLEMAN
 
 MrPokemonsHouse_MapScripts:
 	def_scene_scripts
-;	scene_script MrPokemonsHouseMeetMrPokemonScene, SCENE_MRPOKEMONSHOUSE_MEET_MR_POKEMON
-;	scene_script MrPokemonsHouseNoopScene,          SCENE_MRPOKEMONSHOUSE_NOOP
+	scene_script MrPokemonsHouseMeetMrPokemonScene, SCENE_MRPOKEMONSHOUSE_MEET_MR_POKEMON
+	scene_script MrPokemonsHouseNoopScene,          SCENE_MRPOKEMONSHOUSE_NOOP
 
 	def_callbacks
 ;	callback MAPCALLBACK_TILES, MrPokemonsHouseTilesCallback ;this was for shared map (removed), unnecessary now
@@ -15,365 +15,340 @@ MrPokemonsHouse_MapScripts:
 ;	changeblock 4, 0, $55 ; couch
 ;	endcallback
 
-;MrPokemonsHouseMeetMrPokemonScene:
-;	sdefer MrPokemonsHouseMrPokemonEventScript
-;MrPokemonsHouseNoopScene:
-;	end
+MrPokemonsHouseMeetMrPokemonScene:
+	sdefer MrPokemonsHouseMrPokemonEventScript
+MrPokemonsHouseNoopScene:
+	end
 
+MrPokemonsHouseMrPokemonEventScript:
+	showemote EMOTE_SHOCK, MRPOKEMONSHOUSE_OAK, 15
+	turnobject MRPOKEMONSHOUSE_OAK, DOWN
+	playmusic MUSIC_PROF_OAK
+	opentext
+	writetext MrPokemonsHouse_OakIntroText
+	waitbutton
+	closetext
+	applymovement PLAYER, MrPokemonsHouse_PlayerWalksToMrPokemonMovement
+	turnobject MRPOKEMONSHOUSE_GENTLEMAN, DOWN
+	opentext
+	writetext MrPokemonText_Intro
+	waitbutton
+	closetext
+	applymovement MRPOKEMONSHOUSE_OAK, MrPokemonsHouse_OakWalksToPlayerMovement
+	turnobject PLAYER, LEFT
+	opentext
+	writetext MrPokemonsHouse_OakExplainsVisitText
+	waitbutton
+	closetext
+	applymovement MRPOKEMONSHOUSE_OAK, MrPokemonsHouse_OakWalksToMrPokemonMovement
+	opentext
+	writetext MrPokemonsHouse_OakSaysFarewellText
+	waitbutton
+	closetext
+	applymovement MRPOKEMONSHOUSE_OAK, MrPokemonsHouse_OakLeavesMovement
+	playsound SFX_EXIT_BUILDING
+	disappear MRPOKEMONSHOUSE_OAK
+	waitsfx
+	special RestartMapMusic
+	pause 15
+	turnobject PLAYER, UP
+	opentext
+	writetext MrPokemonText_Invaluable
+	promptbutton
+	writetext MrPokemonText_VisitElm
+	waitbutton
+	closetext
+	setevent EVENT_OAKS_LAB_OAK ;disappear
+	clearevent EVENT_RADIO_TOWER_4F_OAK ;appear
+	clearevent EVENT_ELMS_LAB_ELM ;appear
+	setscene SCENE_MRPOKEMONSHOUSE_NOOP
+	setmapscene ROUTE_30, SCENE_ROUTE30_HOOH_FLYOVER
+	setmapscene NEW_BARK_TOWN, SCENE_NEWBARKTOWN_RIVAL_ENCOUNTER
+	setmapscene ELMS_LAB, SCENE_ELMSLAB_NOOP
+	setmapscene RADIO_TOWER_4F, SCENE_RADIOTOWER4F_RECORDING
+	end
 
-;MrPokemonsHouseMrPokemonEventScript:
-;	showemote EMOTE_SHOCK, MRPOKEMONSHOUSE_GENTLEMAN, 15
-;	turnobject MRPOKEMONSHOUSE_GENTLEMAN, DOWN
-;	opentext
-;	writetext MrPokemonIntroText1
-;	waitbutton
-;	closetext
-;	applymovement PLAYER, MrPokemonsHouse_PlayerWalksToMrPokemon
-;	opentext
-;	writetext MrPokemonIntroText2
-;	promptbutton
-;	waitsfx
-;	giveitem MYSTERY_EGG
-;	writetext MrPokemonsHouse_GotEggText
-;	playsound SFX_KEY_ITEM
-;	waitsfx
-;	itemnotify
-;	setevent EVENT_GOT_MYSTERY_EGG_FROM_MR_POKEMON
-;	blackoutmod CHERRYGROVE_CITY
-;	writetext MrPokemonIntroText3
-;	promptbutton
-;	turnobject MRPOKEMONSHOUSE_GENTLEMAN, RIGHT
-;	writetext MrPokemonIntroText4
-;	promptbutton
-;	turnobject MRPOKEMONSHOUSE_GENTLEMAN, DOWN
-;	turnobject MRPOKEMONSHOUSE_OAK, LEFT
-;	writetext MrPokemonIntroText5
-;	waitbutton
-;	closetext
-;	sjump MrPokemonsHouse_OakScript
+MrPokemonsHouse_OakIntroText:
+	text "<PLAYER>!"
+	line "Come in!"
 
-;MrPokemonsHouse_MrPokemonScript:
-;	faceplayer
-;	opentext
-;	checkitem GOLD_SCALE
-;	iftrue .RedScale
-;	checkevent EVENT_GAVE_MYSTERY_EGG_TO_ELM
-;	iftrue .AlwaysNewDiscoveries
-;	writetext MrPokemonText_ImDependingOnYou
-;	waitbutton
-;	closetext
-;	end
+	para "Meet my friend,"
+	line "people call him"
+	cont "MR.#MON."
+	done
 
-;.AlwaysNewDiscoveries:
-;	writetext MrPokemonText_AlwaysNewDiscoveries
-;	waitbutton
-;	closetext
-;	end
+MrPokemonsHouse_PlayerWalksToMrPokemonMovement:
+	step RIGHT
+	step UP
+	step UP
+	step_end
 
-;.RedScale:
-;	writetext MrPokemonText_GimmeTheScale
-;	yesorno
-;	iffalse .refused
-;	verbosegiveitem LUCKY_EGG
-;	iffalse .full
-;	takeitem GOLD_SCALE
-;	sjump .AlwaysNewDiscoveries
+MrPokemonText_Intro:
+	text "Hello, hello! You"
+	line "must be <PLAYER>."
 
-;.refused
-;	writetext MrPokemonText_Disappointed
-;	waitbutton
-;.full
-;	closetext
-;	end
+	para "PROF.OAK was just"
+	line "talking about you."
 
-;MrPokemonsHouse_OakScript:
-;	playmusic MUSIC_PROF_OAK
-;	applymovement MRPOKEMONSHOUSE_OAK, MrPokemonsHouse_OakWalksToPlayer
-;	turnobject PLAYER, RIGHT
-;	opentext
-;	writetext MrPokemonsHouse_OakText1
-;	promptbutton
-;	waitsfx
-;	writetext MrPokemonsHouse_GetDexText
-;	playsound SFX_ITEM
-;	waitsfx
-;	setflag ENGINE_POKEDEX
-;	writetext MrPokemonsHouse_OakText2
-;	waitbutton
-;	closetext
-;	turnobject PLAYER, DOWN
-;	applymovement MRPOKEMONSHOUSE_OAK, MrPokemonsHouse_OakExits
-;	playsound SFX_EXIT_BUILDING
-;	disappear MRPOKEMONSHOUSE_OAK
-;	waitsfx
-;	special RestartMapMusic
-;	pause 15
-;	turnobject PLAYER, UP
-;	opentext
-;	writetext MrPokemonsHouse_MrPokemonHealText
-;	waitbutton
-;	closetext
-;	special FadeBlackQuickly
-;	special ReloadSpritesNoPalettes
-;	playmusic MUSIC_HEAL
-;	special StubbedTrainerRankings_Healings
-;	special HealParty
-;	pause 60
-;	special FadeInQuickly
-;	special RestartMapMusic
-;	opentext
-;	writetext MrPokemonText_ImDependingOnYou
-;	waitbutton
-;	closetext
-;	setevent EVENT_RIVAL_NEW_BARK_TOWN
-;	setevent EVENT_PLAYERS_HOUSE_1F_NEIGHBOR
-;	clearevent EVENT_PLAYERS_NEIGHBORS_HOUSE_NEIGHBOR
-;	setscene SCENE_MRPOKEMONSHOUSE_NOOP
-;	setmapscene CHERRYGROVE_CITY, SCENE_CHERRYGROVECITY_MEET_RIVAL
-;	setmapscene ELMS_LAB, SCENE_ELMSLAB_MEET_OFFICER
-;	specialphonecall SPECIALCALL_ROBBED
-;	clearevent EVENT_COP_IN_ELMS_LAB
-;	checkevent EVENT_GOT_TOTODILE_FROM_ELM
-;	iftrue .RivalTakesChikorita
-;	checkevent EVENT_GOT_CHIKORITA_FROM_ELM
-;	iftrue .RivalTakesCyndaquil
-;	setevent EVENT_TOTODILE_POKEBALL_IN_ELMS_LAB
-;	end
+	para "He says you're a"
+	line "very impressive"
+	cont "young trainer!"
 
-;.RivalTakesChikorita:
-;	setevent EVENT_CHIKORITA_POKEBALL_IN_ELMS_LAB
-;	end
+	para "I'm very happy to"
+	line "meet you!"
+	done
 
-;.RivalTakesCyndaquil:
-;	setevent EVENT_CYNDAQUIL_POKEBALL_IN_ELMS_LAB
-;	end
+MrPokemonsHouse_OakWalksToPlayerMovement:
+	step DOWN
+	turn_head RIGHT
+	step_end
 
-;MrPokemonsHouse_ForeignMagazines:
-;	jumptext MrPokemonsHouse_ForeignMagazinesText
+MrPokemonsHouse_OakExplainsVisitText:
+	text "I travel to JOHTO"
+	line "from time to time"
+	cont "to tape segments"
+	cont "for my RADIO show."
 
-;MrPokemonsHouse_BrokenComputer:
-;	jumptext MrPokemonsHouse_BrokenComputerText
+	para "Speaking of which,"
+	line "I must be going."
 
-;MrPokemonsHouse_StrangeCoins:
-;	jumptext MrPokemonsHouse_StrangeCoinsText
+	para "I need to get to"
+	line "the RADIO TOWER"
+	cont "in GOLDENROD."
+	done
 
-;MrPokemonsHouse_PlayerWalksToMrPokemon:
-;	step RIGHT
-;	step UP
-;	step_end
+MrPokemonsHouse_OakWalksToMrPokemonMovement:
+	step UP
+	turn_head RIGHT
+	step_end
 
-;MrPokemonsHouse_OakWalksToPlayer:
-;	step DOWN
-;	step LEFT
-;	step LEFT
-;	step_end
+MrPokemonsHouse_OakSaysFarewellText:
+	text "OAK: MR.#MON,"
+	line "I wish I had more"
+	cont "time to chat, but"
+	cont "I must be going."
 
-;MrPokemonsHouse_OakExits:
-;	step DOWN
-;	step LEFT
-;	turn_head DOWN
-;	step_sleep 2
-;	step_end
+	para "Farewell!"
+	done
 
-;MrPokemonIntroText1:
-;	text "Hello, hello! You"
-;	line "must be <PLAY_G>."
-;
-;	para "PROF.ELM said that"
-;	line "you would visit."
-;	done
+MrPokemonsHouse_OakLeavesMovement:
+	step DOWN
+	step DOWN
+	step DOWN
+	step_sleep 2
+	step_end
 
-;MrPokemonIntroText2:
-;	text "This is what I"
-;	line "want PROF.ELM to"
-;	cont "examine."
-;	done
+MrPokemonText_Invaluable:
+	text "PROF.OAK told me"
+	line "that you've been"
+	cont "helping with his"
+	cont "#DEX project."
 
-;MrPokemonsHouse_GotEggText:
-;	text "<PLAYER> received"
-;	line "MYSTERY EGG."
-;	done
+	para "He said your cont-"
+	line "ributions have"
+	cont "been invaluable."
 
-;MrPokemonIntroText3:
-;	text "I know a couple"
-;	line "who run a #MON"
-;	cont "DAY-CARE service."
-;
-;	para "They gave me that"
-;	line "EGG."
-;
-;	para "I was intrigued,"
-;	line "so I sent mail to"
-;	cont "PROF.ELM."
-;
-;	para "For #MON evolu-"
-;	line "tion, PROF.ELM is"
-;	cont "the authority."
-;	done
+	para "PROF.ELM also does"
+	line "#MON research."
 
-;MrPokemonIntroText4:
-;	text "Even PROF.OAK here"
-;	line "recognizes that."
-;	done
+	para "I'm sure he would"
+	line "love to meet you."
+	done
 
-;MrPokemonIntroText5:
-;	text "If my assumption"
-;	line "is correct, PROF."
-;	cont "ELM will know it."
-;	done
+MrPokemonsHouse_MrPokemonScript:
+	faceplayer
+	opentext
+	checkevent EVENT_SHOWED_RAINBOW_WING_TO_MR_POKEMON
+	iftrue .SkipRainbowWing
+	checkitem RAINBOW_WING
+	iffalse .SkipRainbowWing
+	writetext MrPokemonText_RainbowWing
+	promptbutton
+	setevent EVENT_SHOWED_RAINBOW_WING_TO_MR_POKEMON
+	sjump .GoVisitElm
 
-;MrPokemonsHouse_MrPokemonHealText:
-;	text "You are returning"
-;	line "to PROF.ELM?"
-;
-;	para "Here. Your #MON"
-;	line "should have some"
-;	cont "rest."
-;	done
+.SkipRainbowWing:
+	checkevent EVENT_SHOWED_SILVER_WING_TO_MR_POKEMON
+	iftrue .SkipSilverWing
+	checkitem SILVER_WING
+	iffalse .SkipSilverWing
+	writetext MrPokemonText_SilverWing
+	waitbutton
+	closetext
+	setevent EVENT_SHOWED_SILVER_WING_TO_MR_POKEMON
+	sjump .AlwaysNewDiscoveries
 
-;MrPokemonText_ImDependingOnYou:
-;	text "I'm depending on"
-;	line "you!"
-;	done
+.SkipSilverWing:
+	checkevent EVENT_MET_PROF_ELM
+	iffalse .GoVisitElm
+	checkevent EVENT_CLEARED_RADIO_TOWER
+	iffalse .GoSaveOak
+	checkitem GOLD_SCALE
+	iftrue .GoldScale
+	; fallthrough
 
-;MrPokemonText_AlwaysNewDiscoveries:
-;	text "Life is delight-"
-;	line "ful! Always, new"
-;
-;	para "discoveries to be"
-;	line "made!"
-;	done
+.AlwaysNewDiscoveries:
+	writetext MrPokemonText_AlwaysNewDiscoveries
+	waitbutton
+	closetext
+	end
 
-;MrPokemonsHouse_OakText1:
-;	text "OAK: Aha! So"
-;	line "you're <PLAY_G>!"
-;
-;	para "I'm OAK! A #MON"
-;	line "researcher."
-;
-;	para "I was just visit-"
-;	line "ing my old friend"
-;	cont "MR.#MON."
-;
-;	para "I heard you were"
-;	line "running an errand"
-;
-;	para "for PROF.ELM, so I"
-;	line "waited here."
-;
-;	para "Oh! What's this?"
-;	line "A rare #MON!"
-;
-;	para "Let's see…"
-;
-;	para "Hm, I see!"
-;
-;	para "I understand why"
-;	line "PROF.ELM gave you"
-;
-;	para "a #MON for this"
-;	line "errand."
-;
-;	para "To researchers"
-;	line "like PROF.ELM and"
-;
-;	para "I, #MON are our"
-;	line "friends."
-;
-;	para "He saw that you"
-;	line "would treat your"
-;
-;	para "#MON with love"
-;	line "and care."
-;
-;	para "…Ah!"
-;
-;	para "You seem to be"
-;	line "dependable."
-;
-;	para "How would you like"
-;	line "to help me out?"
-;
-;	para "See? This is the"
-;	line "latest version of"
-;	cont "#DEX."
-;
-;	para "It automatically"
-;	line "records data on"
-;
-;	para "#MON you've"
-;	line "seen or caught."
-;
-;	para "It's a hi-tech"
-;	line "encyclopedia!"
-;	done
+.GoVisitElm:
+	writetext MrPokemonText_VisitElm
+	waitbutton
+	closetext
+	end
 
-;MrPokemonsHouse_GetDexText:
-;	text "<PLAYER> received"
-;	line "#DEX!"
-;	done
+.GoSaveOak:
+	writetext MrPokemonText_SaveOak
+	waitbutton
+	closetext
+	end
 
-;MrPokemonsHouse_OakText2:
-;	text "Go meet many kinds"
-;	line "of #MON and"
-;
-;	para "complete that"
-;	line "#DEX!"
-;
-;	para "But I've stayed"
-;	line "too long."
-;
-;	para "I have to get to"
-;	line "GOLDENROD for my"
-;	cont "usual radio show."
-;
-;	para "<PLAY_G>, I'm"
-;	line "counting on you!"
-;	done
+.GoldScale:
+	writetext MrPokemonText_GimmeTheScale
+	yesorno
+	iffalse .refused
+	verbosegiveitem LUCKY_EGG
+	iffalse .full
+	takeitem GOLD_SCALE
+	sjump .AlwaysNewDiscoveries
 
-;MrPokemonText_GimmeTheScale:
-;	text "Hm? That SCALE!"
-;	line "What's that?"
-;	cont "A red GYARADOS?"
-;
-;	para "That's rare! "
-;	line "I, I want it…"
-;
-;	para "<PLAY_G>, would you"
-;	line "care to trade it?"
-;
-;	para "I can offer this"
-;	line "LUCKY EGG I got"
-;	cont "from PROF.OAK."
-;	done
+.refused
+	writetext MrPokemonText_Disappointed
+	waitbutton
+.full
+	closetext
+	end
 
-;MrPokemonText_Disappointed:
-;	text "That's disappoint-"
-;	line "ing. That happens"
-;	cont "to be very rare."
-;	done
+MrPokemonText_AlwaysNewDiscoveries:
+	text "Life is delight-"
+	line "ful! Always, new"
+	cont "discoveries to be"
+	line "made!"
 
-;MrPokemonsHouse_ForeignMagazinesText:
-;	text "It's packed with"
-;	line "foreign magazines."
-;
-;	para "Can't even read"
-;	line "their titles…"
-;	done
+	para "If you find any-"
+	line "thing rare, please"
+	cont "show it to me!"
+	done
 
-;MrPokemonsHouse_BrokenComputerText:
-;	text "It's a big com-"
-;	line "puter. Hmm. It's"
-;	cont "broken."
-;	done
+MrPokemonText_RainbowWing:
+	text "What's that?"
+	line "A rainbow-colored"
+	cont "#MON flew by?"
 
-;MrPokemonsHouse_StrangeCoinsText:
-;	text "A whole pile of"
-;	line "strange coins!"
-;
-;	para "Maybe they're from"
-;	line "another country…"
-;	done
+	para "That sounds like"
+	line "the #MON from"
+	cont "the legend of the"
+	cont "TIN TOWER in"
+	cont "ECRUTEAK CITY!"
+
+	para "PROF.ELM claims he"
+	line "saw that #MON"
+	cont "when he was young."
+
+	para "Hm? That FEATHER!"
+	line "It came from the"
+	cont "#MON you saw?"
+
+	para "PROF.ELM would"
+	line "love to see that!"
+	done
+
+MrPokemonText_SilverWing:
+	text "Hm? That FEATHER?"
+	line "It's silver!"
+
+	para "It reminds me of"
+	line "the legend of a"
+	cont "#MON from the"
+	cont "BRASS TOWER in"
+	cont "ECRUTEAK CITY."
+
+	para "But that tower"
+	line "burned down, and"
+	cont "the #MON hasn't"
+	cont "been seen since."
+
+	para "I wonder, where"
+	line "has it been hiding"
+	cont "all this time?"
+	done
+
+MrPokemonText_GimmeTheScale:
+	text "Hm? That SCALE!"
+	line "What's that?"
+	cont "A gold MAGIKARP?"
+
+	para "I've always loved"
+	line "MAGIKARP!"
+
+	para "That SCALE is so"
+	line "unique, I wish it"
+	cont "were mine."
+
+	para "<PLAYER>, would you"
+	line "care to trade it?"
+
+	para "I can offer this"
+	line "LUCKY EGG I got"
+	cont "from PROF.OAK."
+	done
+
+MrPokemonText_Disappointed:
+	text "That's disappoint-"
+	line "ing. That happens"
+	cont "to be very rare."
+	done
+
+MrPokemonText_SaveOak:
+	text "There's trouble at"
+	line "the RADIO TOWER?"
+
+	para "I hope PROF.OAK"
+	line "is okay!"
+	done
+
+MrPokemonText_VisitElm:
+	text "You should pay"
+	line "PROF.ELM a visit."
+
+	para "His lab is nearby,"
+	line "in NEW BARK TOWN."
+
+	para "If you go south to"
+	line "CHERRYGROVE first,"
+	cont "NEW BARK is east"
+	cont "from there."
+	done
+
+MrPokemonsHouse_ForeignMagazines:
+	jumptext MrPokemonsHouse_ForeignMagazinesText
+MrPokemonsHouse_ForeignMagazinesText:
+	text "It's packed with"
+	line "foreign magazines."
+
+	para "Can't even read"
+	line "their titles…"
+	done
+
+MrPokemonsHouse_BrokenComputer:
+	jumptext MrPokemonsHouse_BrokenComputerText
+MrPokemonsHouse_BrokenComputerText:
+	text "It's a big com-"
+	line "puter. Hmm. It's"
+	cont "broken."
+	done
+
+MrPokemonsHouse_StrangeCoins:
+	jumptext MrPokemonsHouse_StrangeCoinsText
+MrPokemonsHouse_StrangeCoinsText:
+	text "A whole pile of"
+	line "strange coins!"
+
+	para "Maybe they're from"
+	line "another country…"
+	done
 
 MrPokemonsHouse_MapEvents:
 	db 0, 0 ; filler
@@ -385,12 +360,17 @@ MrPokemonsHouse_MapEvents:
 	def_coord_events
 
 	def_bg_events
-;	bg_event  0,  1, BGEVENT_READ, MrPokemonsHouse_ForeignMagazines
-;	bg_event  1,  1, BGEVENT_READ, MrPokemonsHouse_ForeignMagazines
-;	bg_event  6,  1, BGEVENT_READ, MrPokemonsHouse_BrokenComputer
-;	bg_event  7,  1, BGEVENT_READ, MrPokemonsHouse_BrokenComputer
-;	bg_event  6,  4, BGEVENT_READ, MrPokemonsHouse_StrangeCoins
+	bg_event  0,  1, BGEVENT_READ, MrPokemonsHouse_ForeignMagazines
+	bg_event  1,  1, BGEVENT_READ, MrPokemonsHouse_ForeignMagazines
+	bg_event  6,  1, BGEVENT_READ, MrPokemonsHouse_BrokenComputer
+	bg_event  7,  1, BGEVENT_READ, MrPokemonsHouse_BrokenComputer
+	bg_event  6,  4, BGEVENT_READ, MrPokemonsHouse_StrangeCoins
+;	bg_event  7,  4, BGEVENT_UP, MrPokemonsHouse_BrokenComputer
 
 	def_object_events
-;	object_event  3,  5, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MrPokemonsHouse_MrPokemonScript, -1
-;	object_event  6,  5, SPRITE_OAK, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MR_POKEMONS_HOUSE_OAK
+	object_event  2,  4, SPRITE_OAK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MR_POKEMONS_HOUSE_OAK
+	object_event  3,  4, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MrPokemonsHouse_MrPokemonScript, EVENT_MET_PROF_ELM
+	object_event  6,  5, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, MORN | DAY, 0, OBJECTTYPE_SCRIPT, 0, MrPokemonsHouse_MrPokemonScript, EVENT_MR_POKEMONS_HOUSE_MR_POKEMON
+	object_event  3,  1, SPRITE_GENTLEMAN, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, EVE | NITE, 0, OBJECTTYPE_SCRIPT, 0, MrPokemonsHouse_MrPokemonScript, EVENT_MR_POKEMONS_HOUSE_MR_POKEMON
+
+; MORN | DAY | EVE | NITE
