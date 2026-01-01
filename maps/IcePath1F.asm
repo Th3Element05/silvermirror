7a13da66fd9fa521fa38da2767eb5f2971ad8ef0
Sean Crawford <scrawford@sumnerone.com>
2025-12-31 22:04:49 -0600

    re-did ice path for reverse traversal

--- maps/IcePath1F.asm
+++ maps/IcePath1F.asm
@@ -1,7 +1,4 @@
 	object_const_def
-;	const ICEPATH1F_POKE_BALL1
-;	const ICEPATH1F_POKE_BALL2
-;	const ICEPATH1F_POKE_BALL3
 
 IcePath1F_MapScripts:
 	def_scene_scripts
@@ -11,20 +8,20 @@ IcePath1F_MapScripts:
 ;IcePath1FHMWaterfall:
 ;	itemball HM_WATERFALL
 
-;IcePath1FPPUp:
-;	itemball PP_UP
+IcePath1FPPUp:
+	itemball PP_UP
 
-;IcePath1FProtein:
-;	itemball PROTEIN
+IcePath1FProtein:
+	itemball PROTEIN
 
 IcePath1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  4, 19, ROUTE_44, 1
-	warp_event 36, 27, BLACKTHORN_CITY, 7
-	warp_event 37,  5, ICE_PATH_B1F, 1
-	warp_event 37, 13, ICE_PATH_B1F, 7
+	warp_event  4, 19, ROUTE_44, 1 ;1
+	warp_event 36, 27, BLACKTHORN_CITY, 7 ;2
+	warp_event 37,  5, ICE_PATH_B1F, 1 ;3
+	warp_event 37, 13, ICE_PATH_B1F, 7 ;4
 
 	def_coord_events
 
@@ -32,5 +29,5 @@ IcePath1F_MapEvents:
 
 	def_object_events
 ;	object_event 31,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePath1FHMWaterfall, EVENT_GOT_HM07_WATERFALL
-;	object_event 32, 23, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePath1FPPUp, EVENT_ICE_PATH_1F_PP_UP
-;	object_event 35,  9, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePath1FProtein, EVENT_ICE_PATH_1F_PROTEIN
+	object_event  3, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePath1FPPUp, EVENT_ICE_PATH_1F_PP_UP
+	object_event 15,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePath1FProtein, EVENT_ICE_PATH_1F_PROTEIN
