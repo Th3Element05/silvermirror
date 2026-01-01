40170823508aa67268b369672780739a0b8108e2
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-12-31 22:04:49 -0600

    re-did ice path for reverse traversal

--- maps/IcePathB2FBlackthornSide.asm
+++ maps/IcePathB2FBlackthornSide.asm
@@ -1,13 +1,12 @@
 	object_const_def
-;	const ICEPATHB2FBLACKTHORNSIDE_POKE_BALL
 
 IcePathB2FBlackthornSide_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-;IcePathB2FBlackthornSideTMRest:
-;	itemball TM_REST
+IcePathB2FBlackthornSideTMRest:
+	itemball ICE_HEAL
 
 ;IcePathB2FBlackthornSideHiddenIceHeal:
 ;	hiddenitem ICE_HEAL, EVENT_ICE_PATH_B2F_BLACKTHORN_SIDE_HIDDEN_ICE_HEAL
@@ -16,8 +15,8 @@ IcePathB2FBlackthornSide_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  3, 15, ICE_PATH_B1F, 8
-	warp_event  3,  3, ICE_PATH_B3F, 2
+	warp_event  3, 15, ICE_PATH_B1F, 8 ;1
+	warp_event  3,  3, ICE_PATH_B3F, 2 ;2
 
 	def_coord_events
 
@@ -25,4 +24,4 @@ IcePathB2FBlackthornSide_MapEvents:
 ;	bg_event  2, 10, BGEVENT_ITEM, IcePathB2FBlackthornSideHiddenIceHeal
 
 	def_object_events
-;	object_event  8, 16, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePathB2FBlackthornSideTMRest, EVENT_ICE_PATH_B2F_BLACKTHORN_SIDE_TM_REST
+	object_event  9, 16, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, IcePathB2FBlackthornSideTMRest, EVENT_ICE_PATH_B2F_BLACKTHORN_SIDE_ICE_HEAL
