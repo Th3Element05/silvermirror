b107e84fdd9c88c145aa1881e1735020fb3786d4
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2025-12-31 22:02:26 -0600

    scripted sprout tower

--- maps/SproutTower1F.asm
+++ maps/SproutTower1F.asm
@@ -1,44 +1,19 @@
 	object_const_def
-	const SPROUTTOWER1F_SAGE1
-	const SPROUTTOWER1F_SAGE2
-	const SPROUTTOWER1F_GRANNY
-	const SPROUTTOWER1F_TEACHER
-;	const SPROUTTOWER1F_SAGE3
-	const SPROUTTOWER1F_POKE_BALL
 
 SproutTower1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-SproutTower1FSage1Script:
-	jumptextfaceplayer SproutTower1FSage1Text
-
-SproutTower1FSage2Script:
-	jumptextfaceplayer SproutTower1FSage2Text
-
-SproutTower1FGrannyScript:
-	jumptextfaceplayer SproutTower1FGrannyText
-
-SproutTower1FTeacherScript:
-	jumptextfaceplayer SproutTower1FTeacherText
-
-;TrainerSageChow:
-;	trainer SAGE, CHOW, EVENT_BEAT_SAGE_CHOW, SageChowSeenText, SageChowBeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext SageChowAfterBattleText
-;	waitbutton
-;	closetext
-;	end
-
-SproutTower1FParlyzHeal:
-	itemball PARLYZ_HEAL
-
-SproutTower1FStatue:
-	jumptext SproutTower1FStatueText
+TrainerSageChow:
+	trainer SAGE, CHOW, EVENT_BEAT_SAGE_CHOW, SageChowSeenText, SageChowBeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext SageChowAfterBattleText
+	waitbutton
+	closetext
+	end
 
 SageChowSeenText:
 	text "We stand guard in"
@@ -62,20 +37,25 @@ SageChowAfterBattleText:
 	line "thankful for this."
 	done
 
+SproutTower1FSage1Script:
+	jumptextfaceplayer SproutTower1FSage1Text
 SproutTower1FSage1Text:
 	text "Only if you reach"
 	line "the top will you"
 	cont "obtain an HM."
 	done
 
+SproutTower1FSage2Script:
+	jumptextfaceplayer SproutTower1FSage2Text
 SproutTower1FSage2Text:
 	text "SPROUT TOWER was"
 	line "built long ago"
-
-	para "as a place for"
-	line "#MON training."
+	cont "as a place for"
+	cont "#MON training."
 	done
 
+SproutTower1FGrannyScript:
+	jumptextfaceplayer SproutTower1FGrannyText
 SproutTower1FGrannyText:
 	text "A BELLSPROUT over"
 	line "100 feet tall…"
@@ -85,6 +65,8 @@ SproutTower1FGrannyText:
 	cont "pillar here."
 	done
 
+SproutTower1FTeacherScript:
+	jumptextfaceplayer SproutTower1FTeacherText
 SproutTower1FTeacherText:
 	text "See the pillar"
 	line "shaking?"
@@ -93,6 +75,8 @@ SproutTower1FTeacherText:
 	line "ing upstairs."
 	done
 
+SproutTower1FStatue:
+	jumptext SproutTower1FStatueText
 SproutTower1FStatueText:
 	text "A #MON statue…"
 
@@ -100,26 +84,29 @@ SproutTower1FStatueText:
 	line "distinguished."
 	done
 
+SproutTower1FParlyzHeal:
+	itemball PARLYZ_HEAL
+
 SproutTower1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  9, 15, VIOLET_CITY, 7
-	warp_event 10, 15, VIOLET_CITY, 7
-	warp_event  6,  4, SPROUT_TOWER_2F, 1
-	warp_event  2,  6, SPROUT_TOWER_2F, 2
-	warp_event 17,  3, SPROUT_TOWER_2F, 3
+	warp_event  7, 15, VIOLET_CITY, 7
+	warp_event  8, 15, VIOLET_CITY, 7
+	warp_event  4,  4, SPROUT_TOWER_2F, 1
+	warp_event  0,  6, SPROUT_TOWER_2F, 2
+	warp_event 15,  3, SPROUT_TOWER_2F, 3
 
 	def_coord_events
 
 	def_bg_events
-	bg_event  7, 15, BGEVENT_READ, SproutTower1FStatue
-	bg_event 12, 15, BGEVENT_READ, SproutTower1FStatue
+	bg_event  5, 15, BGEVENT_READ, SproutTower1FStatue
+	bg_event 10, 15, BGEVENT_READ, SproutTower1FStatue
 
 	def_object_events
-	object_event  7,  4, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SproutTower1FSage1Script, -1
-	object_event  6,  7, SPRITE_SAGE, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SproutTower1FSage2Script, -1
-	object_event 11, 12, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SproutTower1FGrannyScript, -1
-	object_event  9,  9, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SproutTower1FTeacherScript, -1
-;	object_event  3,  5, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 4, TrainerSageChow, -1
-;	object_event 16,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SproutTower1FParlyzHeal, EVENT_SPROUT_TOWER_1F_PARLYZ_HEAL
+	object_event  1,  5, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 4, TrainerSageChow, -1
+;	object_event  7,  4, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SproutTower1FSage1Script, -1
+	object_event  4,  7, SPRITE_SAGE, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SproutTower1FSage2Script, -1
+	object_event  9, 12, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SproutTower1FGrannyScript, -1
+	object_event  7,  9, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SproutTower1FTeacherScript, -1
+	object_event 14,  7, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SproutTower1FParlyzHeal, EVENT_SPROUT_TOWER_1F_PARLYZ_HEAL
