49c12fb363ce84fe03abace87c71c7a2b4ed89da
Sean Crawford <scrawford@sumnerone.com>
2025-12-31 22:07:04 -0600

    scripted radio tower events

--- maps/RadioTower3F.asm
+++ maps/RadioTower3F.asm
@@ -1,156 +1,186 @@
 	object_const_def
-;	const RADIOTOWER3F_SUPER_NERD
-;	const RADIOTOWER3F_GYM_GUIDE
-;	const RADIOTOWER3F_COOLTRAINER_F
-;	const RADIOTOWER3F_ROCKET1
-;	const RADIOTOWER3F_ROCKET2
-;	const RADIOTOWER3F_ROCKET3
-;	const RADIOTOWER3F_SCIENTIST
+	const RADIOTOWER3F_BLUE
 
 RadioTower3F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
-;	callback MAPCALLBACK_TILES, RadioTower3FCardKeyShutterCallback
+	callback MAPCALLBACK_TILES, RadioTower3FCardKeyShutterCallback
 
-;RadioTower3FCardKeyShutterCallback:
-;	checkevent EVENT_USED_RADIO_TOWER_CARD_KEY
-;	iffalse .ShutDoor
-;	endcallback
+RadioTower3FCardKeyShutterCallback:
+	checkflag ENGINE_ROCKETS_IN_RADIO_TOWER
+	iffalse .StayOpen
+	checkevent EVENT_USED_RADIO_TOWER_CARD_KEY
+	iftrue .StayOpen
+	changeblock 14, 2, $28 ; closed key slot
+	changeblock 14, 4, $29 ; closed gate
+.StayOpen
+	endcallback
+
+; takeover
+RadioTower3FBlueScript:
+	jumptextfaceplayer RadioTower3FBlueText
+RadioTower3FBlueText:
+	text "<RIVAL>: <PLAYER>!"
+	
+	para "GRAMPS is through"
+	line "here, but the gate"
+	cont "is locked!"
+	done
+
+; -1
+RadioTower3FCooltrainerFScript:
+	checkevent EVENT_GOT_NUGGET_FROM_RADIO_TOWER
+	iftrue .GotNugget
+	checkevent EVENT_CLEARED_RADIO_TOWER
+	iftrue .SavedOak
+	checkevent EVENT_USED_RADIO_TOWER_CARD_KEY
+	iftrue .UsedCardKey
+	checkevent EVENT_GOT_RADIO_TOWER_CARD_KEY
+	iftrue .GotCardKey
+	jumptextfaceplayer RadioTower3FCooltrainerFLockedGateText
+
+.GotCardKey:
+	jumptextfaceplayer RadioTower3FCooltrainerFHaveKeyText
+
+.UsedCardKey:
+	jumptextfaceplayer RadioTower3FCooltrainerFGateOpenText
+
+.SavedOak:
+	faceplayer
+	opentext
+	writetext RadioTower3FCooltrainerFYoureAHeroText
+	promptbutton
+	verbosegiveitem NUGGET
+	iffalse .NoRoom
+	setevent EVENT_GOT_NUGGET_FROM_RADIO_TOWER
+.NoRoom:
+	closetext
+	end
+
+.GotNugget:
+	jumptextfaceplayer RadioTower3FCooltrainerFYouWereMarvelousText
+
+RadioTower3FCooltrainerFLockedGateText:
+	text "Some maniac ran"
+	line "through and locked"
+	cont "the security gate"
+	cont "behind him!"
+
+	para "But the DIRECTOR"
+	line "can open it."
+
+	para "He's up on the"
+	line "fifth floor."
+	done
+
+RadioTower3FCooltrainerFHaveKeyText:
+	text "The DIRECTOR gave"
+	line "you his card key?"
+
+	para "Use it to open"
+	line "the gate!"
+	done
+
+RadioTower3FCooltrainerFGateOpenText:
+	text "You got the gate"
+	line "open!"
+	done
+
+RadioTower3FCooltrainerFYoureAHeroText:
+	text "Thank you!"
+	line "You're a hero!"
+
+	para "This is a token of"
+	line "my appreciation."
+	done
+
+;RadioTower3FCooltrainerFBrightpowderText:
+;	text "It's BRIGHTPOWDER."
 ;
-;.ShutDoor:
-;	changeblock 14, 2, $28 ; closed key slot
-;	changeblock 14, 4, $29 ; closed shutter
-;	endcallback
+;	para "A #MON will be"
+;	cont "harder to hit if"
+;	cont "it holds it."
+;	done
 
-;RadioTower3FSuperNerdScript:
-;	jumptextfaceplayer RadioTower3FSuperNerdText
+RadioTower3FCooltrainerFYouWereMarvelousText:
+	text "You were simply"
+	line "marvelous!"
+	done
 
-;RadioTower3FGymGuideScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_CLEARED_RADIO_TOWER
-;	iftrue .NoRockets
-;	writetext RadioTower3FGymGuideText_Rockets
-;	waitbutton
-;	closetext
-;	end
-;
-;.NoRockets:
-;	writetext RadioTower3FGymGuideText
-;	waitbutton
-;	closetext
-;	end
+; civilians
+RadioTower3FGymGuideScript:
+	jumptextfaceplayer RadioTower3FGymGuideText
+RadioTower3FGymGuideText:
+	text "We run 24 hours a"
+	line "day to broadcast"
+	cont "entertaining pro-"
+	cont "grams."
 
-;RadioTower3FCooltrainerFScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_GOT_SUNNY_DAY_FROM_RADIO_TOWER
-;	iftrue .GotSunnyDay
-;	checkevent EVENT_CLEARED_RADIO_TOWER
-;	iftrue .NoRockets
-;	checkevent EVENT_USED_RADIO_TOWER_CARD_KEY
-;	iftrue .UsedCardKey
-;	writetext RadioTower3FCooltrainerFPleaseSaveDirectorText
-;	waitbutton
-;	closetext
-;	end
-;
-;.UsedCardKey:
-;	writetext RadioTower3FCooltrainerFIsDirectorSafeText
-;	waitbutton
-;	closetext
-;	end
-;
-;.NoRockets:
-;	writetext RadioTower3FCooltrainerFYoureMyHeroText
-;	promptbutton
-;	verbosegiveitem TM_SUNNY_DAY
-;	iffalse .NoRoom
-;	writetext RadioTower3FCooltrainerFItsSunnyDayText
-;	waitbutton
-;	closetext
-;	setevent EVENT_GOT_SUNNY_DAY_FROM_RADIO_TOWER
-;	end
+	para "I'll do my best to"
+	line "run around the"
+	cont "clock too!"
+	done
 
-;.GotSunnyDay:
-;	writetext RadioTower3FCooltrainerFYouWereMarvelousText
-;	waitbutton
-;.NoRoom:
-;	closetext
-;	end
+; after
+RadioTower3FSuperNerdScript:
+	jumptextfaceplayer RadioTower3FSuperNerdText
+RadioTower3FSuperNerdText:
+	text "We have recordings"
+	line "of the cries of"
+	cont "all #MON that"
+	cont "have been found."
 
-;TrainerGruntM7:
-;	trainer GRUNTM, GRUNTM_7, EVENT_BEAT_ROCKET_GRUNTM_7, GruntM7SeenText, GruntM7BeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext GruntM7AfterBattleText
-;	waitbutton
-;	closetext
-;	end
+	para "We must have over"
+	line "200 kinds."
+	done
 
-;TrainerGruntM8:
-;	trainer GRUNTM, GRUNTM_8, EVENT_BEAT_ROCKET_GRUNTM_8, GruntM8SeenText, GruntM8BeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext GruntM8AfterBattleText
-;	waitbutton
-;	closetext
-;	end
-
-;TrainerGruntM9:
-;	trainer GRUNTM, GRUNTM_9, EVENT_BEAT_ROCKET_GRUNTM_9, GruntM9SeenText, GruntM9BeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext GruntM9AfterBattleText
-;	waitbutton
-;	closetext
-;	end
-
-;TrainerScientistMarc:
-;	trainer SCIENTIST, MARC, EVENT_BEAT_SCIENTIST_MARC, ScientistMarcSeenText, ScientistMarcBeatenText, 0, .Script
-;
-;.Script:
-;	endifjustbattled
-;	opentext
-;	writetext ScientistMarcAfterBattleText
-;	waitbutton
-;	closetext
-;	end
-
-CardKeySlotScript::
+; misc
+CardKeySlotScript:: ;export for using CARD KEY from bag
 	opentext
 	writetext RadioTower3FCardKeySlotText
 	waitbutton
 	checkevent EVENT_USED_RADIO_TOWER_CARD_KEY
 	iftrue .EndCardKey
-	checkitem CARD_KEY
-	iffalse .EndCardKey
-	writetext InsertedTheCardKeyText
-	waitbutton
-	checkevent EVENT_GOT_RADIO_TOWER_CARD_KEY
+	checkitem CARD_KEY_R
 	iftrue .Open
-	playsound SFX_WRONG
-	writetext WrongCardKeyText
+	checkitem CARD_KEY
+	iftrue .WrongCardKey
+	writetext RadioTower3FNeedCardKeyText
 	waitbutton
-.EndCardKey:
+.EndCardKey
+	closetext
+	end
+
+.WrongCardKey:
+	writetext RadioTower3FInsertedTheCardKeyText
+	waitbutton
+	playsound SFX_WRONG
+	writetext RadioTower3FWrongCardKeyText
+	waitbutton
 	closetext
 	end
 
 .Open:
-;	setevent EVENT_USED_RADIO_TOWER_CARD_KEY
-;	playsound SFX_ENTER_DOOR
-;	changeblock 14, 2, $2a ; open shutter
-;	changeblock 14, 4, $01 ; floor
-;	reloadmappart
+	writetext RadioTower3FInsertedTheCardKeyText
+	waitbutton
+	playsound SFX_ENTER_DOOR
+	changeblock 14, 2, $2a ; open shutter
+	changeblock 14, 4, $01 ; floor
+	reloadmappart
 	closetext
+	setevent EVENT_USED_RADIO_TOWER_CARD_KEY
 	waitsfx
+	showemote EMOTE_SHOCK, RADIOTOWER3F_BLUE, 15
+	turnobject RADIOTOWER3F_BLUE, UP
+	opentext
+	writetext RadioTower3FBlue_GateOpenText
+	waitbutton
+	closetext
+	applymovement RADIOTOWER3F_BLUE, RadioTower3FBlueRunsUpstairsMovement
+	playsound SFX_EXIT_BUILDING
+	disappear RADIOTOWER3F_BLUE
+	clearevent EVENT_RADIO_TOWER_4F_BLUE
 	end
 
 RadioTower3FCardKeySlotText:
@@ -158,205 +188,72 @@ RadioTower3FCardKeySlotText:
 	line "slot."
 	done
 
-InsertedTheCardKeyText:
+RadioTower3FNeedCardKeyText:
+	text "You don't have a"
+	line "CARD KEY."
+	done
+
+RadioTower3FInsertedTheCardKeyText:
 	text "<PLAYER> inserted"
 	line "the CARD KEY."
 	done
 
-WrongCardKeyText:
+RadioTower3FWrongCardKeyText:
 	text "The CARD KEY from"
 	line "SILPH CO. doesn't"
 	cont "work here."
 	done
 
+RadioTower3FBlue_GateOpenText:
+	text "<RIVAL>: You got"
+	line "the gate open!"
 
-;RadioTower3FPersonnelSign:
-;	jumptext RadioTower3FPersonnelSignText
+	para "Let's go!"
+	done
 
-;RadioTower3FPokemonMusicSign:
-;	jumptext RadioTower3FPokemonMusicSignText
+RadioTower3FBlueRunsUpstairsMovement:
+	big_step RIGHT
+	big_step RIGHT
+	big_step RIGHT
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	step_end
 
-;RadioTower3FSuperNerdText:
-;	text "We have recordings"
-;	line "of the cries of"
-;
-;	para "all #MON that"
-;	line "have been found."
-;
-;	para "We must have about"
-;	line "200 kinds."
-;	done
+RadioTower3FPersonnelSign:
+	jumptext RadioTower3FPersonnelSignText
+RadioTower3FPersonnelSignText:
+	text "3F PERSONNEL"
+	done
 
-;RadioTower3FGymGuideText_Rockets:
-;	text "To trainers, #-"
-;	line "MON are their"
-;	cont "beloved partners."
-;
-;	para "It's terrible how"
-;	line "TEAM ROCKET is"
-;
-;	para "trying to control"
-;	line "#MON."
-;	done
-
-;RadioTower3FGymGuideText:
-;	text "We run 24 hours a"
-;	line "day to broadcast"
-;
-;	para "entertaining pro-"
-;	line "grams."
-;
-;	para "I'll do my best to"
-;	line "run around the"
-;	cont "clock too!"
-;	done
-
-;RadioTower3FCooltrainerFPleaseSaveDirectorText:
-;	text "The TEAM ROCKET"
-;	line "boss has locked"
-;	cont "himself in."
-;
-;	para "But the DIRECTOR"
-;	line "can open it."
-;
-;	para "He's up on the"
-;	line "fifth floor."
-;
-;	para "Please save him!"
-;	done
-
-;RadioTower3FCooltrainerFIsDirectorSafeText:
-;	text "Is the DIRECTOR"
-;	line "safe?"
-;	done
-
-;RadioTower3FCooltrainerFYoureMyHeroText:
-;	text "Thank you!"
-;	line "You're my hero!"
-;
-;	para "This is a token of"
-;	line "my appreciation."
-;	done
-
-;RadioTower3FCooltrainerFItsSunnyDayText:
-;	text "It's SUNNY DAY."
-;	line "It powers up fire-"
-;	cont "type moves for a"
-;	cont "while."
-;	done
-
-;RadioTower3FCooltrainerFYouWereMarvelousText:
-;	text "You were simply"
-;	line "marvelous!"
-;	done
-
-;GruntM7SeenText:
-;	text "I've been given"
-;	line "strict orders."
-;
-;	para "I'm to crush any-"
-;	line "one who challenges"
-;	cont "TEAM ROCKET!"
-;	done
-
-;GruntM7BeatenText:
-;	text "What?!"
-;	done
-
-;GruntM7AfterBattleText:
-;	text "I failed in my"
-;	line "duties…"
-;
-;	para "I'll be docked pay"
-;	line "for this…"
-;	done
-
-;GruntM8SeenText:
-;	text "It feels great"
-;	line "ordering #MON"
-;	cont "to commit crimes."
-;	done
-
-;GruntM8BeatenText:
-;	text "You're kidding!"
-;	done
-
-;GruntM8AfterBattleText:
-;	text "I feel lousy over"
-;	line "losing!"
-;
-;	para "Darn it! I hate"
-;	line "useless #MON!"
-;	done
-
-;GruntM9SeenText:
-;	text "Why did the shut-"
-;	line "ter open? Did you"
-;
-;	para "have something to"
-;	line "do with this?"
-;	done
-
-;GruntM9BeatenText:
-;	text "I'm done for!"
-;	done
-
-;GruntM9AfterBattleText:
-;	text "What?! You made it"
-;	line "past our men in"
-;	cont "the UNDERGROUND?"
-;
-;	para "How could you?"
-;	done
-
-;ScientistMarcSeenText:
-;	text "An unknown child"
-;	line "wandering here?"
-;
-;	para "Who are you?"
-;	done
-
-;ScientistMarcBeatenText:
-;	text "Tch! I took you"
-;	line "too lightly!"
-;	done
-
-;ScientistMarcAfterBattleText:
-;	text "Bwahahaha…"
-;
-;	para "I can transmit as"
-;	line "strong a signal as"
-;	cont "I need from here."
-;	done
-
-;RadioTower3FPersonnelSignText:
-;	text "3F PERSONNEL"
-;	done
-
-;RadioTower3FPokemonMusicSignText:
-;	text "#MON MUSIC with"
-;	line "Host DJ BEN"
-;	done
+RadioTower3FPokemonMusicSign:
+	jumptext RadioTower3FPokemonMusicSignText
+RadioTower3FPokemonMusicSignText:
+	text "#MON MUSIC with"
+	line "Host DJ BEN"
+	done
 
 RadioTower3F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  0,  0, RADIO_TOWER_2F, 1
-	warp_event  7,  0, RADIO_TOWER_4F, 2
-	warp_event 17,  0, RADIO_TOWER_4F, 4
+	warp_event  0,  0, RADIO_TOWER_2F, 1 ;1
+	warp_event  7,  0, RADIO_TOWER_4F, 2 ;2
+	warp_event 17,  0, RADIO_TOWER_4F, 4 ;3
 
 	def_coord_events
 
 	def_bg_events
-;	bg_event  3,  0, BGEVENT_READ, RadioTower3FPersonnelSign
-;	bg_event  9,  0, BGEVENT_READ, RadioTower3FPokemonMusicSign
-;	bg_event 14,  2, BGEVENT_UP, CardKeySlotScript
+	bg_event  3,  0, BGEVENT_READ, RadioTower3FPersonnelSign
+	bg_event  9,  0, BGEVENT_READ, RadioTower3FPokemonMusicSign
+	bg_event 14,  2, BGEVENT_UP, CardKeySlotScript
 
 	def_object_events
-;	object_event  7,  4, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, RadioTower3FSuperNerdScript, EVENT_RADIO_TOWER_CIVILIANS_AFTER
-;	object_event  3,  4, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, RadioTower3FGymGuideScript, -1
-;	object_event 11,  3, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, RadioTower3FCooltrainerFScript, -1
+	object_event 14,  4, SPRITE_BLUE, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, RadioTower3FBlueScript, EVENT_RADIO_TOWER_3F_BLUE
+	object_event 11,  3, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, RadioTower3FCooltrainerFScript, -1
+	object_event  3,  4, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, RadioTower3FGymGuideScript, EVENT_RADIO_TOWER_CIVILIANS
+	object_event  7,  4, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, RadioTower3FSuperNerdScript, EVENT_RADIO_TOWER_CIVILIANS_AFTER
 ;	object_event  5,  1, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 2, TrainerGruntM7, EVENT_RADIO_TOWER_ROCKET_TAKEOVER
 ;	object_event  6,  2, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 3, TrainerGruntM8, EVENT_RADIO_TOWER_ROCKET_TAKEOVER
 ;	object_event 16,  6, SPRITE_ROCKET, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 3, TrainerGruntM9, EVENT_RADIO_TOWER_ROCKET_TAKEOVER
