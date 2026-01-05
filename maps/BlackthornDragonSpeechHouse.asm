93fbca25ee81f585016f54fc5f414198f96a07c5
Sean Crawford <scrawford@sumnerone.com>
2026-01-05 17:44:04 -0600

    scripted blackthorn city locations

--- maps/BlackthornDragonSpeechHouse.asm
+++ maps/BlackthornDragonSpeechHouse.asm
@@ -1,47 +1,43 @@
 	object_const_def
-;	const BLACKTHORNDRAGONSPEECHHOUSE_GRANNY
-;	const BLACKTHORNDRAGONSPEECHHOUSE_EKANS
 
 BlackthornDragonSpeechHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-;BlackthornDragonSpeechHouseGrannyScript:
-;	jumptextfaceplayer BlackthornDragonSpeechHouseGrannyText
+BlackthornDragonSpeechHouseGrannyScript:
+	jumptextfaceplayer BlackthornDragonSpeechHouseGrannyText
+BlackthornDragonSpeechHouseGrannyText:
+	text "A clan of trainers"
+	line "who can freely"
+	cont "command dragons"
+	cont "live right here in"
+	cont "BLACKTHORN."
 
-;BlackthornDragonSpeechHouseDratiniScript:
-;	opentext
-;	writetext BlackthornDragonSpeechHouseDratiniText
-;	cry DRATINI
-;	waitbutton
-;	closetext
-;	end
+	para "As a result, there"
+	line "are many legends"
+	cont "about dragons in"
+	cont "this town."
+	done
+
+BlackthornDragonSpeechHouseDratiniScript:
+	faceplayer
+	opentext
+	writetext BlackthornDragonSpeechHouseDratiniText
+	cry DRATINI
+	waitbutton
+	closetext
+	end
+
+BlackthornDragonSpeechHouseDratiniText:
+	text "DRATINI: Draa!"
+	done
 
 ;BlackthornDragonSpeechHousePictureBookshelf: ; unreferenced
 ;	jumpstd PictureBookshelfScript
 
 ;BlackthornDragonSpeechHouseMagazineBookshelf: ; unreferenced
 ;	jumpstd MagazineBookshelfScript
-
-;BlackthornDragonSpeechHouseGrannyText:
-;	text "A clan of trainers"
-;	line "who can freely"
-;
-;	para "command dragons"
-;	line "live right here in"
-;	cont "BLACKTHORN."
-;
-;	para "As a result, there"
-;	line "are many legends"
-;
-;	para "about dragons in"
-;	line "this town."
-;	done
-
-;BlackthornDragonSpeechHouseDratiniText:
-;	text "DRATINI: Draa!"
-;	done
 
 BlackthornDragonSpeechHouse_MapEvents:
 	db 0, 0 ; filler
@@ -55,5 +51,5 @@ BlackthornDragonSpeechHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-;	object_event  2,  3, SPRITE_GRANNY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseGrannyScript, -1
-;	object_event  5,  5, SPRITE_DRATINI, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseDratiniScript, -1
+	object_event  2,  3, SPRITE_GRANNY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseGrannyScript, -1
+	object_event  5,  5, SPRITE_DRATINI_MOVE, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, BlackthornDragonSpeechHouseDratiniScript, -1
