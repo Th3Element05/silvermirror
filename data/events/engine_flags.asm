1167d6b3c4f373c9ca955cbbd5ddd74425c3a136
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-05 17:29:18 -0600

    cleaned up engine flags/tweaked for radio tower

--- data/events/engine_flags.asm
+++ data/events/engine_flags.asm
@@ -35,20 +35,21 @@ EngineFlags:
 
 	engine_flag wUnusedTwoDayTimerOn, 0 ; unused, possibly related to a 2-day timer
 
+;(see constants\wram_constants.asm)
 	engine_flag wStatusFlags, STATUSFLAGS_POKEDEX_F
 	engine_flag wStatusFlags, STATUSFLAGS_UNOWN_DEX_F
 	engine_flag wStatusFlags, STATUSFLAGS_CAUGHT_POKERUS_F
-	engine_flag wStatusFlags, STATUSFLAGS_ROCKET_SIGNAL_F
+	engine_flag wStatusFlags, STATUSFLAGS_ROCKET_SIGNAL_F            ;unused
 	engine_flag wStatusFlags, STATUSFLAGS_HALL_OF_FAME_F
-	engine_flag wStatusFlags, STATUSFLAGS_MAIN_MENU_MOBILE_CHOICES_F
+	engine_flag wStatusFlags, STATUSFLAGS_MAIN_MENU_MOBILE_CHOICES_F ;unused
 
 	engine_flag wStatusFlags2, STATUSFLAGS2_BUG_CONTEST_TIMER_F
 	engine_flag wStatusFlags2, STATUSFLAGS2_SAFARI_GAME_F
 	engine_flag wStatusFlags2, STATUSFLAGS2_ROCKETS_IN_RADIO_TOWER_F
-	engine_flag wStatusFlags2, STATUSFLAGS2_BIKE_SHOP_CALL_F
-	engine_flag wStatusFlags2, STATUSFLAGS2_ROCKETS_SILPH_CO_F ;STATUSFLAGS2_UNUSED_5_F
+	engine_flag wStatusFlags2, STATUSFLAGS2_BIKE_SHOP_CALL_F         ;unused
+	engine_flag wStatusFlags2, STATUSFLAGS2_ROCKETS_SILPH_CO_F
 	engine_flag wStatusFlags2, STATUSFLAGS2_REACHED_GOLDENROD_F
-	engine_flag wStatusFlags2, STATUSFLAGS2_ROCKETS_IN_MAHOGANY_F
+	engine_flag wStatusFlags2, STATUSFLAGS2_CAN_USE_WATERFALL_F
 
 	engine_flag wBikeFlags, BIKEFLAGS_STRENGTH_ACTIVE_F
 	engine_flag wBikeFlags, BIKEFLAGS_ALWAYS_ON_BIKE_F
@@ -115,7 +116,7 @@ EngineFlags:
 
 	engine_flag wLuckyNumberShowFlag, LUCKYNUMBERSHOW_GAME_OVER_F
 
-	engine_flag wStatusFlags2, STATUSFLAGS2_UNUSED_3_F
+;	engine_flag wStatusFlags2, STATUSFLAGS2_UNUSED_3_F
 
 	engine_flag wDailyFlags1, DAILYFLAGS1_KURT_MAKING_BALLS_F
 	engine_flag wDailyFlags1, DAILYFLAGS1_BUG_CONTEST_F
@@ -344,6 +345,6 @@ EngineFlags:
 	engine_flag wPlayerCaught2, 2 ; Celebi
 	engine_flag wPlayerCaught2, 3 ; Sudowoodo
 	engine_flag wPlayerCaught2, 4 ; Snorlax
-	engine_flag wPlayerCaught2, 5 ; Magikarp
+;	engine_flag wPlayerCaught2, 5 ; Magikarp
 
 	assert_table_length NUM_ENGINE_FLAGS
