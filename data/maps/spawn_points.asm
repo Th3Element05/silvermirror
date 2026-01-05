953003c50f41a9506ac1967cf679392ccd289a71
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-05 17:26:56 -0600

    moved new bark spawn point

--- data/maps/spawn_points.asm
+++ data/maps/spawn_points.asm
@@ -25,7 +25,7 @@ SpawnPoints:
 	spawn CINNABAR_ISLAND,            11, 14
 	spawn ROUTE_23,                    9,  6
 
-	spawn NEW_BARK_TOWN,              13,  6
+	spawn NEW_BARK_TOWN,               6,  5 ;13,  6
 	spawn CHERRYGROVE_CITY,           29,  4
 	spawn VIOLET_CITY,                31, 26
 	spawn ROUTE_32,                   11, 74
