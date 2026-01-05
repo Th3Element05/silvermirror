9b070f31b735119446106d215d0e9d300a3bd048
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-05 17:45:08 -0600

    comment out dayoftheweek book

--- maps/DayOfWeekSiblingsHouse.asm
+++ maps/DayOfWeekSiblingsHouse.asm
@@ -1,72 +1,72 @@
 	object_const_def
-	const DAYOFWEEKSIBLINGSHOUSE_POKEDEX
+;	const DAYOFWEEKSIBLINGSHOUSE_POKEDEX
 
 DayOfWeekSiblingsHouse_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
 
-DayOfWeekSiblingsHousePokedexScript:
-	opentext
-	writetext DayOfWeekSiblingsHousePokedexText1
-	yesorno
-	iffalse .End
-	writetext DayOfWeekSiblingsHousePokedexText2
-	yesorno
-	iffalse .End
-	writetext DayOfWeekSiblingsHousePokedexText3
-	waitbutton
-.End:
-	closetext
-	end
+;DayOfWeekSiblingsHousePokedexScript:
+;	opentext
+;	writetext DayOfWeekSiblingsHousePokedexText1
+;	yesorno
+;	iffalse .End
+;	writetext DayOfWeekSiblingsHousePokedexText2
+;	yesorno
+;	iffalse .End
+;	writetext DayOfWeekSiblingsHousePokedexText3
+;	waitbutton
+;.End:
+;	closetext
+;	end
 
-DayOfWeekSiblingsHousePokedexText1:
-	text "There's something"
-	line "written here."
+;DayOfWeekSiblingsHousePokedexText1:
+;	text "There's something"
+;	line "written here."
+;
+;	para "Read it?"
+;	done
 
-	para "Read it?"
-	done
+;DayOfWeekSiblingsHousePokedexText2:
+;	text "To my brothers and"
+;	line "sisters:"
+;
+;	para "We take pride in"
+;	line "serving #MON"
+;	cont "trainers."
+;
+;	para "When trainers talk"
+;	line "to you, give them"
+;	cont "something useful."
+;
+;	para "Love,"
+;	line "MONICA"
+;
+;	para "Keep reading?"
+;	done
 
-DayOfWeekSiblingsHousePokedexText2:
-	text "To my brothers and"
-	line "sisters:"
-
-	para "We take pride in"
-	line "serving #MON"
-	cont "trainers."
-
-	para "When trainers talk"
-	line "to you, give them"
-	cont "something useful."
-
-	para "Love,"
-	line "MONICA"
-
-	para "Keep reading?"
-	done
-
-DayOfWeekSiblingsHousePokedexText3:
-	text "Monday, MONICA"
-	line "ROUTE 40"
-
-	para "Tuesday, TUSCANY"
-	line "ROUTE 29"
-
-	para "Wednesday, WESLEY"
-	line "LAKE OF RAGE"
-
-	para "Thursday, ARTHUR"
-	line "ROUTE 36"
-
-	para "Friday, FRIEDA"
-	line "ROUTE 32"
-
-	para "Saturday, SANTOS"
-	line "BLACKTHORN CITY"
-
-	para "Sunday, SUNNY"
-	line "ROUTE 37"
-	done
+;DayOfWeekSiblingsHousePokedexText3:
+;	text "Monday, MONICA"    ;sharp beak
+;	line "ROUTE 40"
+;
+;	para "Tuesday, TUSCANY"  ;twisted spoon
+;	line "ROUTE 29"
+;
+;	para "Wednesday, WESLEY" ;black belt
+;	line "LAKE OF RAGE"
+;
+;	para "Thursday, ARTHUR"  ;hard stone
+;	line "ROUTE 36"
+;
+;	para "Friday, FRIEDA"    ;poison barb
+;	line "ROUTE 32"
+;
+;	para "Saturday, SANTOS"  ;spell tag
+;	line "BLACKTHORN CITY"
+;
+;	para "Sunday, SUNNY"     ;magnet
+;	line "ROUTE 37"
+;	done
 
 DayOfWeekSiblingsHouse_MapEvents:
 	db 0, 0 ; filler
@@ -80,4 +80,4 @@ DayOfWeekSiblingsHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, DayOfWeekSiblingsHousePokedexScript, -1
+;	object_event  3,  3, SPRITE_POKEDEX, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, DayOfWeekSiblingsHousePokedexScript, -1
