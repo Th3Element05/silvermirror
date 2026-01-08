e36b7c8e52a15d07cbc6decddd182fd80758d7bf
Sean Crawford <scrawford@sumnerone.com>
2026-01-08 08:18:08 -0600

    worked on mahogany mart script

--- maps/MahoganyMart1F.asm
+++ maps/MahoganyMart1F.asm
@@ -1,24 +1,18 @@
 	object_const_def
-;	const MAHOGANYMART1F_PHARMACIST
-;	const MAHOGANYMART1F_BLACK_BELT
-;	const MAHOGANYMART1F_LANCE
-;	const MAHOGANYMART1F_DRAGONITE
-;	const MAHOGANYMART1F_GRANNY
+	const MAHOGANYMART1F_GRANNY
 
 MahoganyMart1F_MapScripts:
 	def_scene_scripts
-;	scene_script MahoganyMart1FNoopScene,                SCENE_MAHOGANYMART1F_NOOP
-;	scene_script MahoganyMart1FLanceUncoversStairsScene, SCENE_MAHOGANYMART1F_LANCE_UNCOVERS_STAIRS
+	scene_script MahoganyMart1FNoopScene,  SCENE_MAHOGANYMART1F_NOOP
+	scene_script MahoganyMart1FNinjaScene, SCENE_MAHOGANYMART1F_NINJA
 
 	def_callbacks
 ;	callback MAPCALLBACK_TILES, MahoganyMart1FStaircaseCallback
+	callback MAPCALLBACK_OBJECTS, MahoganyMart1FCheckDayOfWeekCallback
 
-;MahoganyMart1FNoopScene:
-;	end
-
-;MahoganyMart1FLanceUncoversStairsScene:
-;	sdefer MahoganyMart1FLanceUncoversStaircaseScript
-;	end
+MahoganyMart1FNoopScene:
+MahoganyMart1FNinjaScene:
+	end
 
 ;MahoganyMart1FStaircaseCallback:
 ;	checkevent EVENT_UNCOVERED_STAIRCASE_IN_MAHOGANY_MART
@@ -29,194 +23,106 @@ MahoganyMart1F_MapScripts:
 ;	changeblock 6, 2, $28 ; stairs
 ;	endcallback
 
-;MahoganyMart1FPharmacistScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_DECIDED_TO_HELP_LANCE
-;	iftrue .LanceEntered
-;	pokemart MARTTYPE_STANDARD, MART_MAHOGANY_1
-;	closetext
-;	end
-;
-;.LanceEntered:
-;	writetext MahoganyMart1FPharmacistText_LanceEntered
-;	waitbutton
-;	closetext
-;	end
+MahoganyMart1FCheckDayOfWeekCallback:
+	readvar VAR_WEEKDAY
+	ifequal SATURDAY, .Weekend
+	ifequal SUNDAY, .Weekend
+;Weekday
+	appear MAHOGANYMART1F_GRANNY
+	endcallback
+.Weekend
+	disappear MAHOGANYMART1F_GRANNY
+	endcallback
 
-;MahoganyMart1FBlackBeltScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_DECIDED_TO_HELP_LANCE
-;	iftrue .LanceEntered
-;	writetext MahoganyMart1FBlackBeltText
-;	waitbutton
-;	closetext
-;	end
-;
-;.LanceEntered:
-;	writetext MahoganyMart1FBlackBeltText_LanceEntered
-;	waitbutton
-;	closetext
-;	end
+; scripts
+MahoganyMartMerchantScript:
+	faceplayer
+	opentext
+	readvar VAR_WEEKDAY
+	ifequal SUNDAY, .Closed
+	ifequal SATURDAY, .Closed
+	pokemart MARTTYPE_BITTER, MART_UNDERGROUND
+	closetext
+	end
 
-;MahoganyMart1FLanceUncoversStaircaseScript:
-;	pause 15
-;	opentext
-;	writetext MahoganyMart1FLanceDragoniteHyperBeamText
-;	pause 15
-;	closetext
-;	playsound SFX_TACKLE
-;	applymovement MAHOGANYMART1F_DRAGONITE, MahoganyMart1FDragoniteTackleMovement
-;	applymovement MAHOGANYMART1F_BLACK_BELT, MahoganyMart1FBlackBeltKnockedBackMovement
-;	pause 15
-;	disappear MAHOGANYMART1F_DRAGONITE
-;	pause 15
-;	applymovement MAHOGANYMART1F_LANCE, MahoganyMart1FLanceApproachPlayerMovement
-;	opentext
-;	writetext MahoganyMart1FLanceRadioText
-;	waitbutton
-;	closetext
-;	follow MAHOGANYMART1F_LANCE, PLAYER
-;	applymovement MAHOGANYMART1F_LANCE, MahoganyMart1FLanceApproachPharmacistMovement
-;	applymovement MAHOGANYMART1F_PHARMACIST, MahoganyMart1FPharmacistShovedAsideMovement
-;	applymovement MAHOGANYMART1F_LANCE, MahoganyMart1FLanceApproachHiddenStairsMovement
-;	stopfollow
-;	opentext
-;	writetext MahoganyMart1FLanceStairsText
-;	waitbutton
-;	showemote EMOTE_SHOCK, MAHOGANYMART1F_PHARMACIST, 10
-;	playsound SFX_FAINT
-;	changeblock 6, 2, $1e ; stairs
-;	reloadmappart
-;	closetext
-;	setevent EVENT_UNCOVERED_STAIRCASE_IN_MAHOGANY_MART
-;	turnobject MAHOGANYMART1F_LANCE, LEFT
-;	opentext
-;	writetext MahoganyMart1FLanceSplitUpText
-;	waitbutton
-;	closetext
-;	applymovement MAHOGANYMART1F_LANCE, MahoganyMart1FLanceGoDownStairsMovement
-;	playsound SFX_EXIT_BUILDING
-;	disappear MAHOGANYMART1F_LANCE
-;	setscene SCENE_MAHOGANYMART1F_NOOP
-;	waitsfx
+.Closed:
+	writetext MahoganyMartMerchantNotOpenText
+	waitbutton
+	closetext
+	end
+
+MahoganyMartMerchantNotOpenText:
+	text "Sorry, we're not"
+	line "open today."
+
+	para "I need to get to"
+	line "GOLDENROD."
+	done
+
+
+;hidden switch opens stairs
+;if granny is there, she kicks you out
+;this is the same granny from goldenrod underground
+;she sells the same herbs
+;she is not in the shop on the days/times she is in goldenrod underground
+;(saturday/sunday)
+;that is when you can reveal the stairs and enter the ninja hideout
+
+;ninja clan used to be at brass tower
+;have silver wing (and clear bell?)
+
+
+;MahoganyMart1FSecretStairsScript:
 ;	end
 
-;MahoganyMart1FGrannyScript:
-;	faceplayer
-;	opentext
-;	pokemart MARTTYPE_STANDARD, MART_MAHOGANY_2
-;	closetext
-;	end
+MahoganyMart1FSecretStairsConditional:
+	conditional_event EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8, .Proceed
+.Proceed
+	opentext
+	writetext MahoganyMart1FSomethingHereText
+	waitbutton
+	closetext
+	changeblock 6, 2, $28 ; stairs
+	reloadmappart
+	checkevent EVENT_MAHOGANY_MART_GRANNY
+	iffalse .YouSawNothing
 
-;MahoganyMart1FDragoniteTackleMovement:
-;	fix_facing
-;	big_step LEFT
-;	big_step RIGHT
-;	remove_fixed_facing
-;	step_end
+	;
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8
+	end
 
-;MahoganyMart1FBlackBeltKnockedBackMovement:
-;	fix_facing
-;	big_step LEFT
-;	remove_fixed_facing
-;	turn_head DOWN
-;	turn_head LEFT
-;	turn_head UP
-;	turn_head RIGHT
-;	step_end
+.YouSawNothing
+	showemote EMOTE_SHOCK, MAHOGANYMART1F_GRANNY, 15
+	end
 
-;MahoganyMart1FPharmacistShovedAsideMovement:
-;	fix_facing
-;	big_step LEFT
-;	remove_fixed_facing
-;	step_end
 
-;MahoganyMart1FLanceApproachPlayerMovement:
-;	slow_step LEFT
-;	turn_head DOWN
-;	step_end
 
-;MahoganyMart1FLanceApproachPharmacistMovement:
-;	slow_step RIGHT
-;	slow_step UP
-;	slow_step UP
-;	step_end
 
-;MahoganyMart1FLanceApproachHiddenStairsMovement:
-;	slow_step UP
-;	slow_step RIGHT
-;	slow_step RIGHT
-;	step_end
+MahoganyMart1FSomethingHereText:
+	text "There's something"
+	line "here…"
+	done
 
-;MahoganyMart1FLanceGoDownStairsMovement:
-;	slow_step RIGHT
-;	step_end
 
-;MahoganyMart1FRageCandyBarText: ; unreferenced
-;	text "Hello, kiddo!"
-;
-;	para "How would you like"
-;	line "some RAGECANDYBAR?"
-;
-;	para "It's the thing to"
-;	line "eat in MAHOGANY!"
-;	done
 
-;MahoganyMart1FPharmacistText_LanceEntered:
-;	text "Arrgh… You found"
-;	line "the secret stair-"
-;	cont "way…"
-;	done
 
-;MahoganyMart1FBlackBeltText:
-;	text "Heheh! The experi-"
-;	line "ment worked like a"
-;	cont "charm."
-;
-;	para "MAGIKARP are just"
-;	line "worthless, but"
-;
-;	para "GYARADOS are big"
-;	line "moneymakers."
-;	done
 
-;MahoganyMart1FBlackBeltText_LanceEntered:
-;	text "Urrgh…"
-;
-;	para "That guy's dragon"
-;	line "#MON are tough…"
-;	done
+MahoganyMart1FSecretStairsItemfinderSpoof:
+	hiddenitem ENERGYPOWDER, EVENT_TEMPORARY_UNTIL_MAP_RELOAD_8
 
-;MahoganyMart1FLanceDragoniteHyperBeamText:
-;	text "LANCE: DRAGONITE,"
-;	line "HYPER BEAM."
-;	done
+MahoganyMart1FMural:
+	jumptext MahoganyMart1FMuralText
+MahoganyMart1FMuralText:
+	text "It's a mural of"
+	line "ECRUTEAK CITY."
 
-;MahoganyMart1FLanceRadioText:
-;	text "What took you,"
-;	line "<PLAY_G>?"
-;
-;	para "Just as I thought,"
-;	line "that strange radio"
-;
-;	para "signal is coming"
-;	line "from here."
-;	done
+	para "It shows the city"
+	line "before BRASS TOWER"
+	cont "burned down."
 
-;MahoganyMart1FLanceStairsText:
-;	text "The stairs are"
-;	line "right here."
-;	done
-
-;MahoganyMart1FLanceSplitUpText:
-;	text "LANCE: <PLAY_G>, we"
-;	line "should split up to"
-;
-;	para "check this place."
-;	line "I'll go first."
-;	done
+	para "The mural looks"
+	line "very old."
+	done
 
 MahoganyMart1F_MapEvents:
 	db 0, 0 ; filler
@@ -229,10 +135,11 @@ MahoganyMart1F_MapEvents:
 	def_coord_events
 
 	def_bg_events
+;	bg_event  7,  3, BGEVENT_READ, MahoganyMart1FSecretStairsScript
+	bg_event  7,  3, BGEVENT_IFNOTSET, MahoganyMart1FSecretStairsConditional
+	bg_event  7,  3, BGEVENT_ITEM, MahoganyMart1FSecretStairsItemfinderSpoof
+	bg_event  2,  0, BGEVENT_READ, MahoganyMart1FMural
+	bg_event  3,  0, BGEVENT_READ, MahoganyMart1FMural
 
 	def_object_events
-;	object_event  4,  3, SPRITE_PHARMACIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyMart1FPharmacistScript, EVENT_TEAM_ROCKET_BASE_POPULATION
-;	object_event  1,  6, SPRITE_BLACK_BELT, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyMart1FBlackBeltScript, EVENT_TEAM_ROCKET_BASE_POPULATION
-;	object_event  4,  6, SPRITE_LANCE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MAHOGANY_MART_LANCE_AND_DRAGONITE
-;	object_event  3,  6, SPRITE_DRAGONITE_MOVE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_MAHOGANY_MART_LANCE_AND_DRAGONITE
-;	object_event  1,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyMart1FGrannyScript, EVENT_MAHOGANY_MART_OWNERS
+	object_event  1,  3, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyMartMerchantScript, EVENT_MAHOGANY_MART_GRANNY
