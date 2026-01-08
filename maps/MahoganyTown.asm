770652b21b733caa4b3c4f36bd01f8297020f370
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-08 08:17:35 -0600

    scripted mahogany town

--- maps/MahoganyTown.asm
+++ maps/MahoganyTown.asm
@@ -1,227 +1,63 @@
-;DEF MAHOGANYTOWN_RAGECANDYBAR_PRICE EQU 300
-;
 	object_const_def
-;	const MAHOGANYTOWN_POKEFAN_M
-;	const MAHOGANYTOWN_GRAMPS
-;	const MAHOGANYTOWN_FISHER
-;	const MAHOGANYTOWN_LASS
 
 MahoganyTown_MapScripts:
 	def_scene_scripts
-;	scene_script MahoganyTownNoop1Scene, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR
-;	scene_script MahoganyTownNoop2Scene, SCENE_MAHOGANYTOWN_NOOP
 
 	def_callbacks
 	callback MAPCALLBACK_NEWMAP, MahoganyTownFlypointCallback
-
-;MahoganyTownNoop1Scene:
-;	end
-;
-;MahoganyTownNoop2Scene:
-;	end
 
 MahoganyTownFlypointCallback:
 	setflag ENGINE_FLYPOINT_MAHOGANY
 	endcallback
 
-;MahoganyTownTryARageCandyBarScript:
-;	showemote EMOTE_SHOCK, MAHOGANYTOWN_POKEFAN_M, 15
-;	applymovement MAHOGANYTOWN_POKEFAN_M, MahoganyTownRageCandyBarMerchantBlocksYouMovement
-;	follow PLAYER, MAHOGANYTOWN_POKEFAN_M
-;	applymovement PLAYER, MahoganyTownPlayerStepLeftMovement
-;	stopfollow
-;	turnobject PLAYER, RIGHT
-;	scall RageCandyBarMerchantScript
-;	applymovement MAHOGANYTOWN_POKEFAN_M, MahoganyTownRageCandyBarMerchantReturnsMovement
-;	end
-;
-;MahoganyTownPokefanMScript:
-;	faceplayer
-;RageCandyBarMerchantScript:
-;	checkevent EVENT_CLEARED_ROCKET_HIDEOUT
-;	iftrue .ClearedRocketHideout
-;	scall .SellRageCandyBars
-;	end
-;
-;.ClearedRocketHideout:
-;	opentext
-;	writetext RageCandyBarMerchantSoldOutText
-;	waitbutton
-;	closetext
-;	end
-;
-;.SellRageCandyBars:
-;	opentext
-;	writetext RageCandyBarMerchantTryOneText
-;	special PlaceMoneyTopRight
-;	yesorno
-;	iffalse .Refused
-;	checkmoney YOUR_MONEY, MAHOGANYTOWN_RAGECANDYBAR_PRICE
-;	ifequal HAVE_LESS, .NotEnoughMoney
-;	giveitem RAGECANDYBAR
-;	iffalse .NoRoom
-;	waitsfx
-;	playsound SFX_TRANSACTION
-;	takemoney YOUR_MONEY, MAHOGANYTOWN_RAGECANDYBAR_PRICE
-;	special PlaceMoneyTopRight
-;	writetext RageCandyBarMerchantSavorItText
-;	waitbutton
-;	closetext
-;	end
-;
-;.NotEnoughMoney:
-;	writetext RageCandyBarMerchantNotEnoughMoneyText
-;	waitbutton
-;	closetext
-;	end
-;
-;.Refused:
-;	writetext RageCandyBarMerchantRefusedText
-;	waitbutton
-;	closetext
-;	end
-;
-;.NoRoom:
-;	writetext RageCandyBarMerchantNoRoomText
-;	waitbutton
-;	closetext
-;	end
-;
-;MahoganyTownGrampsScript:
-;	faceplayer
-;	opentext
-;	checkevent EVENT_CLEARED_ROCKET_HIDEOUT
-;	iftrue .ClearedRocketHideout
-;	writetext MahoganyTownGrampsText
-;	waitbutton
-;	closetext
-;	end
-;
-;.ClearedRocketHideout:
-;	writetext MahoganyTownGrampsText_ClearedRocketHideout
-;	waitbutton
-;	closetext
-;	end
-;
-;MahoganyTownFisherScript:
-;	jumptextfaceplayer MahoganyTownFisherText
-;
-;MahoganyTownLassScript:
-;	jumptextfaceplayer MahoganyTownLassText
+MahoganyTownGrampsScript:
+	jumptextfaceplayer MahoganyTownGrampsText
+MahoganyTownGrampsText:
+	text "A #MON with an"
+	line "unusual color was"
+	cont "spotted swimming"
+	cont "in LAKE OF RAGE."
+
+	para "All of the anglers"
+	line "in the area have"
+	cont "tried to hook it."
+
+	para "But none of them"
+	line "have had any luck."
+	done
+
+MahoganyTownFisherScript:
+	jumptextfaceplayer MahoganyTownFisherText
+MahoganyTownFisherText:
+	text "Since you came"
+	line "this far, take the"
+	cont "time to do some"
+	cont "sightseeing."
+
+	para "You should head"
+	line "north to check out"
+	cont "LAKE OF RAGE."
+	done
+
+MahoganyTownLassScript:
+	jumptextfaceplayer MahoganyTownLassText
+MahoganyTownLassText:
+	text "Visit my grandma's"
+	line "shop, she sells"
+	cont "stuff that nobody"
+	cont "else has!"
+
+	para "But she's only"
+	line "here on weekdays."
+
+	para "She runs a table"
+	line "in GOLDENROD's"
+	cont "UNDERGROUND on the"
+	cont "weekend."
+	done
 
 MahoganyTownSign:
 	jumptext MahoganyTownSignText
-
-MahoganyTownRagecandybarSign:
-	jumptext MahoganyTownRagecandybarSignText
-
-MahoganyGymSign:
-	jumptext MahoganyGymSignText
-
-MahoganyTownPokecenterSign:
-	jumpstd PokecenterSignScript
-
-;MahoganyTownCollideDownFaceLeftMovement: ; unreferenced
-;	step DOWN
-;	big_step UP
-;	turn_head DOWN
-;MahoganyTownPlayerStepLeftMovement:
-;	step LEFT
-;	step_end
-;
-;MahoganyTownRageCandyBarMerchantBlocksYouMovement:
-;	step RIGHT
-;	step DOWN
-;	turn_head LEFT
-;	step_end
-;
-;MahoganyTownRageCandyBarMerchantReturnsMovement:
-;	step UP
-;	turn_head DOWN
-;	step_end
-;
-;RageCandyBarMerchantTryOneText:
-;	text "Hiya, kid!"
-;
-;	para "I see you're new"
-;	line "in MAHOGANY TOWN."
-;
-;	para "Since you're new,"
-;	line "you should try a"
-;
-;	para "yummy RAGECANDY-"
-;	line "BAR!"
-;
-;	para "Right now, it can"
-;	line "be yours for just"
-;	cont "¥300! Want one?"
-;	done
-;
-;RageCandyBarMerchantSavorItText:
-;	text "Good! Savor it!"
-;	done
-;
-;RageCandyBarMerchantNotEnoughMoneyText:
-;	text "You don't have"
-;	line "enough money."
-;	done
-;
-;RageCandyBarMerchantRefusedText:
-;	text "Oh, fine then…"
-;	done
-;
-;RageCandyBarMerchantNoRoomText:
-;	text "You don't have"
-;	line "room for this."
-;	done
-;
-;RageCandyBarMerchantSoldOutText:
-;	text "RAGECANDYBAR's"
-;	line "sold out."
-;
-;	para "I'm packing up."
-;	line "Don't bother me,"
-;	cont "kiddo."
-;	done
-;
-;MahoganyTownGrampsText:
-;	text "Are you off to see"
-;	line "the GYARADOS ram-"
-;	cont "page at the LAKE?"
-;	done
-;
-;MahoganyTownGrampsText_ClearedRocketHideout:
-;	text "MAGIKARP have"
-;	line "returned to LAKE"
-;	cont "OF RAGE."
-;
-;	para "That should be"
-;	line "good news for the"
-;	cont "anglers there."
-;	done
-;
-;MahoganyTownFisherText:
-;	text "Since you came"
-;	line "this far, take the"
-;
-;	para "time to do some"
-;	line "sightseeing."
-;
-;	para "You should head"
-;	line "north and check"
-;
-;	para "out LAKE OF RAGE"
-;	line "right now."
-;	done
-;
-;MahoganyTownLassText:
-;	text "Visit Grandma's"
-;	line "shop. She sells"
-;
-;	para "stuff that nobody"
-;	line "else has."
-;	done
-
 MahoganyTownSignText:
 	text "MAHOGANY TOWN"
 
@@ -229,12 +65,8 @@ MahoganyTownSignText:
 	line "Home of the Ninja"
 	done
 
-MahoganyTownRagecandybarSignText:
-	text "While visiting"
-	line "MAHOGANY TOWN, try"
-	cont "a RAGECANDYBAR!"
-	done
-
+MahoganyGymSign:
+	jumptext MahoganyGymSignText
 MahoganyGymSignText:
 	text "MAHOGANY TOWN"
 	line "#MON GYM"
@@ -243,6 +75,9 @@ MahoganyGymSignText:
 	para "The Teacher of"
 	line "Winter's Harshness"
 	done
+
+MahoganyTownPokecenterSign:
+	jumpstd PokecenterSignScript
 
 MahoganyTown_MapEvents:
 	db 0, 0 ; filler
@@ -255,17 +90,13 @@ MahoganyTown_MapEvents:
 	warp_event  9,  1, ROUTE_43_MAHOGANY_GATE, 3
 
 	def_coord_events
-;	coord_event 19,  8, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
-;	coord_event 19,  9, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
 
 	def_bg_events
-	bg_event  1,  5, BGEVENT_READ, MahoganyTownSign
-	bg_event  9,  7, BGEVENT_READ, MahoganyTownRagecandybarSign
+	bg_event  9,  7, BGEVENT_READ, MahoganyTownSign
 	bg_event  7, 13, BGEVENT_READ, MahoganyGymSign
 	bg_event 16, 13, BGEVENT_READ, MahoganyTownPokecenterSign
 
 	def_object_events
-;	object_event 19,  8, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyTownPokefanMScript, EVENT_MAHOGANY_TOWN_POKEFAN_M_BLOCKS_EAST
-;	object_event  6,  9, SPRITE_GRAMPS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyTownGrampsScript, -1
-;	object_event  6, 14, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, MahoganyTownFisherScript, EVENT_MAHOGANY_TOWN_POKEFAN_M_BLOCKS_GYM
-;	object_event 12,  8, SPRITE_LASS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyTownLassScript, EVENT_MAHOGANY_MART_OWNERS
+	object_event 11, 15, SPRITE_GRAMPS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyTownGrampsScript, -1
+	object_event  5,  8, SPRITE_FISHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, MahoganyTownFisherScript, -1
+	object_event 12,  8, SPRITE_LASS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MahoganyTownLassScript, -1
