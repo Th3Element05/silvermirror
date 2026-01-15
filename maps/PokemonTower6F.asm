e6b26764f4df556fd57d3d1507e7c2d811c24693
Sean Crawford <scrawford@sumnerone.com>
2026-01-14 23:54:35 -0600

    tweaked ghost text for spookiness

--- maps/PokemonTower6F.asm
+++ maps/PokemonTower6F.asm
@@ -52,13 +52,13 @@ PokemonTower6FGhostScript:
 	end
 
 PokemonTower6FBegoneText:
-	text "Be gone…"
-	line "Intruders…"
+	text " Be gone…"
+	line "   Intruders…"
 	done
 
 PokemonTower6FGetOutText:
-	text "Get out!"
-	line "Get out!"
+	text " Get out!"
+	line "   Get out!"
 	done
 
 PokemonTower6FSilphScopeText:
