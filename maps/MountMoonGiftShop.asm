6c650be4a898b63cb71f1eb64a624230bab16892
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-20 12:21:28 -0600

    script mt moon gift shop

--- maps/MountMoonGiftShop.asm
+++ maps/MountMoonGiftShop.asm
@@ -1,8 +1,4 @@
 	object_const_def
-	const MOUNTMOONGIFTSHOP_GRAMPS1 ; morning only
-	const MOUNTMOONGIFTSHOP_GRAMPS2 ; day only
-	const MOUNTMOONGIFTSHOP_LASS1 ; morning only
-	const MOUNTMOONGIFTSHOP_LASS2 ; day only
 
 MountMoonGiftShop_MapScripts:
 	def_scene_scripts
@@ -18,26 +14,53 @@ MountMoonGiftShopClerkScript:
 
 MountMoonGiftShopLassScript:
 	jumptextfaceplayer MountMoonGiftShopLassText
-
 MountMoonGiftShopLassText:
 	text "When the sun goes"
 	line "down, CLEFAIRY"
 	cont "come out to play."
 	done
 
+MountMoonGiftShopPhoto:
+	jumptext MountMoonGiftShopPhotoText
+MountMoonGiftShopPhotoText:
+	text "It's a photo of"
+	line "some pink things"
+	cont "outside."
+
+	para "It's titled:"
+	line "CLEFAIRY sighting!"
+
+	para "The photo is too"
+	line "blurry to make out"
+	cont "what they are."
+	done
+
+;MountMoonGiftShopNothingHere:
+;	jumptext MountMoonGiftShopNothingHereText
+;MountMoonGiftShopNothingHere:
+;	text "There's nothing"
+;	line "here."
+;	done
+
 MountMoonGiftShop_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  4,  7, MOUNT_MOON_SQUARE, 3
-	warp_event  5,  7, MOUNT_MOON_SQUARE, 3
+	warp_event  4,  7, MOUNT_MOON_SQUARE, 2
+	warp_event  5,  7, MOUNT_MOON_SQUARE, 2
 
 	def_coord_events
 
 	def_bg_events
+	bg_event  2,  0, BGEVENT_READ, MountMoonGiftShopPhoto
+	bg_event  3,  0, BGEVENT_READ, MountMoonGiftShopPhoto
+;	bg_event  7,  3, BGEVENT_READ, MountMoonGiftShopNothingHere
 
 	def_object_events
-	object_event  4,  3, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, MORN, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopClerkScript, -1
-	object_event  1,  2, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, DAY, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopClerkScript, -1
+	object_event  6,  2, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, MORN, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopClerkScript, -1
+	object_event  1,  2, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, DAY | EVE, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopClerkScript, -1
 	object_event  1,  6, SPRITE_LASS, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, MORN, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopLassScript, -1
-	object_event  5,  4, SPRITE_LASS, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, DAY, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopLassScript, -1
+	object_event  5,  3, SPRITE_LASS, SPRITEMOVEDATA_STANDING_LEFT, 0, 1, -1, DAY, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopLassScript, -1
+	object_event  6,  6, SPRITE_LASS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 1, -1, EVE, 0, OBJECTTYPE_SCRIPT, 0, MountMoonGiftShopLassScript, -1
+
+; MORN | DAY | EVE | NITE
