22a4ce0ef7ab9936c608397511e4f3bb0e6af2eb
Sean Crawford <scrawford@sumnerone.com>
2026-01-20 12:24:06 -0600

    script entrance script

--- maps/EcruteakTinTowerEntrance.asm
+++ maps/EcruteakTinTowerEntrance.asm
@@ -4,128 +4,216 @@
 
 EcruteakTinTowerEntrance_MapScripts:
 	def_scene_scripts
-	scene_script EcruteakTinTowerEntranceNoop1Scene, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS
-	scene_script EcruteakTinTowerEntranceNoop2Scene, SCENE_ECRUTEAKTINTOWERENTRANCE_NOOP
 
 	def_callbacks
+	callback MAPCALLBACK_OBJECTS, TinTowerEntranceBlockingSagesCallback
 
-EcruteakTinTowerEntranceNoop1Scene:
-EcruteakTinTowerEntranceNoop2Scene:
-	end
+TinTowerEntranceBlockingSagesCallback:
+	checkevent EVENT_TIN_TOWER_ENTRANCE_ACCESS_GRANTED
+	iftrue .NotBlocked
+	moveobject ECRUTEAKTINTOWERENTRANCE_SAGE_LEFT, 4, 6
+	moveobject ECRUTEAKTINTOWERENTRANCE_SAGE_RIGHT, 5, 6
+.NotBlocked
+	endcallback
 
-EcruteakTinTowerEntranceLeftSageScript:
-	turnobject ECRUTEAKTINTOWERENTRANCE_SAGE_LEFT, RIGHT
-	showemote EMOTE_SHOCK, ECRUTEAKTINTOWERENTRANCE_SAGE_LEFT, 15
-	turnobject PLAYER, LEFT
-	sjump EcruteakTinTowerEntranceBadgeCheck
+; scripts
+TinTowerEntranceSageScript:
+	checkevent EVENT_KOJI_ALLOWS_YOU_PASSAGE_TO_TIN_TOWER
+	iftrue .AccessGranted
+	checkevent EVENT_WISE_TRIO_EXPLAINED_CLEAR_BELL
+	iftrue .WhatSoothesTheSoul
+	checkevent EVENT_TIN_TOWER_ENTRANCE_ACCESS_GRANTED
+	iftrue .AccessGranted
+	checkevent EVENT_ROUTE_30_SAW_HOOH
+	iftrue .HoohReturned
+	jumptextfaceplayer TinTowerEntranceAccessDeniedText
 
-EcruteakTinTowerEntranceRightSageScript:
-	turnobject ECRUTEAKTINTOWERENTRANCE_SAGE_RIGHT, LEFT
-	showemote EMOTE_SHOCK, ECRUTEAKTINTOWERENTRANCE_SAGE_RIGHT, 15
-	turnobject PLAYER, RIGHT
-;	sjump EcruteakTinTowerEntranceBadgeCheck
-	; fallthrough
-EcruteakTinTowerEntranceBadgeCheck:
-	checkscene SCENE_ECRUTEAKTINTOWERENTRANCE_NOOP
-	iftrue .AllowedThrough
-	checkflag ENGINE_FOGBADGE
-	iftrue .GotFogBadge
-	writetext EcruteakTinTowerEntranceSageText
+.AccessGranted
+	jumptext TinTowerEntrancePleaseDoGoInText
+
+.WhatSoothesTheSoul
+	jumptext TinTowerEntranceSoothTheSoulText
+
+.HoohReturned
+	faceplayer
+	opentext
+	writetext TinTowerEntranceNoRainbowWingText
+	checkitem RAINBOW_WING
+	iftrue .HaveRainbowWing
 	waitbutton
 	closetext
-	applymovement PLAYER, PlayerTurnedAwayMovement
 	end
 
-.GotFogBadge
-	setscene SCENE_ECRUTEAKTINTOWERENTRANCE_NOOP
-	jumptextfaceplayer EcruteakTinTowerEntranceSageText_GotFogBadge
-;	waitbutton
-;	closetext
-;	end
+.HaveRainbowWing
+	promptbutton
+	writetext TinTowerEntranceHaveRainbowWingText
+	waitbutton
+	closetext
+	applymovement ECRUTEAKTINTOWERENTRANCE_SAGE_LEFT, TinTowerEntranceSageLeftMovement
+	applymovement ECRUTEAKTINTOWERENTRANCE_SAGE_RIGHT, TinTowerEntranceSageRightMovement
+	setevent EVENT_TIN_TOWER_ENTRANCE_ACCESS_GRANTED
+	end
 
-.AllowedThrough
-	jumptextfaceplayer EcruteakTinTowerEntranceSageText_PleaseDoGoOn
-;	waitbutton
-;	closetext
-;	end
+TinTowerEntranceAccessDeniedText:
+	text "I beg your pardon,"
+	line "but I must ask you"
+	cont "to leave."
 
-EcruteakTinTowerEntranceSageText:
-	text "TIN TOWER is off"
-	line "limits to anyone"
-	cont "without ECRUTEAK"
-	cont "GYM's BADGE."
+	para "Our wisest sages"
+	line "have seen signs"
+	cont "that a momentous"
+	roll "event will soon"
+	cont "occur."
 
-	para "Sorry, but you'll"
-	line "have to leave."
+	para "They forbid anyone"
+	line "from entering TIN"
+	cont "TOWER right now."
 	done
 
-EcruteakTinTowerEntranceSageText_GotFogBadge:
-	text "TIN TOWER is off"
-	line "limits to anyone"
-	cont "without ECRUTEAK"
-	cont "GYM's BADGE."
+TinTowerEntranceNoRainbowWingText:
+	text "A momentous event"
+	line "has occurred!"
 
-	para "Ah!"
+	para "The rainbow-winged"
+	line "#MON returned"
+	cont "to TIN TOWER!"
 
-	para "ECRUTEAK's GYM"
-	line "BADGE! Please, go"
-	cont "right through."
+	para "Our wisest sages"
+	line "forbid anyone from"
+	cont "approaching TIN"
+	roll "TOWER right now."
 	done
 
-EcruteakTinTowerEntranceSageText_PleaseDoGoOn:
-	text "Please, do go on."
+TinTowerEntranceHaveRainbowWingText:
+	text "Wait…"
+	line "What is that?"
+
+	para "<……> <……>"
+
+	para "That RAINBOW WING!"
+
+	para "The WISE TRIO will"
+	line "want to see this."
+
+	para "Please, go speak"
+	line "with them right"
+	cont "away!"
 	done
 
-PlayerTurnedAwayMovement:
-	step DOWN
+TinTowerEntrancePleaseDoGoInText:
+	text "Please, do go in."
+	done
+
+TinTowerEntranceSoothTheSoulText:
+	text "…What soothes the"
+	line "soul…"
+
+	para "The WISE TRIO say"
+	line "things that are so"
+	cont "very difficult to"
+	roll "understand…"
+	done
+
+TinTowerEntranceSageLeftMovement:
+	step UP
+	step LEFT
+	turn_head RIGHT
 	step_end
 
-EcruteakTinTowerEntranceWanderingSageScript:
-;	faceplayer
-;	opentext
-	checkevent EVENT_GOT_CLEAR_BELL ;(radio tower) got rainbow_wing instead?
-	iftrue .GotClearBell
-	jumptextfaceplayer EcruteakTinTowerEntranceWanderingSageText
-;	waitbutton
-;	closetext
-;	end
+TinTowerEntranceSageRightMovement:
+	step UP
+	step RIGHT
+	turn_head LEFT
+	step_end
 
-.GotClearBell:
-	jumptextfaceplayer EcruteakTinTowerEntranceWanderingSageText_GotClearBell
-;	waitbutton
-;	closetext
-;	end
 
-EcruteakTinTowerEntranceWanderingSageText:
-	text "The TIN TOWER"
-	line "ahead is a nine-"
-	cont "tier tower of"
-	cont "divine beauty."
+; npc
+TinTowerEntranceWanderingSageScript:
+	jumptextfaceplayer TinTowerEntranceWanderingSageText
 
-	para "It soothes the"
-	line "soul of all who"
-	cont "see it."
+TinTowerEntranceWanderingSageText:
+	text "In the past, there"
+	line "were two nine-tier"
+	cont "towers here."
+
+	para "The BRASS TOWER,"
+	line "which was said to"
+	cont "awaken #MON,"
+	roll "and the TIN TOWER,"
+	cont "where #MON were"
+	roll "said to rest."
+
+	para "The view from the"
+	line "tops of the TOWERs"
+	cont "must have been"
+	roll "magnificent."
+
+	para "However…"
+
+	para "About 150 years"
+	line "ago, a lightning"
+	cont "bolt struck the"
+	roll "BRASS TOWER."
+
+	para "It was engulfed in"
+	line "flames that raged"
+	cont "for days, and"
+	roll "three #MON died"
+	cont "in the blaze."
+
+	para "A sudden downpour"
+	line "finally put out"
+	cont "the flames."
+
+	para "And that is how"
+	line "the BURNED TOWER"
+	cont "came to be."
 	done
 
-EcruteakTinTowerEntranceWanderingSageText_GotClearBell:
-	text "The TIN TOWER"
-	line "shook! A #MON"
+TinTowerEntranceGrampsScript:
+	checkevent EVENT_ROUTE_30_SAW_HOOH
+	iftrue .HoohReturned
+	jumptextfaceplayer TinTowerEntranceGrampsTowersText
 
-	para "must have returned"
-	line "to the top!"
-	done
+.HoohReturned:
+	checkevent EVENT_FOUGHT_HO_OH
+	iftrue .Beasts
+	jumptextfaceplayer TinTowerEntranceGrampsHoohReturnedText
 
-EcruteakTinTowerEntranceGrampsScript:
-	jumptextfaceplayer EcruteakTinTowerEntranceGrampsText
-EcruteakTinTowerEntranceGrampsText:
+.Beasts
+	jumptextfaceplayer TinTowerEntranceGrampsBeastsText
+
+TinTowerEntranceGrampsTowersText:
 	text "Two towers…"
 	line "Two #MON…"
 
 	para "But when one"
 	line "burned down, both"
 	cont "#MON flew away,"
-	cont "never to return."
+	roll "never to return."
 	done
+
+TinTowerEntranceGrampsHoohReturnedText:
+	text "The TIN TOWER"
+	line "shook! A #MON"
+	cont "must have returned"
+	roll "to the top!"
+	done
+
+TinTowerEntranceGrampsBeastsText:
+	text "The BURNED TOWER…"
+
+	para "They say the three"
+	line "#MON that died"
+	cont "in that fire would"
+	roll "be resurrected"
+	cont "someday…"
+
+	para "Legends name them"
+	line "RAIKOU, ENTEI,"
+	cont "and SUICUNE."
+	done
+
 
 EcruteakTinTowerEntrance_MapEvents:
 	db 0, 0 ; filler
@@ -138,13 +226,18 @@ EcruteakTinTowerEntrance_MapEvents:
 	warp_event 17,  3, WISE_TRIOS_ROOM, 3
 
 	def_coord_events
-	coord_event  4,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceLeftSageScript
-	coord_event  5,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_SAGE_BLOCKS, EcruteakTinTowerEntranceRightSageScript
+;	coord_event  4,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_BADGE_CHECK, TinTowerEntranceLeftBadgeCheckScript
+;	coord_event  5,  7, SCENE_ECRUTEAKTINTOWERENTRANCE_BADGE_CHECK, TinTowerEntranceRightBadgeCheckScript
 
 	def_bg_events
 
 	def_object_events
-	object_event  3,  7, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceBadgeCheck, -1
-	object_event  6,  7, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceBadgeCheck, -1
-	object_event  6, 10, SPRITE_SAGE, SPRITEMOVEDATA_WALK_UP_DOWN, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceWanderingSageScript, -1
-	object_event  3, 11, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, EcruteakTinTowerEntranceGrampsScript, -1
+	object_event  3,  5, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, TinTowerEntranceSageScript, -1
+	object_event  6,  5, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, TinTowerEntranceSageScript, -1
+	object_event  6,  9, SPRITE_SAGE, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, TinTowerEntranceWanderingSageScript, -1
+	object_event  3, 11, SPRITE_GRAMPS, SPRITEMOVEDATA_WANDER, 1, 1, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, TinTowerEntranceGrampsScript, -1
+
+;off limits until you get fogbadge
+;after seeing ho-oh, explain clear bell in mahogany
+;that's it, that's this map
+;EVENT_ROUTE_30_SAW_HOOH
