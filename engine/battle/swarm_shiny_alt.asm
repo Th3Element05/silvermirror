f4d812d09f47fd195800ac54a3f38e576ca82f6e
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-31 21:57:54 -0600

    swarm species shiny checks

--- engine/battle/swarm_shiny_alt.asm
+++ engine/battle/swarm_shiny_alt.asm
@@ -4,65 +4,108 @@ GenerateAltSwarmShiny:
 	ld a, [wSwarmMapNumber]
 	ld c, a
 	call GetWorldMapLocation
+; kanto
+	cp LANDMARK_ROUTE_1
+	jp z, .sentret
 	cp LANDMARK_ROUTE_3
-	jp z, .jigglypuff
+	jp z, .mankey
+	cp LANDMARK_ROUTE_4
+	jp z, .sandshrew
 	cp LANDMARK_ROUTE_5
 	jp z, .snubbull
-	cp LANDMARK_ROUTE_6
-	jp z, .abra
 	cp LANDMARK_ROUTE_7
 	jp z, .murkrow
 	cp LANDMARK_ROUTE_8
 	jp z, .houndour
 	cp LANDMARK_ROUTE_10
-	jp z, .mareep
+	jp z, .magnemite
 	cp LANDMARK_ROUTE_11
-	jp z, .hoppip
+	jp z, .ekans
+	cp LANDMARK_ROUTE_12
+	jp z, .qwilfish
 	cp LANDMARK_ROUTE_13
-	jp z, .farfetch_d
+	jp z, .hoothoot
 	cp LANDMARK_ROUTE_15
 	jp z, .venonat
-	cp LANDMARK_ROUTE_17
+	cp LANDMARK_ROUTE_16
 	jp z, .natu
-	cp LANDMARK_ROUTE_19
-	jp z, .staryu
+	cp LANDMARK_ROUTE_17
+	jp z, .slugma
 	cp LANDMARK_ROUTE_21
-	jp z, .marill
-	cp LANDMARK_ROUTE_24
-	jp z, .oddish
-	cp LANDMARK_ROUTE_25
-	jp z, .bellsprout
+	jp z, .tangela
+	cp LANDMARK_ROUTE_22
+	jp z, .nidoran_f
+	cp LANDMARK_ROUTE_26
+	jp z, .poliwag
 	cp LANDMARK_VIRIDIAN_FOREST
-	jp z, .pikachu
+	jp z, .caterpie
 	cp LANDMARK_MT_MOON
-	jp z, .clefairy
-	cp LANDMARK_VERMILION_CITY
-	jp z, .shellder
+	jp z, .zubat
 	cp LANDMARK_ROCK_TUNNEL
-	jp z, .phanpy
+	jp z, .cubone
 	cp LANDMARK_SEAFOAM_ISLANDS
-	jp z, .jynx
-	cp LANDMARK_POWER_PLANT
-	jp z, .electabuzz
+	jp z, .krabby
 	cp LANDMARK_POKEMON_MANSION
 	jp z, .magmar
 	cp LANDMARK_VICTORY_ROAD
-	jp z, .ditto
-	cp LANDMARK_ROUTE_30
-	jp z, .ditto
+	jp z, .larvitar
+	cp LANDMARK_BEHIND_BILLS_HOUSE
+	jp z, .eevee
+; johto
+	cp LANDMARK_ROUTE_29
+	jp z, .hoppip
+	cp LANDMARK_ROUTE_31
+	jp z, .bellsprout
+	cp LANDMARK_ROUTE_32
+	jp z, .mareep
+	cp LANDMARK_ROUTE_33
+	jp z, .aipom
+	cp LANDMARK_ROUTE_38
+	jp z, .miltank
+	cp LANDMARK_ROUTE_39
+	jp z, .meowth
+	cp LANDMARK_ROUTE_43
+	jp z, .girafarig
+	cp LANDMARK_ROUTE_44
+	jp z, .lickitung
+	cp LANDMARK_ROUTE_45
+	jp z, .gligar
+	cp LANDMARK_ROUTE_46
+	jp z, .phanpy
+	cp LANDMARK_DARK_CAVE
+	jp z, .teddiursa
+	cp LANDMARK_RUINS_OF_ALPH
+	jp z, .smeargle
+	cp LANDMARK_SLOWPOKE_WELL
+	jp z, .slowpoke
+	cp LANDMARK_ILEX_FOREST
+	jp z, .paras
+	cp LANDMARK_NATIONAL_PARK
+	jp z, .spinarak
+	cp LANDMARK_BURNED_TOWER
+	jp z, .koffing
+	cp LANDMARK_OLIVINE_CITY
+	jp z, .corsola
+	cp LANDMARK_ICE_PATH
+	jp z, .swinub
 	jp .skipshine
 
-.jigglypuff
+; kanto
+.sentret
 	ld a, [wCurPartySpecies]
-	cp JIGGLYPUFF
+	cp SENTRET
+	jp .next
+.mankey
+	ld a, [wCurPartySpecies]
+	cp MANKEY
+	jp .next
+.sandshrew
+	ld a, [wCurPartySpecies]
+	cp SANDSHREW
 	jp .next
 .snubbull
 	ld a, [wCurPartySpecies]
 	cp SNUBBULL
-	jp .next
-.abra
-	ld a, [wCurPartySpecies]
-	cp ABRA
 	jp .next
 .murkrow
 	ld a, [wCurPartySpecies]
@@ -72,17 +115,21 @@ GenerateAltSwarmShiny:
 	ld a, [wCurPartySpecies]
 	cp HOUNDOUR
 	jp .next
-.mareep
+.magnemite
 	ld a, [wCurPartySpecies]
-	cp MAREEP
+	cp MAGNEMITE
 	jp .next
-.hoppip
+.ekans
 	ld a, [wCurPartySpecies]
-	cp HOPPIP
+	cp EKANS
 	jp .next
-.farfetch_d
+.qwilfish
 	ld a, [wCurPartySpecies]
-	cp FARFETCH_D
+	cp QWILFISH
+	jp .next
+.hoothoot
+	ld a, [wCurPartySpecies]
+	cp HOOTHOOT
 	jp .next
 .venonat
 	ld a, [wCurPartySpecies]
@@ -92,45 +139,37 @@ GenerateAltSwarmShiny:
 	ld a, [wCurPartySpecies]
 	cp NATU
 	jp .next
-.staryu
+.slugma
 	ld a, [wCurPartySpecies]
-	cp STARYU
+	cp SLUGMA
 	jp .next
-.marill
+.tangela
 	ld a, [wCurPartySpecies]
-	cp MARILL
+	cp TANGELA
 	jp .next
-.bellsprout
+.nidoran_f
 	ld a, [wCurPartySpecies]
-	cp BELLSPROUT
+	cp NIDORAN_F
 	jp .next
-.oddish
+.poliwag
 	ld a, [wCurPartySpecies]
-	cp ODDISH
+	cp POLIWAG
 	jp .next
-.pikachu
+.caterpie
 	ld a, [wCurPartySpecies]
-	cp PIKACHU
+	cp CATERPIE
 	jp .next
-.clefairy
+.zubat
 	ld a, [wCurPartySpecies]
-	cp CLEFAIRY
+	cp ZUBAT
 	jp .next
-.shellder
+.cubone
 	ld a, [wCurPartySpecies]
-	cp SHELLDER
+	cp CUBONE
 	jp .next
-.phanpy
+.krabby
 	ld a, [wCurPartySpecies]
-	cp PHANPY
-	jp .next
-.jynx
-	ld a, [wCurPartySpecies]
-	cp JYNX
-	jp .next
-.electabuzz
-	ld a, [wCurPartySpecies]
-	cp ELECTABUZZ
+	cp KRABBY
 	jp .next
 .magmar
 	ld a, [wCurPartySpecies]
@@ -140,10 +179,85 @@ GenerateAltSwarmShiny:
 	ld a, [wCurPartySpecies]
 	cp LARVITAR
 	jp .next
-.ditto
+.eevee
 	ld a, [wCurPartySpecies]
-	cp DITTO
+	cp EEVEE
 	jp .next
+
+; johto
+.hoppip
+	ld a, [wCurPartySpecies]
+	cp HOPPIP
+	jp .next
+.bellsprout
+	ld a, [wCurPartySpecies]
+	cp BELLSPROUT
+	jp .next
+.mareep
+	ld a, [wCurPartySpecies]
+	cp MAREEP
+	jp .next
+.aipom
+	ld a, [wCurPartySpecies]
+	cp AIPOM
+	jp .next
+.miltank
+	ld a, [wCurPartySpecies]
+	cp MILTANK
+	jp .next
+.meowth
+	ld a, [wCurPartySpecies]
+	cp MEOWTH
+	jp .next
+.girafarig
+	ld a, [wCurPartySpecies]
+	cp GIRAFARIG
+	jp .next
+.lickitung
+	ld a, [wCurPartySpecies]
+	cp LICKITUNG
+	jp .next
+.gligar
+	ld a, [wCurPartySpecies]
+	cp GLIGAR
+	jp .next
+.phanpy
+	ld a, [wCurPartySpecies]
+	cp PHANPY
+	jp .next
+.teddiursa
+	ld a, [wCurPartySpecies]
+	cp TEDDIURSA
+	jp .next
+.smeargle
+	ld a, [wCurPartySpecies]
+	cp SMEARGLE
+	jp .next
+.slowpoke
+	ld a, [wCurPartySpecies]
+	cp SLOWPOKE
+	jp .next
+.paras
+	ld a, [wCurPartySpecies]
+	cp PARAS
+	jp .next
+.spinarak
+	ld a, [wCurPartySpecies]
+	cp SPINARAK
+	jp .next
+.koffing
+	ld a, [wCurPartySpecies]
+	cp KOFFING
+	jp .next
+.corsola
+	ld a, [wCurPartySpecies]
+	cp CORSOLA
+	jp .next
+.swinub
+	ld a, [wCurPartySpecies]
+	cp SWINUB
+	jp .next
+
 .next
 	jr nz, .skipshine
 	;fallthrough
