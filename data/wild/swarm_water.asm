be8460852e9debd21e8d5216b1d55d1a6081781d
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-01-31 21:59:10 -0600

    added entries for johto swarm locations (still need to adjust encounter tables)

--- data/wild/swarm_water.asm
+++ data/wild/swarm_water.asm
@@ -2,63 +2,94 @@
 
 SwarmWaterWildMons:
 
-	; STARYU swarm
-	map_id ROUTE_19
+;	; STARYU swarm
+;	map_id ROUTE_19
+;	db 6 percent ; encounter rate
+;	db 16, STARYU     ; 50
+;	db 15, TENTACOOL  ; 30
+;	db 16, HORSEA     ; 10
+;	db 16, HORSEA     ; 10
+
+;	; HORSEA swarm
+;	map_id ROUTE_20
+;	db 6 percent ; encounter rate
+;	db 16, HORSEA     ; 50
+;	db 15, TENTACOOL  ; 30
+;	db 15, TENTACOOL  ; 10
+;	db 16, STARYU     ; 10
+
+;	; SHELLDER swarm
+;	map_id VERMILION_CITY
+;	db 6 percent ; encounter rate
+;	db 10, SHELLDER   ; 50
+;	db 10, TENTACOOL  ; 30
+;	db 10, HORSEA     ; 10
+;	db 10, STARYU     ; 10
+
+; JOHTO swarms
+
+	; QWILFISH
+	def_water_wildmons ROUTE_32
 	db 6 percent ; encounter rate
-	db 16, STARYU     ; 50
-	db 15, TENTACOOL  ; 30
-	db 16, HORSEA     ; 10
-	db 16, HORSEA     ; 10
+	db 15, TENTACOOL
+	db 15, WOOPER
+	db 20, QUAGSIRE
+	db 25, TENTACRUEL
 
-	; HORSEA swarm
-	map_id ROUTE_20
+	; MANTINE
+	def_water_wildmons ROUTE_41
 	db 6 percent ; encounter rate
-	db 16, HORSEA     ; 50
-	db 15, TENTACOOL  ; 30
-	db 15, TENTACOOL  ; 10
-	db 16, STARYU     ; 10
+	db 15, TENTACOOL
+	db 16, HORSEA
+	db 25, TENTACRUEL
+	db 16, MANTINE
 
-	; SHELLDER swarm
-	map_id VERMILION_CITY
+	; MARILL
+	def_water_wildmons ROUTE_42
+	db 4 percent ; encounter rate
+	db 15, GOLDEEN
+	db 10, MAGIKARP
+	db 10, GOLDEEN
+	db 20, SEAKING
+
+	; REMORAID
+	def_water_wildmons ROUTE_44
+	db 2 percent ; encounter rate
+	db 15, POLIWAG
+	db 10, MAGIKARP
+	db 10, POLIWAG
+	db 20, POLIWHIRL
+
+	; WOOPER
+	def_water_wildmons UNION_CAVE_B2F
+	db 4 percent ; encounter rate
+	db 15, TENTACOOL
+	db 15, WOOPER
+	db 25, TENTACRUEL
+	db 20, QUAGSIRE
+
+	; CHINCHOU
+	def_water_wildmons OLIVINE_CITY
 	db 6 percent ; encounter rate
-	db 10, SHELLDER   ; 50
-	db 10, TENTACOOL  ; 30
-	db 10, HORSEA     ; 10
-	db 10, STARYU     ; 10
+	db 15, TENTACOOL
+	db 16, HORSEA
+	db 10, TENTACOOL
+	db 25, TENTACRUEL
 
-;	; QWILFISH swarm
-;	map_id ROUTE_32
-;	db 6 percent ; encounter rate
-;	db 15, QWILFISH
-;	db 20, QWILFISH
-;	db 20, TENTACRUEL
+	; MAGIKARP
+	def_water_wildmons LAKE_OF_RAGE
+	db 6 percent ; encounter rate
+	db 10, MAGIKARP
+	db  5, MAGIKARP
+	db 15, MAGIKARP
+	db 20, GYARADOS
 
-;	; CHINCHOU swarm
-;	map_id OLIVINE_CITY
-;	db 6 percent ; encounter rate
-;	db 10, CHINCHOU
-;	db 15, CHINCHOU
-;	db 20, TENTACRUEL
-
-;	; REMORAID swarm
-;	map_id ROUTE_44
-;	db 6 percent ; encounter rate
-;	db 10, REMORAID
-;	db 15, REMORAID
-;	db 25, POLIWHIRL
-
-;	; MANTINE swarm
-;	map_id ROUTE_41
-;	db 6 percent ; encounter rate
-;	db 20, MANTINE
-;	db 20, MANTINE
-;	db 20, TENTACRUEL
-
-;	; DRATINI swarm
-;	map_id DRAGONS_DEN_B1F
-;	db 6 percent ; encounter rate
-;	db 15, DRATINI
-;	db 10, DRATINI
-;	db 15, MAGIKARP
+	; DRATINI
+	def_water_wildmons DRAGONS_DEN_B1F
+	db 4 percent ; encounter rate
+	db 10, MAGIKARP
+	db  5, MAGIKARP
+	db 15, MAGIKARP
+	db 10, DRATINI
 
 	db -1 ; end
