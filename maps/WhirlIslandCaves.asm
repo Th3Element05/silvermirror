2848d97b956b0c4eec6009bda573689d01edf3d9
Sean Crawford <scrawford@sumnerone.com>
2026-02-02 17:05:49 -0600

    fixed whirl islands warps

--- maps/WhirlIslandCaves.asm
+++ maps/WhirlIslandCaves.asm
@@ -29,24 +29,24 @@ WhirlIslandCaves_MapEvents:
 	def_warp_events
 ;NW
 	warp_event  5, 27, ROUTE_41, 1            ;1  ;NW,1
-	warp_event  5, 23, WHIRL_ISLAND_B1F, 1    ;2  ;NW,2
+	warp_event  5, 23, WHIRL_ISLAND_B1F_B2F, 1    ;2  ;NW,2
 	warp_event  3, 35, WHIRL_ISLAND_CAVES, 11 ;3  ;NW,3 to SW,4
 	warp_event  7, 35, WHIRL_ISLAND_CAVES, 16 ;4  ;NW,4 to Cave2
 ;NE
 	warp_event 25, 13, ROUTE_41, 2           ;5  ;NE,1
-	warp_event 39,  3, WHIRL_ISLAND_B1F, 2   ;6  ;NE,2
-	warp_event 35, 11, WHIRL_ISLAND_B1F, 3   ;7  ;NE,3
+	warp_event 39,  3, WHIRL_ISLAND_B1F_B2F, 2   ;6  ;NE,2
+	warp_event 35, 11, WHIRL_ISLAND_B1F_B2F, 3   ;7  ;NE,3
 ;SW
 	warp_event  5,  7, ROUTE_41, 3           ;8  ;SW,1
-	warp_event 17,  3, WHIRL_ISLAND_B1F, 5   ;9  ;SW,2
-	warp_event  3,  3, WHIRL_ISLAND_B1F, 4   ;10 ;SW,3
+	warp_event 17,  3, WHIRL_ISLAND_B1F_B2F, 5   ;9  ;SW,2
+	warp_event  3,  3, WHIRL_ISLAND_B1F_B2F, 4   ;10 ;SW,3
 	warp_event  3, 15, WHIRL_ISLAND_CAVES, 3 ;11 ;SW,4 to NW,3 (to Cave,2)
-	warp_event 17, 15, WHIRL_ISLAND_B2F, 4   ;12 ;SW,5
+	warp_event 17, 15, WHIRL_ISLAND_B1F_B2F, 13   ;12 ;SW,5
 ;SE
 	warp_event 35, 35, ROUTE_41, 4           ;13 ;SE,1
-	warp_event 35, 25, WHIRL_ISLAND_B1F, 6   ;14 ;SE,2
+	warp_event 35, 25, WHIRL_ISLAND_B1F_B2F, 6   ;14 ;SE,2
 ;Cave
-	warp_event 19, 27, WHIRL_ISLAND_B1F, 9   ;15 ;Cave,1
+	warp_event 19, 27, WHIRL_ISLAND_B1F_B2F, 9   ;15 ;Cave,1
 	warp_event 15, 35, WHIRL_ISLAND_CAVES, 4 ;16 ;Cave,2 to NW,4
 
 	def_coord_events
