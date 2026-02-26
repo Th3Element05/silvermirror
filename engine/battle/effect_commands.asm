629b13c9254784d0798962a4068573057d2ceac9
Sean Crawford <scrawford@sumnerone.com>
2026-02-26 00:41:16 -0600

    Changed SLEEP turns to 1-3 instead of 2-7

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -3672,18 +3672,18 @@ BattleCommand_SleepTarget:
 	jr nz, .fail
 
 	call AnimateCurrentMove
-	ld b, SLP_MASK
-	ld a, [wInBattleTowerBattle]
-	and a
-	jr z, .random_loop
+;	ld b, SLP_MASK
+;	ld a, [wInBattleTowerBattle]
+;	and a
+;	jr z, .random_loop ; Sleep for 1-3 turns instead of 2-7
 	ld b, %011
 
 .random_loop
 	call BattleRandom
 	and b
 	jr z, .random_loop
-	cp SLP_MASK
-	jr z, .random_loop
+;	cp SLP_MASK
+;	jr z, .random_loop ; Sleep for 1-3 turns instead of 2-7
 	inc a
 	ld [de], a
 	call UpdateOpponentInParty
