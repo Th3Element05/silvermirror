cb3a8a8d323e53985156972320fa3df102b0af78
Sean Crawford <scrawford@sumnerone.com>
2026-03-07 17:34:37 -0600

    removed steel_wing_house from route28, moved to route26

--- maps/Route28.asm
+++ maps/Route28.asm
@@ -17,8 +17,8 @@ Route28_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  7,  3, ROUTE_28_STEEL_WING_HOUSE, 1
 	warp_event 33,  5, VICTORY_ROAD_GATE, 3
+;	warp_event  7,  3, ROUTE_28_STEEL_WING_HOUSE, 1
 
 	def_coord_events
 
