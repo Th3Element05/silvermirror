104a8484085672e2705a6f6bec46d86d47a69464
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-03-12 00:53:45 -0500

    updated pokedex entries

--- data/pokemon/dex_entries/charmander.asm
+++ data/pokemon/dex_entries/charmander.asm
@@ -1,10 +1,18 @@
 	db "LIZARD@" ; species name
 	dw 200, 190 ; height, weight
 
-	db   "If it's healthy,"
+	db   "When it's healthy,"
 	next "the flame on the"
 	next "tip of its tail"
 
-	page "will burn vigor-"
-	next "ously, even if it"
-	next "gets a bit wet.@"
+	page "burns vigorously,"
+	next "even if it gets a"
+	next "little wet.@"
+
+;	db   "If it's healthy,"
+;	next "the flame on the"
+;	next "tip of its tail"
+;
+;	page "will burn vigor-"
+;	next "ously, even if it"
+;	next "gets a bit wet.@"
