ddc766ac822272f2c4fdcb26ca2d672eff2df43a
Sean Crawford <scrawford@sumnerone.com>
2026-03-12 00:53:45 -0500

    updated pokedex entries

--- data/pokemon/dex_entries/bulbasaur.asm
+++ data/pokemon/dex_entries/bulbasaur.asm
@@ -1,10 +1,26 @@
 	db "SEED@" ; species name
 	dw 204, 150 ; height, weight
 
-	db   "While it is young,"
-	next "it uses the"
-	next "nutrients that are"
+	db   "While it's young,"
+	next "it uses nutrients"
+	next "that are stored"
 
-	page "stored in the"
-	next "seeds on its back"
-	next "in order to grow.@"
+	page "in seeds on its"
+	next "back in order to"
+	next "grow stronger.@"
+
+;	db   "While it's young,"
+;	next "it uses nutrients"
+;	next "that are stored in"
+;
+;	page "seeds on its back"
+;	next "in order to grow"
+;	next "stronger.@"
+
+;	db   "While it is young,"
+;	next "it uses the"
+;	next "nutrients that are"
+;
+;	page "stored in the"
+;	next "seeds on its back"
+;	next "in order to grow.@"
