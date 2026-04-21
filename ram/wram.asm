8041a28b9d35f866b22bde8d5b8810e5a5d0bbf2
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-04-21 00:21:12 -0500

    expanded phone contacts to 12

--- ram/wram.asm
+++ ram/wram.asm
@@ -3428,7 +3428,7 @@ wPartyMon{d:n}Nickname:: ds MON_NAME_LENGTH
 endr
 wPartyMonNicknamesEnd::
 
-	ds 22
+	ds 20 ;22 -> DEF CONTACT_LIST_SIZE EQU 10 ;12
 
 wPokedexCaught:: flag_array NUM_POKEMON
 wEndPokedexCaught::
