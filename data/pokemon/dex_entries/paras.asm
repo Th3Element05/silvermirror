50aea2dbe2a04f2688346c466b7831bebd5fe40f
Sean Crawford <scrawford@sumnerone.com>
2026-05-22 09:41:38 -0500

    tweak paras dex entry

--- data/pokemon/dex_entries/paras.asm
+++ data/pokemon/dex_entries/paras.asm
@@ -1,10 +1,10 @@
 	db "MUSHROOM@" ; species name
 	dw 100, 120 ; height, weight
 
-	db   "The tochukaso"
+	db   "The mushrooms"
 	next "growing on this"
 	next "#MON's back"
 
-	page "orders it to"
-	next "extract juice from"
-	next "tree trunks.@"
+	page "command it to"
+	next "extract nutrients"
+	next "from tree trunks.@"
