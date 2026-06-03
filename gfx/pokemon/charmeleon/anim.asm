bec11941eb5e8cbdaae89ac98539ca19b44cda9e
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-06-03 13:24:43 -0500

    adjusted CHARMELEON and GASTLY front/backsprites, reduced length of CHARMELEON and MAGNEZONE animations.

--- gfx/pokemon/charmeleon/anim.asm
+++ gfx/pokemon/charmeleon/anim.asm
@@ -1,8 +1,6 @@
-	frame 3, 08
+	frame 1, 08
 	frame 2, 08
-	frame 3, 08
+	frame 1, 08
 	frame 2, 08
-	frame 1, 15
-	frame 3, 08
-	frame 4, 30
+	frame 1, 08
 	endanim
--- gfx/pokemon/magnezone/anim_idle.asm
+++ gfx/pokemon/magnezone/anim_idle.asm
@@ -1,4 +1,8 @@
-    frame 9, 07
-    frame 0, 07
-    frame 9, 07
-    endanim
+	frame 2, 12
+	frame 0, 12
+	frame 2, 12
+	endanim
+;	frame 9, 07
+;	frame 0, 07
+;	frame 9, 07
+;	endanim
