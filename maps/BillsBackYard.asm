7fada1e79e7e66d19fc3f6d0fcfe58d9f8ce55ab
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-07-02 15:37:42 -0500

    added time-of-day itemballs, but can't include them as-is due to some split evolution lines in Classic Mode.

--- maps/BillsBackYard.asm
+++ maps/BillsBackYard.asm
@@ -1,5 +1,7 @@
 	object_const_def
 	const BILLSBACKYARD_LUM_BERRY
+;	const BILLSBACKYARD_SUN_STONE
+;	const BILLSBACKYARD_DUSK_STONE
 
 BillsBackYard_MapScripts:
 	def_scene_scripts
@@ -44,6 +46,61 @@ BillsBackYard_LumBerry:
 BillsBackYard_NoFruit:
 	farsjump Std_NoFruitScript
 
+
+;BehindBillsHouse_SunStoneScript:
+;; This whole script is written out rather than as an itemball
+;; Due to only being available during the DAY
+;	getitemname STRING_BUFFER_3, SUN_STONE
+;	giveitem SUN_STONE
+;	iffalse BehindBillsHouse_BagFull
+;	disappear BILLSBACKYARD_SUN_STONE
+;	opentext
+;	getitemname STRING_BUFFER_3, SUN_STONE
+;	writetext Text_FoundSunDuskStone
+;	playsound SFX_ITEM
+;	waitsfx
+;	itemnotify
+;	closetext
+;	end
+
+;BehindBillsHouse_DuskStoneScript:
+;; This whole script is written out rather than as an itemball
+;; Due to only being available during the NITE
+;	getitemname STRING_BUFFER_3, DUSK_STONE
+;	giveitem DUSK_STONE
+;	iffalse BehindBillsHouse_BagFull
+;	disappear BILLSBACKYARD_DUSK_STONE
+;	opentext
+;	getitemname STRING_BUFFER_3, DUSK_STONE
+;	writetext Text_FoundSunDuskStone
+;	playsound SFX_ITEM
+;	waitsfx
+;	itemnotify
+;	closetext
+;	end
+
+;BehindBillsHouse_BagFull:
+;	opentext
+;	writetext Text_FoundSunDuskStone
+;	promptbutton
+;	writetext Text_NoRoomForSunDuskStone
+;	waitbutton
+;	closetext
+;	end
+
+;Text_FoundSunDuskStone:
+;	text "<PLAYER> found"
+;	line "@"
+;	text_ram wStringBuffer3
+;	text "!"
+;	done
+
+;Text_NoRoomForSunDuskStone:
+;	text "But <PLAYER>'s pack"
+;	line "is full."
+;	done
+
+
 BillsBackYard_MapEvents:
 	db 0, 0 ; filler
 
@@ -58,3 +115,5 @@ BillsBackYard_MapEvents:
 
 	def_object_events
 	object_event 11, 19, SPRITE_BERRY, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, BillsBackYard_LumBerry, EVENT_BILLS_BACK_YARD_LUM_BERRY
+;	object_event  9, 17, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, BillsBackYardSunStone, EVENT_BILLS_BACK_YARD_SUN_STONE
+;	object_event 11, 16, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, BillsBackYardDuskStone, EVENT_BILLS_BACK_YARD_DUSK_STONE
