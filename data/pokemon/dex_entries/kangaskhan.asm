f35b7af74d9389383abddca542dc93ef83c41b48
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-07-12 22:06:05 -0500

    changed kangaskhan and phanpy dex entries

--- data/pokemon/dex_entries/kangaskhan.asm
+++ data/pokemon/dex_entries/kangaskhan.asm
@@ -1,10 +1,18 @@
 	db "PARENT@" ; species name
 	dw 703, 1760 ; height, weight
 
-	db   "To avoid"
-	next "crushing the"
-	next "baby it carries in"
+	db   "It raises its off-"
+	next "spring a pouch on"
+	next "its belly. It lets"
 
-	page "its pouch, it"
-	next "always sleeps"
-	next "standing up.@"
+	page "the baby out to"
+	next "play only when it"
+	next "feels safe.@"
+
+;	db   "To avoid crushing"
+;	next "the baby that it"
+;	next "carries around in"
+;
+;	page "its pouch, it"
+;	next "always sleeps"
+;	next "standing up.@"
--- data/pokemon/dex_entries/phanpy.asm
+++ data/pokemon/dex_entries/phanpy.asm
@@ -1,10 +1,26 @@
 	db "LONG NOSE@" ; species name
 	dw 108, 740 ; height, weight
 
-	db   "During the desert-"
-	next "ed morning hours,"
-	next "it comes ashore"
+	db   "It is very strong"
+	next "for its size. It"
+	next "can easily pick up"
 
-	page "where it deftly"
-	next "uses its trunk to"
-	next "take a shower.@"
+	page "and carry around"
+	next "an adult human on"
+	next "its back.@"
+
+;	db   "It swings its long"
+;	next "snout around play-"
+;	next "fully, but because"
+;
+;	page "it is so strong,"
+;	next "that can be"
+;	next "dangerous.@"
+
+;	db   "During the desert-"
+;	next "ed morning hours,"
+;	next "it comes ashore"
+;
+;	page "where it deftly"
+;	next "uses its trunk to"
+;	next "take a shower.@"
