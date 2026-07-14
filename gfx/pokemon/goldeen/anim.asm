8d4ecc6abce3f0268fc52c451a199e3557312f95
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-07-14 08:36:16 -0500

    reduced length of goldeen animation

--- gfx/pokemon/goldeen/anim.asm
+++ gfx/pokemon/goldeen/anim.asm
@@ -1,7 +1,15 @@
 	frame 1, 30
 	frame 0, 16
 	setrepeat 4
-	frame 2, 12
-	frame 0, 12
-	dorepeat 3
+	frame 2, 10
+	frame 0, 10
+	dorepeat 2
 	endanim
+
+;	frame 1, 30
+;	frame 0, 16
+;	setrepeat 4
+;	frame 2, 12
+;	frame 0, 12
+;	dorepeat 3
+;	endanim
