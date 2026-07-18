dd75eaf425118f2b3529352d10a95a406dbceef9
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-07-18 00:55:21 -0500

    reduced length of some pokemon animations

--- gfx/pokemon/rhydon/anim.asm
+++ gfx/pokemon/rhydon/anim.asm
@@ -2,7 +2,7 @@
 	frame 1, 34
 	frame 2, 10
 	frame 4, 10
-	setrepeat 16
+	setrepeat 8 ;16
 	frame 3, 02
 	frame 4, 02
 	dorepeat 5
--- gfx/pokemon/tauros/anim.asm
+++ gfx/pokemon/tauros/anim.asm
@@ -2,9 +2,9 @@
 	frame 3, 10
 	frame 1, 28
 	frame 0, 12
-	setrepeat 3
+;	setrepeat 3
 	frame 2, 11
 	frame 3, 11
-	dorepeat 5
+;	dorepeat 5
 	frame 1, 07
 	endanim
