426f55b809c1c9bad63083ef93300f53fea310a4
Th3Element05 <57608622+Th3Element05@users.noreply.github.com>
2026-07-20 23:54:13 -0500

    fixed warp events misaligned with warp carpets

--- maps/VioletKylesHouse.asm
+++ maps/VioletKylesHouse.asm
@@ -34,8 +34,8 @@ VioletKylesHouse_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
+	warp_event  2,  7, VIOLET_CITY, 6
 	warp_event  3,  7, VIOLET_CITY, 6
-	warp_event  4,  7, VIOLET_CITY, 6
 
 	def_coord_events
 
--- maps/VioletNicknameSpeechHouse.asm
+++ maps/VioletNicknameSpeechHouse.asm
@@ -46,8 +46,8 @@ VioletNicknameSpeechHouse_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
+	warp_event  2,  7, VIOLET_CITY, 4
 	warp_event  3,  7, VIOLET_CITY, 4
-	warp_event  4,  7, VIOLET_CITY, 4
 
 	def_coord_events
 
